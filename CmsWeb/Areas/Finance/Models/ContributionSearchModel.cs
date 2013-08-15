089c308ed6080e8c6663aba3e9e9f4f8d8e9544b
David Carroll <david@bvcms.com>
2013-08-14 22:35:00 -0500

    Contribution Search features

--- CmsWeb/Areas/Finance/Models/ContributionSearchModel.cs
+++ CmsWeb/Areas/Finance/Models/ContributionSearchModel.cs
@@ -17,183 +17,223 @@ using CmsData;
 
 namespace CmsWeb.Models
 {
-	public class ContributionSearchModel : PagerModel2
-	{
-	    private APIContributionSearchModel api { get; set; }
-	    public ContributionSearchInfo SearchInfo { get; set; }
+    public class ContributionSearchModel : PagerModel2
+    {
+        private APIContributionSearchModel api { get; set; }
+        public ContributionSearchInfo SearchInfo { get; set; }
 
-	    private string _name = null;
-	    public string Name
-	    {
-	        get
-	        {
-	            if (!_name.HasValue())
-	                _name = (from p in DbUtil.Db.People
-	                         where p.PeopleId == SearchInfo.PeopleId
-	                         select p.Name).SingleOrDefault() ?? "";
-	            return _name;
-	        }
-	    }
+        private string _name = null;
+        public string Name
+        {
+            get
+            {
+                if (!_name.HasValue())
+                    _name = (from p in DbUtil.Db.People
+                             where p.PeopleId == SearchInfo.PeopleId
+                             select p.Name).SingleOrDefault() ?? "";
+                return _name;
+            }
+        }
 
-	    private void Setup()
-	    {
-			GetCount = api.Count;
-			Sort = "Date";
-			Direction = "desc";
-	        SearchInfo = api.model;
-	    }
+        private void Setup()
+        {
+            GetCount = api.Count;
+            Sort = "Date";
+            Direction = "desc";
+            SearchInfo = api.model;
+        }
 
-	    public ContributionSearchModel()
-	    {
+        public ContributionSearchModel()
+        {
             api = new APIContributionSearchModel(DbUtil.Db);
-	        Setup();
-	    }
-	    public ContributionSearchModel(ContributionSearchInfo m)
-	    {
+            Setup();
+        }
+        public ContributionSearchModel(ContributionSearchInfo m)
+        {
             api = new APIContributionSearchModel(DbUtil.Db, m);
-	        Setup();
-	    }
+            Setup();
+        }
 
-	    public ContributionSearchModel(int? peopleId, int? year)
-	    {
+        public ContributionSearchModel(int? peopleId, int? year)
+        {
             api = new APIContributionSearchModel(DbUtil.Db);
-	        Setup();
-	        SearchInfo.PeopleId = peopleId;
-	        SearchInfo.Year = year;
-	    }
+            Setup();
+            SearchInfo.PeopleId = peopleId;
+            SearchInfo.Year = year;
+        }
 
-	    public IEnumerable<ContributionInfo> ContributionsList()
-	    {
-	        var q = api.FetchContributions();
+        public IEnumerable<ContributionInfo> ContributionsList()
+        {
+            var q = api.FetchContributions();
             q = ApplySort(q).Skip(StartRow).Take(PageSize);
-	        return api.ContributionsList(q);
-	    }
-	    public IEnumerable<ContributionInfo> ContributionsListAll()
-	    {
-	        var q = api.FetchContributions();
-	        return api.ContributionsList(q);
-	    }
+            return api.ContributionsList(q);
+        }
+        public IEnumerable<ContributionInfo> ContributionsListAll()
+        {
+            var q = api.FetchContributions();
+            return api.ContributionsList(q);
+        }
 
-	    public IQueryable<Contribution> ApplySort(IQueryable<Contribution> q)
-		{
-			if ((Direction ?? "desc") == "asc")
-				switch (Sort)
-				{
-					case "Date":
-						q = q.OrderBy(c => c.ContributionDate);
-						break;
-					case "Amount":
-						q = from c in q
-							orderby c.ContributionAmount, c.ContributionDate descending
-							select c;
-						break;
-					case "Type":
-						q = from c in q
-							orderby c.ContributionTypeId, c.ContributionDate descending
-							select c;
-						break;
-					case "Status":
-						q = from c in q
-							orderby c.ContributionStatusId, c.ContributionDate descending
-							select c;
-						break;
-					case "Fund":
-						q = from c in q
-							orderby c.FundId, c.ContributionDate descending
-							select c;
-						break;
+        public IQueryable<Contribution> ApplySort(IQueryable<Contribution> q)
+        {
+            if ((Direction ?? "desc") == "asc")
+                switch (Sort)
+                {
+                    case "Date":
+                        q = q.OrderBy(c => c.ContributionDate);
+                        break;
+                    case "Amount":
+                        q = from c in q
+                            orderby c.ContributionAmount, c.ContributionDate descending
+                            select c;
+                        break;
+                    case "Type":
+                        q = from c in q
+                            orderby c.ContributionTypeId, c.ContributionDate descending
+                            select c;
+                        break;
+                    case "Status":
+                        q = from c in q
+                            orderby c.ContributionStatusId, c.ContributionDate descending
+                            select c;
+                        break;
+                    case "Fund":
+                        q = from c in q
+                            orderby c.FundId, c.ContributionDate descending
+                            select c;
+                        break;
                     case "Name":
-						q = from c in q
-							orderby c.Person.Name2
-							select c;
-						break;
-				}
-			else
-				switch (Sort)
-				{
-					case "Date":
-						q = q.OrderByDescending(c => c.ContributionDate);
-						break;
-					case "Amount":
-						q = from c in q
-							orderby c.ContributionAmount descending, c.ContributionDate descending
-							select c;
-						break;
-					case "Type":
-						q = from c in q
-							orderby c.ContributionTypeId descending, c.ContributionDate descending
-							select c;
-						break;
-					case "Status":
-						q = from c in q
-							orderby c.ContributionStatusId descending, c.ContributionDate descending
-							select c;
-						break;
-					case "Fund":
-						q = from c in q
-							orderby c.FundId descending, c.ContributionDate descending
-							select c;
-						break;
+                        q = from c in q
+                            orderby c.Person.Name2
+                            select c;
+                        break;
+                }
+            else
+                switch (Sort)
+                {
+                    case "Date":
+                        q = q.OrderByDescending(c => c.ContributionDate);
+                        break;
+                    case "Amount":
+                        q = from c in q
+                            orderby c.ContributionAmount descending, c.ContributionDate descending
+                            select c;
+                        break;
+                    case "Type":
+                        q = from c in q
+                            orderby c.ContributionTypeId descending, c.ContributionDate descending
+                            select c;
+                        break;
+                    case "Status":
+                        q = from c in q
+                            orderby c.ContributionStatusId descending, c.ContributionDate descending
+                            select c;
+                        break;
+                    case "Fund":
+                        q = from c in q
+                            orderby c.FundId descending, c.ContributionDate descending
+                            select c;
+                        break;
                     case "Name":
-						q = from c in q
-							orderby c.Person.Name2 descending
-							select c;
-						break;
-				}
-			return q;
-		}
-		public SelectList ContributionStatuses()
-		{
-			return new SelectList(new CodeValueModel().ContributionStatuses99(),
-				"Id", "Value", SearchInfo.Status.ToString());
-		}
-		public SelectList ContributionTypes()
-		{
-			return new SelectList(new CodeValueModel().ContributionTypes0(),
-				"Id", "Value", SearchInfo.Type.ToString());
-		}
-		public SelectList BundleTypes()
-		{
-			return new SelectList(new CodeValueModel().BundleHeaderTypes0(),
-				"Id", "Value", SearchInfo.Type.ToString());
-		}
-		public IEnumerable<SelectListItem> Years()
-		{
-			var q = from c in DbUtil.Db.Contributions
-					where c.PeopleId == SearchInfo.PeopleId || SearchInfo.PeopleId == null
-					group c by c.ContributionDate.Value.Year
-						into g
-						orderby g.Key descending
-						select new SelectListItem { Text = g.Key.ToString() };
-			var list = q.ToList();
-			list.Insert(0, new SelectListItem { Text = "(not specified)", Value = "0" });
-			return list;
-		}
+                        q = from c in q
+                            orderby c.Person.Name2 descending
+                            select c;
+                        break;
+                }
+            return q;
+        }
+        public SelectList ContributionStatuses()
+        {
+            return new SelectList(new CodeValueModel().ContributionStatuses99(),
+                "Id", "Value", SearchInfo.Status.ToString());
+        }
+        public SelectList ContributionTypes()
+        {
+            return new SelectList(new CodeValueModel().ContributionTypes0(),
+                "Id", "Value", SearchInfo.Type.ToString());
+        }
+        public SelectList BundleTypes()
+        {
+            return new SelectList(new CodeValueModel().BundleHeaderTypes0(),
+                "Id", "Value", SearchInfo.Type.ToString());
+        }
+        public IEnumerable<SelectListItem> Years()
+        {
+            var q = from c in DbUtil.Db.Contributions
+                    where c.PeopleId == SearchInfo.PeopleId || SearchInfo.PeopleId == null
+                    group c by c.ContributionDate.Value.Year
+                        into g
+                        orderby g.Key descending
+                        select new SelectListItem { Text = g.Key.ToString() };
+            var list = q.ToList();
+            list.Insert(0, new SelectListItem { Text = "(not specified)", Value = "0" });
+            return list;
+        }
 
-		public IEnumerable<SelectListItem> Funds()
-		{
-			var list = (from c in DbUtil.Db.Contributions
-						where c.PeopleId == SearchInfo.PeopleId || SearchInfo.PeopleId == null
-						group c by new { c.FundId, c.ContributionFund.FundName }
-							into g
-							orderby g.Key.FundName
-							select new SelectListItem()
-								   {
-									   Value = g.Key.FundId.ToString(),
-									   Text = g.Key.FundName
-								   }).ToList();
-			list.Insert(0, new SelectListItem { Text = "(not specified)", Value = "0" });
-			return list;
-		}
+        public IEnumerable<SelectListItem> Funds()
+        {
+            var list = (from c in DbUtil.Db.Contributions
+                        where c.PeopleId == SearchInfo.PeopleId || SearchInfo.PeopleId == null
+                        group c by new { c.FundId, c.ContributionFund.FundName }
+                            into g
+                            orderby g.Key.FundName
+                            select new SelectListItem()
+                                   {
+                                       Value = g.Key.FundId.ToString(),
+                                       Text = g.Key.FundName
+                                   }).ToList();
+            list.Insert(0, new SelectListItem { Text = "(not specified)", Value = "0" });
+            return list;
+        }
 
-	    public decimal Total()
-	    {
-	        return api.Total();
-	    }
+        public decimal Total()
+        {
+            return api.Total();
+        }
 
-	    public int Count()
-	    {
-	        return api.Count();
-	    }
-	}
+        public int Count()
+        {
+            return api.Count();
+        }
+
+        public void Return(int cid)
+        {
+            var c = DbUtil.Db.Contributions.Single(ic => ic.ContributionId == cid);
+            var r = CreateContributionRecord(c);
+            c.ContributionStatusId = ContributionStatusCode.Returned;
+            r.ContributionTypeId = ContributionTypeCode.ReturnedCheck;
+            r.ContributionDesc = "Returned Check for Contribution Id = " + c.ContributionId;
+
+            DbUtil.Db.Contributions.InsertOnSubmit(r);
+            DbUtil.Db.SubmitChanges();
+        }
+
+        public void Reverse(int cid)
+        {
+            var c = DbUtil.Db.Contributions.Single(ic => ic.ContributionId == cid);
+            var r = CreateContributionRecord(c);
+            c.ContributionStatusId = ContributionStatusCode.Reversed;
+            r.ContributionTypeId = ContributionTypeCode.Reversed;
+            r.ContributionDesc = "Reversed Contribution Id = " + c.ContributionId;
+            DbUtil.Db.Contributions.InsertOnSubmit(r);
+            DbUtil.Db.SubmitChanges();
+        }
+
+        private static Contribution CreateContributionRecord(Contribution c)
+        {
+            var now = Util.Now;
+            var r = new Contribution
+            {
+                ContributionStatusId = ContributionStatusCode.Recorded,
+                CreatedBy = Util.UserId1,
+                CreatedDate = now,
+                PeopleId = c.PeopleId,
+                ContributionAmount = c.ContributionAmount,
+                ContributionDate = now.Date,
+                PostingDate = now,
+                FundId = c.FundId,
+            };
+            return r;
+        }
+    }
 }
