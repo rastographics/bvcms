18a9b0312531e41603db9cc9ca461f4a8cea58f0
David Carroll <david@bvcms.com>
2013-09-18 13:18:27 -0500

    Bug Fixes

--- CmsWeb/Code/Fingerprint.cs
+++ CmsWeb/Code/Fingerprint.cs
@@ -6,31 +6,9 @@ using System.Web;
 using System.Web.Caching;
 using System.Web.Hosting;
 using System.Xml.Linq;
-using UtilityExtensions;
 
 public class Fingerprint
 {
-    public static string Tag(string path)
-    {
-        if (HttpRuntime.Cache[path] == null)
-        {
-            string absolute = HostingEnvironment.MapPath("~" + path);
-            var ext = Path.GetExtension(absolute);
-            var f = Path.GetFileNameWithoutExtension(absolute);
-            var d = path.Remove(path.LastIndexOf('/'));
-            DateTime dt = File.GetLastWriteTime(absolute);
-#if DEBUG
-            const string min = "";
-#else
-            const string min = ".min";
-#endif
-            string tag = "?v=" + dt.Ticks;
-            string result = "{0}/{1}{2}{3}{4}".Fmt(d, f, min, ext, tag);
-            HttpRuntime.Cache.Insert(path, result, new CacheDependency(absolute));
-        }
-
-        return HttpRuntime.Cache[path] as string;
-    }
     public static HtmlString Script(string path)
     {
         if (HttpRuntime.Cache[path] == null)
