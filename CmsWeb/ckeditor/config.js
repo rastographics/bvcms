b1b39a4d46a8b0e027815afa9d47ab13e25d3a6f
David Carroll <david@bvcms.com>
2013-12-04 23:02:57 -0600

    Still working on new CKEditor support

--- CmsWeb/ckeditor/config.js
+++ CmsWeb/ckeditor/config.js
@@ -4,7 +4,22 @@
  */
 
 CKEDITOR.editorConfig = function (config) {
-    // Define changes to default configuration here. For example:
-    // config.language = 'fr';
-    // config.uiColor = '#AADC6E';
+    config.toolbar = 'MyToolbar';
+
+    config.allowedContent = true;
+    config.toolbar_MyToolbar =
+	[
+    	{ name: 'document', items: ['Source', '-', 'Preview'] },
+    	{ name: 'clipboard', items: ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo'] },
+    	{ name: 'editing', items: ['Find', 'Replace', '-', 'SelectAll', '-', 'SpellChecker', 'Scayt'] },
+    	{ name: 'links', items: ['Link', 'Unlink', 'Anchor'] },
+    	'/',
+    	{ name: 'basicstyles', items: ['Bold', 'Italic', 'Underline', 'Strike', '-', 'RemoveFormat'] },
+    	{ name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'] },
+    	{ name: 'insert', items: ['Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar'] },
+    	'/',
+    	{ name: 'styles', items: ['Styles', 'Format', 'Font', 'FontSize'] },
+    	{ name: 'colors', items: ['TextColor', 'BGColor'] },
+    	{ name: 'tools', items: ['Maximize', 'ShowBlocks', '-', 'About'] }
+	];
 };
