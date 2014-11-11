337656120e62a1efc8f45f7bc5ffd0488c90df1f
Brian Lankford <brianlankford1@gmail.com>
2014-11-11 16:45:35 -0600

    Got initial custom view engine started.

--- CmsWeb/Framework/CmsViewEngine.cs
+++ CmsWeb/Framework/CmsViewEngine.cs
@@ -1,11 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Web.Mvc;
 
 namespace CmsWeb.Framework
 {
-    public class CmsViewEngine
+    public class CmsViewEngine : IViewEngine
     {
+        public bool UseTouchPointViews { get; private set; }
+         
+        public CmsViewEngine(bool useTouchPointViews)
+        {
+            UseTouchPointViews = useTouchPointViews;  
+        }
+
+        public ViewEngineResult FindPartialView(ControllerContext controllerContext, string partialViewName, bool useCache)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ViewEngineResult FindView(ControllerContext controllerContext, string viewName, string masterName, bool useCache)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReleaseView(ControllerContext controllerContext, IView view)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
