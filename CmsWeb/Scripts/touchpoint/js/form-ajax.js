169eb83c8fed07bab78dce725f4da0131c6b4e49
Brian Lankford <brianlankford1@gmail.com>
2014-12-04 15:43:54 -0600

    Pulled in form-ajax and search-add.  Don't have them working yet.

---
+++ CmsWeb/Scripts/touchpoint/js/form-ajax.js
@@ -0,0 +1,169 @@
+﻿$(function () {
+    $.AttachFormElements = function () {
+        $("form.ajax input.ajax-typeahead").typeahead({
+            minLength: 3,
+            remote: {
+                url: "test",
+                beforeSend: function(jqXhr, settings) {
+                    $.SetLoadingIndicator();
+                },
+                replace: function (url, uriEncodedQuery) {
+                    return $("input:focus").data("link") + "?query=" + uriEncodedQuery;
+                }
+            }
+        });
+        $.DatePickersAndChosen();
+    };
+    $.DatePickersAndChosen = function () {
+        $("form.ajax .date").datepicker({
+            autoclose: true,
+            orientation: "auto",
+            forceParse: false,
+            format: $.dtoptions.format
+        });
+        $('form.ajax select:not([plain])').chosen();
+        $('form.ajax a.editable').editable();
+    };
+    $("ul.nav-tabs a.ajax,a.ajax.ui-tabs-anchor").on("click", function (event) {
+        var $this = $(this);
+        var alreadyClicked = $this.data('clicked');
+        if (alreadyClicked) {
+            return false;
+        }
+        $this.data('clicked', true);
+        var state = $this.attr("href") || $this.data("target");
+        var d = $(state);
+        var url = d.data("link");
+        if (!d.hasClass("loaded"))
+            $.ajax({
+                type: 'POST',
+                url: url,
+                data: {},
+                success: function (data, status) {
+                    d.addClass("loaded");
+                    d.html(data).ready(function () {
+                        var $form = d.find("form.ajax");
+                        if ($form.data("init")) {
+                            $.InitFunctions[$form.data("init")]();
+                        }
+                        if ($form.data("init2")) {
+                            $.InitFunctions[$form.data("init2")]();
+                        }
+                    });
+                }
+            });
+        return true;
+    });
+    $("div.tab-pane").on("click", "a.ajax-refresh", function (event) {
+        event.preventDefault();
+        var d = $(this).closest("div.tab-pane");
+        $.formAjaxClick($(this), d.data("link"));
+        return false;
+    });
+    $("form.ajax a.submit").on("click", function (event) {
+        event.preventDefault();
+        var t = $(this);
+        if (t.data("confirm"))
+            bootbox.confirm(t.data("confirm"), function(ret) {
+                if (ret == true)
+                    $.formAjaxSubmit(t);
+            });
+        else
+            $.formAjaxSubmit(t);
+        return false;
+    });
+    $.formAjaxSubmit = function(a) {
+        var $form = a.closest("form.ajax");
+        $form.attr("action", a[0].href);
+        $form.submit();
+    };
+
+    $("form.ajax a.ajax").on("click", function (event) {
+        event.preventDefault();
+        var t = $(this);
+        if (t.data("confirm"))
+            bootbox.confirm(t.data("confirm"), function(ret) {
+                if (ret == true)
+                    $.formAjaxClick(t);
+            });
+        else
+            $.formAjaxClick(t);
+        return false;
+    });
+    $.formAjaxClick = function (a, link) {
+        var $form = a.closest("form.ajax");
+        var url = link || a.data("link");
+        if (typeof url === 'undefined')
+            url = a[0].href;
+        var data = $form.serialize();
+        if (data.length === 0)
+            data = {};
+        if (!a.hasClass("validate") || $form.valid()) {
+            $.ajax({
+                type: 'POST',
+                url: url,
+                data: data,
+                success: function (ret, status) {
+                    if(a.data("redirect"))
+                        window.location = ret;
+                    else if ($form.hasClass("modal")) {
+                        $form.html(ret).ready(function () {
+                            $form.removeClass("hide");
+                            var top = ($(window).height() - $form.height()) / 2;
+                            if (top < 10)
+                                top = 10;
+                            $form.css({ 'margin-top': top, 'top': '0' });
+                            $.AttachFormElements();
+                            if (a.data("callback"))
+                                $.InitFunctions[a.data("callback")]();
+                        });
+                    } else {
+                        var results = $($form.data("results") || $form);
+                        results.replaceWith(ret).ready(function () {
+                            $.AttachFormElements();
+                            if ($form.data("init"))
+                                $.InitFunctions[$form.data("init")]();
+                            if ($form.data("init2")) {
+                                $.InitFunctions[$form.data("init2")]();
+                            }
+                            if (a.data("callback"))
+                                $.InitFunctions[a.data("callback")]();
+                        });
+                    }
+                },
+                error: function (xhr, ajaxOptions, thrownError) {
+                    alert(xhr.status);
+                    alert(thrownError);
+                }
+            });
+        }
+        return false;
+    };
+
+    $.validator.addMethod("unallowedcode", function (value, element, params) {
+        return value !== params.code;
+    }, "required, select item");
+
+    var $loadingcount = 0;
+    $.ajaxSetup({
+        beforeSend: function () {
+            $.SetLoadingIndicator();
+        },
+        complete: function () {
+            $loadingcount--;
+            if ($loadingcount === 0)
+                $("#loading-indicator").hide();
+        }
+    });
+    $.SetLoadingIndicator = function() {
+        $("#loading-indicator").css({
+            'position': 'absolute',
+            'left': $(window).width() / 2,
+            'top': $(window).height() / 2,
+            'z-index': 2000
+        }).show();
+        $loadingcount++;
+    };
+    if(!$.InitFunctions)
+        $.InitFunctions = {};
+});
---
+++ CmsWeb/Scripts/touchpoint/js/search/search-add.js
@@ -0,0 +1,132 @@
+﻿$(function () {
+    $("a.searchadd").on("click", function (ev) {
+        ev.preventDefault();
+        $("<form id='search-add' class='modal fade hide validate ajax form-horizontal' data-width='700' data-keyboard='false' data-backdrop='static' />")
+            .load($(this).attr("href"), {}, function () {
+                $(this).modal("show");
+                $.AttachFormElements();
+                $(this).validate({
+                    highlight: function (element) {
+                        $(element).closest(".control-group").addClass("error");
+                    },
+                    unhighlight: function (element) {
+                        $(element).closest(".control-group").removeClass("error");
+                    }
+                });
+                $(this).on('hidden', function () {
+                    $(this).remove();
+                });
+            });
+    });
+    $("#search-add a.commit").on("click", function (ev) {
+        ev.preventDefault();
+        var $this = $(this);
+        var alreadyClicked = $this.data('clicked');
+        if (alreadyClicked) {
+            return false;
+        }
+        $this.data('clicked', true);
+        var f = $this.closest("form");
+        var q = f.serialize();
+        var loc = $this.attr("href");
+        $.post(loc, q, function (ret) {
+            f.modal("hide");
+            if (ret.message)
+                alert(ret.message);
+            else if (ret.from === 'Menu')
+                window.location = '/Person2/' + ret.pid;
+            else
+                AddSelected(ret);
+        });
+        return false;
+    });
+    $(document).on("shown", "#search-add", function () {
+        $("#search-add #Name").focus();
+        $("#search-add input").on("keydown", function (event) {
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                $("#searchperson").click();
+                return false;
+            }
+            else
+                return true;
+        });
+    });
+    $.fn.loadWith = function (u, f) {
+        var c = $(this);
+        $.post(u, function (d) {
+            c.replaceWith(d).ready(f);
+        });
+    };
+    $("#search-add a.clear").on('click', function (ev) {
+        ev.preventDefault();
+        $("#Name").val('');
+        $("#Phone").val('');
+        $("#Address").val('');
+        $("#dob").val('');
+        return false;
+    });
+
+    $("form.ajax tbody > tr a.reveal").on("click", function (e) {
+        e.stopPropagation();
+    });
+    $.NotReveal = function (ev) {
+        if ($(ev.target).is("a"))
+            if (!$(ev.target).is('.reveal'))
+                return true;
+        return false;
+    };
+    $("form.ajax tr.section").on("click", function (ev) {
+        if ($.NotReveal(ev)) return;
+        ev.preventDefault();
+        $ToggleShown($(this));
+    });
+    $('form.ajax a[rel="reveal"]').on("click", function (ev) {
+        ev.preventDefault();
+        $ToggleShown($(this).parents("tr"));
+    });
+    var $ToggleShown = function (tr) {
+        if (tr.hasClass("notshown"))
+            $ShowAll(tr);
+        else if (tr.hasClass("shown"))
+            $CollapseAll(tr);
+        else
+            tr.next("tr").find("div.collapse")
+                .off('hidden')
+                .on("hidden", function (e) { e.stopPropagation(); })
+                .collapse("toggle");
+    };
+    var $ShowAll = function (tr) {
+        tr.nextUntil("tr.section").find("div.collapse")
+            .off('hidden')
+            .on("hidden", function (e) { e.stopPropagation(); })
+            .collapse("show");
+        tr.removeClass("notshown").addClass("shown");
+        tr.find("i").removeClass("fa-caret-right").addClass("fa-caret-down");
+    };
+    var $CollapseAll = function (tr) {
+        tr.nextUntil("tr.section").find("div.collapse")
+            .off("hidden")
+            .on("hidden", function (e) { e.stopPropagation(); })
+            .collapse('hide');
+        tr.removeClass("shown").addClass("notshown");
+        tr.find("i").removeClass("fa-caret-down").addClass("fa-caret-right");
+    };
+    $("form.ajax tr.master").on("click", function (ev) {
+        if ($.NotReveal(ev)) return;
+        ev.preventDefault();
+        $(this).next("tr").find("div.collapse")
+            .off('hidden')
+            .on("hidden", function (e) { e.stopPropagation(); })
+            .collapse("toggle");
+    });
+    $("form.ajax tr.details").on("click", function (ev) {
+        if ($.NotReveal(ev)) return;
+        ev.preventDefault();
+        ev.stopPropagation();
+        $(this).find("div.collapse")
+            .off("hidden")
+            .on("hidden", function (e) { e.stopPropagation(); })
+            .collapse('hide');
+    });
+});
