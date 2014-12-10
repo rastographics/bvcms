0127e042d9cae6c49ea3655ff4e74b7e61b95c12
Brian Lankford <brianlankford1@gmail.com>
2014-12-10 10:44:07 -0600

    Got both church attendance reports finished for now.

--- CmsWeb/Scripts/touchpoint/js/reports/church-attendance.js
+++
@@ -1,9 +0,0 @@
-﻿$(function () {
-    $('.input-group.date').datepicker();
-    $("a.run").click(function (ev) {
-        ev.preventDefault();
-        if (!$.DateValid($("#Sunday").val(), true))
-            return;
-        window.location = "/Reports/ChurchAttendance/" + $.SortableDate($("#Sunday").val());
-    });
-});
