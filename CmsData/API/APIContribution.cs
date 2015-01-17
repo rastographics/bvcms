1f32a15ae55a936499fbac23603585a67e9bc6e0
David Carroll <david@bvcms.com>
2015-01-16 21:41:54 -0600

    Prevent Contribution Statements from printing statement if zero contributions, pledges, gift-in-kind, or non-tax items.

--- CmsData/API/APIContribution.cs
+++ CmsData/API/APIContribution.cs
@@ -465,16 +465,10 @@ namespace CmsData.API
     {
         public string Name { get; set; }
         public string MailingAddress { get; set; }
-//        public string Address1 { get; set; }
-//        public string Address2 { get; set; }
-//        public string City { get; set; }
-//        public string State { get; set; }
-//        public string Zip { get; set; }
         public int PeopleId { get; set; }
         public int? SpouseID { get; set; }
         public int FamilyId { get; set; }
         public DateTime? DeacesedDate { get; set; }
-//        public string CityStateZip { get { return UtilityExtensions.Util.FormatCSZ4(City, State, Zip); } }
         public int hohInd { get; set; }
         public int FamilyPositionId { get; set; }
         public int? Age { get; set; }
