1989c9562511a097da06989abdadbfe53458d7cf
Brian Lankford <brianlankford1@gmail.com>
2015-03-04 11:28:43 -0600

    Updated to the latest 1.0 release of jquery, not going to 2.0 yet, because of browser support issues for now.

--- CmsWeb/Code/ViewExtensions.cs
+++ CmsWeb/Code/ViewExtensions.cs
@@ -749,7 +749,7 @@ namespace CmsWeb
         }
         public static HtmlString jQuery()
         {
-            return new HtmlString("<script src='//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>\n");
+            return new HtmlString("<script src='//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js'></script>\n");
         }
         public static HtmlString jQueryUICss()
         {
