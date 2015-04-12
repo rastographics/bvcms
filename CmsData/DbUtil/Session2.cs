28f2a9f166e474bda23e8e0e36d5a4be2cb4d39e
David Carroll <david@bvcms.com>
2015-04-12 12:01:11 -0500

    ServerLink in debug mode should use localhost

--- CmsData/DbUtil/Session2.cs
+++ CmsData/DbUtil/Session2.cs
@@ -82,7 +82,13 @@ namespace CmsData
 
         public string ServerLink(string path = "")
         {
+#if DEBUG                    
+            var request = HttpContext.Current.Request;
+            return Util.URLCombine(Util.Scheme() + "://" + request.Url.Authority, path);
+#else
             return Util.URLCombine(CmsHost, path);
+#endif
+
         }
 
         public void CopySession()
