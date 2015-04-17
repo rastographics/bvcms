5061f46fa7ae057cadb5e7c82746c1c648088f2b
Brian Lankford <brianlankford1@gmail.com>
2015-04-17 08:23:51 -0500

    Adding in organizations javascript files.

---
+++ CmsWeb/Content/touchpoint/js/org/org-member-dialog.js
@@ -0,0 +1,46 @@
+﻿$(function () {
+    $('body').on('click', '#membergroups .ckbox', function (ev) {
+        var f = $(this).closest("form");
+        var q = f.serialize() + '&' + $.param({
+             'ck': $(this).is(":checked")
+        });
+        $.post($(this).attr("href"), q);
+        return true;
+    });
+
+    $('body').on('click', '#membergroups .update-smallgroup', function (ev) {
+        ev.preventDefault();
+        var href = $(this).attr("href");
+        var msg = "This will add or remove everybody to/from this sub-group. Are you sure?";
+
+        bootbox.confirm(msg, function (confirmed) {
+            if (confirmed)
+                $.post(href);
+        });
+        return false;
+    });
+
+    $('body').on('keydown', '#OrgSearch', function (event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            $("#orgsearchbtn").click();
+        }
+    });
+
+    $('body').on('click', 'a.movemember', function (ev) {
+        ev.preventDefault();
+        var f = $(this).closest('form');
+        var q = f.serialize();
+        var href = $(this).attr("href");
+        bootbox.confirm("are you sure?", function (confirmed) {
+            if (confirmed) {
+                $.post(href, q, function (ret) {
+                    f.modal("hide");
+                    RebindMemberGrids();
+                });
+            }
+        });
+        return false;
+    });
+});
+
---
+++ CmsWeb/Content/touchpoint/js/org/reg-setting.js
@@ -0,0 +1,77 @@
+﻿$(function () {
+
+    CKEDITOR.replace('editor', {
+        height: 200,
+        customConfig: '/Content/touchpoint/lib/ckeditor/js/ckeditorconfig.js'
+    });
+    
+    $.InitFunctions.SettingFormsInit = function (f) {
+        $('a.notifylist').SearchUsers({
+            UpdateShared: function (topid, topid0, ele) {
+                $.post("/Org/UpdateNotifyIds", {
+                    id: $("#OrganizationId").val(),
+                    topid: topid,
+                    field: ele.data("field")
+                }, function (ret) {
+                    ele.html(ret);
+                });
+            }
+        });
+    };
+
+    $('body').on('click', 'a.editor', function (ev) {
+        if (!$(this).attr("href"))
+            return false;
+        var name = $(this).attr("tb");
+        ev.preventDefault();
+        CKEDITOR.instances['editor'].setData($("#" + name).val());
+        dimOn();
+        $("#EditorDialog").center().show();
+        $("#saveedit").off("click").on("click", function (ev) {
+            ev.preventDefault();
+            var v = CKEDITOR.instances['editor'].getData();
+            $("#" + name).val(v);
+            $("#" + name + "_ro").html(v);
+            CKEDITOR.instances['editor'].setData('');
+            $('#EditorDialog').hide("close");
+            dimOff();
+            return false;
+        });
+        return false;
+    });
+
+    $('body').on('click', '#canceledit', function (ev) {
+        ev.preventDefault();
+        $('#EditorDialog').hide("close");
+        dimOff();
+        return false;
+    });
+});
+
+CKEDITOR.on('dialogDefinition', function (ev) {
+    var dialogName = ev.data.name;
+    var dialogDefinition = ev.data.definition;
+    if (dialogName == 'link') {
+        var advancedTab = dialogDefinition.getContents('advanced');
+	    advancedTab.label = "SpecialLinks";
+        advancedTab.remove('advCSSClasses');
+        advancedTab.remove('advCharset');
+        advancedTab.remove('advContentType');
+        advancedTab.remove('advStyles');
+        advancedTab.remove('advAccessKey');
+        advancedTab.remove('advName');
+        advancedTab.remove('advId');
+        advancedTab.remove('advTabIndex');
+
+        var relField = advancedTab.get('advRel');
+        relField.label = "Sub-Group";
+        var titleField = advancedTab.get('advTitle');
+        titleField.label = "Message";
+        var idField = advancedTab.get('advLangCode');
+        idField.label = "OrgId/MeetingId";
+        var langdirField = advancedTab.get('advLangDir');
+        langdirField.label = "Confirmation";
+	    langdirField.items[1][0] = "Yes, send confirmation";
+	    langdirField.items[2][0] = "No, do not send confirmation";
+    }
+});
