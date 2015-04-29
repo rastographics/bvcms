bcb707d52e973502494477400d6a9f30cd192bb5
David Mohundro <david@mohundro.com>
2015-04-29 11:00:22 -0500

    Add restructuring option for cleancss gulp command

--- CmsWeb/gulpfile.js
+++ CmsWeb/gulpfile.js
@@ -1,47 +1,67 @@
 var gulp = require('gulp'),
-  less = require('gulp-less'),
-  minify = require('gulp-minify-css'),
-  sourcemaps = require('gulp-sourcemaps');
+    less = require('gulp-less'),
+    minify = require('gulp-minify-css'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
 
-var cssOutput = 'content/touchpoint/css';
-var select2Output = 'content/touchpoint/lib/select2/css';
+var cssOutput = 'content/touchpoint/css',
+    select2Output = 'content/touchpoint/lib/select2/css',
+    jsOutput = 'content/touchpoint/js';
 
-gulp.task('default', function() {
-  // place code here
+gulp.task('default', ['less', 'compress-js'], function() {
+    // place code here
 });
 
 gulp.task('less', function() {
-    gulp.src('content/touchpoint/src/less/app.less')
-        .pipe(less())
-        .pipe(minify())
-        .pipe(gulp.dest(cssOutput));
-    gulp.src('content/touchpoint/src/less/account.less')
-        .pipe(less())
-        .pipe(minify())
-        .pipe(gulp.dest(cssOutput));
-    gulp.src('content/touchpoint/src/less/error.less')
-        .pipe(less())
-        .pipe(minify())
-        .pipe(gulp.dest(cssOutput));
-    gulp.src('content/touchpoint/src/less/print.less')
-        .pipe(less())
-        .pipe(minify())
-        .pipe(gulp.dest(cssOutput));
-    gulp.src('content/touchpoint/src/less/editor.less')
-        .pipe(less())
-        .pipe(minify())
-        .pipe(gulp.dest(cssOutput));
-    gulp.src('content/touchpoint/src/less/select2/select2-bootstrap.less')
-        .pipe(less())
-        .pipe(minify())
-        .pipe(gulp.dest(select2Output));
+    var handleLess = function (lessFile, output) {
+        gulp.src('content/touchpoint/src/less/' + lessFile)
+            .pipe(less())
+            .pipe(minify({restructuring: false}))
+            .pipe(gulp.dest(output));
+    };
+
+    ['app.less',
+    'account.less',
+    'error.less',
+    'print.less',
+    'editor.less'].map(function(lessFile) {
+        handleLess(lessFile, cssOutput);
+    });
+
+    handleLess('select2/select2-bootstrap.less', select2Output);
+});
+
+gulp.task('compress-js', function() {
+    var handleJs = function(filesToInclude, outputName) {
+        gulp.src('content/touchpoint/js/' + filesToInclude)
+            .pipe(concat(outputName))
+            .pipe(uglify())
+            .pipe(gulp.dest(jsOutput));
+    };
+
+    /*
+    ['admin',
+    'dialog',
+    'email',
+    'finance',
+    'meeting',
+    'org',
+    'people',
+    'search',
+    'support'].map(function(folder) {
+        handleJs(folder + '/*.js', folder + '.min.js');
+    });
+
+    handleJs('*.js', 'app.min.js');
+    */
 });
 
 gulp.task('less-sourcemaps', function() {
     gulp.src('content/touchpoint/src/less/app.less')
         .pipe(sourcemaps.init())
         .pipe(less())
-        .pipe(minify())
+        .pipe(minify({restructuring: false}))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(cssOutput));
 });
