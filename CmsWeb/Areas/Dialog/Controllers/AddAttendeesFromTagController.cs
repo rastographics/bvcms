ac8c368750c7a58856584b5e4a67f66eaf8052e7
David Carroll <david@bvcms.com>
2015-04-29 21:55:37 -0500

    AddAttendeesFromTag

--- CmsWeb/Areas/Dialog/Controllers/AddAttendeesFromTagController.cs
+++ CmsWeb/Areas/Dialog/Controllers/AddAttendeesFromTagController.cs
@@ -1,87 +1,39 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using CmsData;
+using CmsWeb.Areas.Dialog.Models;
 using UtilityExtensions;
-using CmsData.Codes;
-using CmsWeb.Code;
 
 namespace CmsWeb.Areas.Dialog.Controllers
 {
-	[Authorize(Roles = "Edit")]
-    [RouteArea("Dialog", AreaPrefix= "AddAttendeesFromTag"), Route("{action}/{id:int}")]
-	public class AddAttendeesFromTagController : CmsController
-	{
-        [Route("~/AddAttendeesFromTag/{id:int}")]
-		public ActionResult Index(int id)
-		{
-			ViewBag.tag = CodeValueModel.Tags();
-			ViewBag.meetingid = id;
-			return View();
+    [RouteArea("Dialog", AreaPrefix="AddAttendeesFromTag"), Route("{action}/{id?}")]
+    public class AddAttendeesFromTagController : CmsStaffController
+    {
+        [HttpPost, Route("~/AddAttendeesFromTag/{id:int}")]
+        public ActionResult Index(int id)
+        {
+            var model = new AddAttendeesFromTag(id);
+            model.RemoveExistingLop(DbUtil.Db, id, AddAttendeesFromTag.Op);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Process(AddAttendeesFromTag model)
+        {
+            model.Validate(ModelState);
+
+            if(!ModelState.IsValid) // show validation errors
+                return View("Index", model);
+
+            model.UpdateLongRunningOp(DbUtil.Db, AddAttendeesFromTag.Op);
+            if(model.ShowCount(DbUtil.Db))
+                return View("Index", model); // let them confirm by seeing the count and the tagname
+
+            if (!model.Started.HasValue)
+            { 
+                DbUtil.LogActivity("Add attendees from tag for {0}".Fmt(Session["ActiveOrganization"]));
+                model.Process(DbUtil.Db);
+            }
+
+			return View(model);
 		}
-		[HttpPost]
-        [Route("~/AddAttendeesFromTag/Start")]
-		public ActionResult Start(int tag, int meetingid, bool addasmembers)
-		{
-			var runningtotals = new AddToOrgFromTagRun
-			{
-				Started = DateTime.Now,
-				Count = 0,
-				Processed = 0,
-				Orgid = meetingid
-			};
-			DbUtil.Db.AddToOrgFromTagRuns.InsertOnSubmit(runningtotals);
-			DbUtil.Db.SubmitChanges();
-			var host = Util.Host;
-		    var qid = DbUtil.Db.FetchLastQuery().Id;
-			System.Threading.Tasks.Task.Factory.StartNew(() =>
-			{
-				System.Threading.Thread.CurrentThread.Priority = System.Threading.ThreadPriority.BelowNormal;
-				var Db = new CMSDataContext(Util.GetConnectionString(host));
-				IEnumerable<int> q = null;
-				if (tag == -1) // (last query)
-					q = Db.PeopleQuery(qid).Select(pp => pp.PeopleId);
-				else
-					q = from t in Db.TagPeople
-						where t.Id == tag
-						select t.PeopleId;
-				var pids = q.ToList();
-				var meeting = Db.Meetings.SingleOrDefault(mm => mm.MeetingId == meetingid);
-				var joindate = meeting.MeetingDate.Value.AddMinutes(-1);
-				var orgid = meeting.OrganizationId;
-				foreach (var pid in pids)
-				{
-					Db.Dispose();
-					Db = new CMSDataContext(Util.GetConnectionString(host));
-					if (addasmembers)
-						OrganizationMember.InsertOrgMembers(Db,
-							orgid, pid, MemberTypeCode.Member, joindate, null, false);
-					Db.RecordAttendance(meetingid, pid, true);
-					var r = Db.AddToOrgFromTagRuns.Where(mm => mm.Orgid == meetingid).OrderByDescending(mm => mm.Id).First();
-					r.Processed++;
-					r.Count = pids.Count;
-					Db.SubmitChanges();
-				}
-				var rr = Db.AddToOrgFromTagRuns.Where(mm => mm.Orgid == meetingid).OrderByDescending(mm => mm.Id).First();
-				rr.Completed = DateTime.Now;
-				Db.SubmitChanges();
-				Db.UpdateMainFellowship(orgid);
-			});
-			return Redirect("/AddAttendeesFromTag/Progress/" + meetingid);
-		}
-		[HttpPost]
-		public JsonResult Progress2(int id)
-		{
-			var r = DbUtil.Db.AddToOrgFromTagRuns.Where(mm => mm.Orgid == id).OrderByDescending(mm => mm.Id).First();
-			return Json(new { r.Count, r.Error, r.Processed, Completed = r.Completed.ToString(), r.Running });
-		}
-		[HttpGet]
-		public ActionResult Progress(int id)
-		{
-			ViewBag.meetingid = id;
-			var r = DbUtil.Db.AddToOrgFromTagRuns.Where(mm => mm.Orgid == id).OrderByDescending(mm => mm.Id).First();
-			return View(r);
-		}
-	}
+    }
 }
