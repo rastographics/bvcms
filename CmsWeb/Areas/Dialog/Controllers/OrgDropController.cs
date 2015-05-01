43609095ee2e92e06eac8e3e61f32677d737d8ff
David Carroll <david@bvcms.com>
2015-04-30 22:47:37 -0500

    OrgDrop separated from OrgMembersUpdate

---
+++ CmsWeb/Areas/Dialog/Controllers/OrgDropController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Web.Mvc;
+using CmsData;
+using CmsWeb.Areas.Dialog.Models;
+using UtilityExtensions;
+
+namespace CmsWeb.Areas.Dialog.Controllers
+{
+    [RouteArea("Dialog", AreaPrefix="OrgDrop"), Route("{action}/{id?}")]
+    public class OrgDropController : CmsStaffController
+    {
+        [HttpPost, Route("~/OrgDrop/{id:int}")]
+        public ActionResult Index(int id)
+        {
+            if (id != DbUtil.Db.CurrentOrgId0)
+                throw new Exception("Current org has changed from {0} to {1}, aborting".Fmt(id, DbUtil.Db.CurrentOrgId0));
+            var model = new OrgDrop(id);
+            model.RemoveExistingLop(DbUtil.Db, id, OrgDrop.Op);
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Process(OrgDrop model)
+        {
+            model.UpdateLongRunningOp(DbUtil.Db, OrgDrop.Op);
+            if (!model.Started.HasValue)
+            { 
+                DbUtil.LogActivity("Drop Memers from Org {0}".Fmt(Session["ActiveOrganization"]));
+                model.Process(DbUtil.Db);
+            }
+			return View(model);
+		}
+    }
+}
