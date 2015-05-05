4088f8e69ec3b631c645f3923c7344fbca358c2f
Brian Lankford <brianlankford1@gmail.com>
2015-05-05 10:26:05 -0500

    Fixed rollsheet and rally rollsheet report dialogs.

--- CmsWeb/Areas/Dialog/Controllers/NewMeetingController.cs
+++ CmsWeb/Areas/Dialog/Controllers/NewMeetingController.cs
@@ -25,7 +25,7 @@ namespace CmsWeb.Areas.Dialog.Controllers
             ViewBag.Method = "POST";
             return View("MeetingInfo", m);
         }
-        [HttpPost, Route("ForNewRollsheet/{orgid:int}")]
+        [HttpGet, Route("ForNewRollsheet/{orgid:int}")]
         public ActionResult ForNewRollsheet(int orgid)
         {
             var oi = new SettingsAttendanceModel() { Id = orgid };
@@ -39,7 +39,7 @@ namespace CmsWeb.Areas.Dialog.Controllers
             ViewBag.Method = "POST";
             return View("MeetingInfo", m);
         }
-        [HttpPost, Route("ForNewRallyRollsheet/{orgid:int}")]
+        [HttpGet, Route("ForNewRallyRollsheet/{orgid:int}")]
         public ActionResult ForNewRallyRollsheet(int orgid)
         {
             var oi = new SettingsAttendanceModel { Id = orgid };
