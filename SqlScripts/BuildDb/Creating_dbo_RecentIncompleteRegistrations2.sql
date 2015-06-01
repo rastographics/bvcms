5909efddb20fa11ca2d697ecd3159650dc07009e
David Carroll <david@bvcms.com>
2015-06-01 13:22:37 -0500

    DbSchema Change

--- SqlScripts/BuildDb/Creating_dbo_RecentIncompleteRegistrations2.sql
+++ SqlScripts/BuildDb/Creating_dbo_RecentIncompleteRegistrations2.sql
@@ -1,11 +1,7 @@
-
-CREATE FUNCTION [dbo].[RecentIncompleteRegistrations]
+CREATE FUNCTION [dbo].[RecentIncompleteRegistrations2]
 (	
-	@prog INT
-	,@div INT 
-	,@org INT 
-	,@begdt DATETIME
-	,@enddt DATETIME
+	@orgs VARCHAR(MAX) 
+	, @days INT
 )
 RETURNS 
 @t TABLE 
@@ -13,32 +9,24 @@ RETURNS
 	PeopleId INT,
 	OrgId INT,
 	DatumId INT,
-	Stamp DATETIME
+	Stamp DATETIME,
+	OrgName NVARCHAR(80),
+	Name NVARCHAR(80)
 )
 AS
 BEGIN
 	DECLARE @tt TABLE (oid INT, pid1 INT, pid2 INT, pid3 INT, pid4 INT, stamp DATETIME, completed BIT, datumid INT)
+	DECLARE @enddt DATETIME = GETDATE()
+	DECLARE @begdt DATETIME = DATEADD(DAY, -ISNULL(@days, 90), @enddt)
 
 	INSERT @tt ( oid , pid1 , pid2 , pid3 , pid4 , stamp , completed, datumid )
 	SELECT r.OrganizationId, PeopleId1, PeopleId2, PeopleId3, PeopleId4, Stamp, completed, Id
 	FROM dbo.RegistrationList r
 	JOIN dbo.Organizations o ON o.OrganizationId = r.OrganizationId
+	JOIN dbo.SplitInts(@orgs) s ON s.Value = o.OrganizationId
 	WHERE expired = 0
 	AND r.OrganizationId IS NOT NULL
 	
-	AND (ISNULL(@prog, 0) = 0
-		OR EXISTS(SELECT NULL 
-					FROM dbo.DivOrg di
-					JOIN dbo.ProgDiv pp ON pp.DivId = di.DivId
-					WHERE di.OrgId = o.OrganizationId AND pp.ProgId = @prog))
-					
-	AND (ISNULL(@div, 0) = 0
-		OR EXISTS(SELECT NULL 
-					FROM dbo.DivOrg dd
-					WHERE dd.OrgId = o.OrganizationId AND dd.DivId = @div))
-
-	AND (ISNULL(@org, 0) = 0 OR o.OrganizationId = @org)
-
 	;WITH registrations AS (
 		SELECT r.oid, r.pid1 pid, r.datumid, stamp, r.completed 
 		FROM @tt r
@@ -57,14 +45,16 @@ BEGIN
 		WHERE r.pid4 IS NOT NULL
 	), 
 	oidpids AS (
-		SELECT oid, pid, MAX(r.datumid) datumid, MAX(stamp) stamp
+		SELECT oid, pid, MAX(r.datumid) datumid, MAX(stamp) stamp, o.OrganizationName, p.Name2
 		FROM registrations r
-		GROUP BY oid, pid
+		JOIN dbo.Organizations o ON o.OrganizationId = r.oid
+		JOIN dbo.People p ON p.PeopleId = r.pid
+		GROUP BY oid, pid, o.OrganizationName, p.Name2
 		HAVING MAX(CAST(ISNULL(completed, 0) AS INT)) = 0
 	)
 	INSERT @t
-	        ( PeopleId, OrgId, DatumId, Stamp )
-	SELECT pid, oid, op.datumid, stamp
+	        ( PeopleId, OrgId, DatumId, Stamp, OrgName, Name )
+	SELECT pid, oid, op.datumid, stamp, op.OrganizationName, op.Name2
 	FROM oidpids op
 	LEFT JOIN dbo.OrganizationMembers om ON om.PeopleId = op.pid AND om.OrganizationId = op.oid
 	WHERE om.PeopleId IS NULL
