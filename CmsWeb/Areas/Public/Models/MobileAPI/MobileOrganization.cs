d88b8ef0d767d05de36c52365c0c64305309552a
Steven Yarbrough <steven@bvcms.com>
2015-06-15 16:16:12 -0500

    Added adjustment for taking attendance on Orgs that don't have a schedule

--- CmsWeb/Areas/Public/Models/MobileAPI/MobileOrganization.cs
+++ CmsWeb/Areas/Public/Models/MobileAPI/MobileOrganization.cs
@@ -5,44 +5,61 @@ using System.Web;
 
 namespace CmsWeb.MobileAPI
 {
-	public class MobileOrganization
-	{
-		public int id = 0;
+    public class MobileOrganization
+    {
+        public int id = 0;
 
-		public string name { get; set; }
+        public string name { get; set; }
 
-		public DateTime datetime { get; set; }
+        public DateTime? datetime { get; set; }
 
-		public MobileOrganization populate(OrganizationInfo oi)
-		{
-			id = oi.id;
-			name = oi.name;
+        public MobileOrganization populate(OrganizationInfo oi)
+        {
+            id = oi.id;
+            name = oi.name;
 
-			datetime = createOrgDateTime(oi.time, oi.day);
-			return this;
-		}
+            if (oi.time != null && oi.day != null)
+            {
+                datetime = createOrgDateTime(oi.time.Value, oi.day.Value);
+            }
+            else if (oi.lastMeetting != null && oi.lastMeetting.Value > DateTime.Today.AddDays(-7))
+            {
+                datetime = oi.lastMeetting;
+            }
 
-		public DateTime createOrgDateTime(DateTime time, int day)
-		{
-			DateTime dt = DateTime.Today.AddDays(-(int)DateTime.Today.DayOfWeek).AddDays(day).AddHours(time.Hour).AddMinutes(time.Minute);
+            return this;
+        }
 
-			if (dt.Date > DateTime.Today)
-				dt = dt.AddDays(-7);
+        public DateTime createOrgDateTime(DateTime time, int day)
+        {
+            DateTime dt = DateTime.Today.AddDays(-(int)DateTime.Today.DayOfWeek).AddDays(day).AddHours(time.Hour).AddMinutes(time.Minute);
 
-			return dt;
-		}
+            if (dt.Date > DateTime.Today)
+                dt = dt.AddDays(-7);
 
-		public void changeHourOffset(int offset)
-		{
-			datetime = datetime.AddHours(offset);
-		}
-	}
+            return dt;
+        }
 
-	public class OrganizationInfo
-	{
-		public int id { get; set; }
-		public string name { get; set; }
-		public DateTime time { get; set; }
-		public int day { get; set; }
-	}
+        public void changeHourOffset(int offset)
+        {
+            if (datetime != null)
+            {
+                datetime = datetime.Value.AddHours(offset);
+            }
+        }
+
+        public bool hasInvalidDate()
+        {
+            return datetime == null;
+        }
+    }
+
+    public class OrganizationInfo
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime? time { get; set; }
+        public int? day { get; set; }
+        public DateTime? lastMeetting { get; set; }
+    }
 }
