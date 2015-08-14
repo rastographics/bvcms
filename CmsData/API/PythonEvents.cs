79a8cc7a8f1e3e6ae350f44619c5d4a15890112e
David Carroll <david@bvcms.com>
2015-08-14 08:26:21 -0500

    Upload excel to dropbox

--- CmsData/API/PythonEvents.cs
+++ CmsData/API/PythonEvents.cs
@@ -7,7 +7,6 @@ using UtilityExtensions;
 using IronPython.Hosting;
 using System;
 using System.Configuration;
-using System.Data;
 using System.Diagnostics;
 using System.Net;
 using System.Text;
@@ -15,7 +14,6 @@ using System.Text.RegularExpressions;
 using CmsData.API;
 using Dapper;
 using Microsoft.Scripting.Hosting;
-using OfficeOpenXml;
 
 namespace CmsData
 {
@@ -666,7 +664,7 @@ namespace CmsData
             return s.FmtFone(prefix);
         }
 
-        public string UploadExcelFromSqlToFtp(string sqlscript, string username, string password, string filename, string targetpath)
+        public string UploadExcelFromSqlToFtp(string sqlscript, string username, string password, string targetpath, string filename)
         {
             var script = db.Content(sqlscript, "");
             if (!script.HasValue())
@@ -679,6 +677,23 @@ namespace CmsData
                 webClient.UploadData(url, bytes);
             }
             return url;
+        }
+        public string UploadExcelFromSqlToDropBox(string sqlscript, string targetpath, string filename)
+        {
+            var usertoken = ConfigurationManager.AppSettings["DropBoxUserToken"];
+            var url = ConfigurationManager.AppSettings["DropBoxUrl"];
+
+            var script = db.Content(sqlscript, "");
+            if (!script.HasValue())
+                throw new Exception("no sql script found");
+            var bytes = db.Connection.ExecuteReader(script).ToExcelBytes(filename);
+
+            var wc = new WebClient();
+            wc.Headers.Add($"Authorization: Bearer {usertoken}");
+            wc.Headers.Add("Content-Type: application/octet-stream");
+            wc.Headers.Add($@"Dropbox-API-Arg: {{""path"":""{targetpath}/{filename}""}}");
+            wc.UploadData("https://content.dropboxapi.com/2-beta-2/files/upload", bytes);
+            return $"{url}/{targetpath}/{filename}";
         }
         public string UploadExcelFromSqlToRackspace(string sqlscript, string filename)
         {
