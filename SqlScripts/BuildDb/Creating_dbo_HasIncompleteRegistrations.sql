30c779a860452f00a447e30114ae89b90bd1fc58
David Carroll <david@bvcms.com>
2015-11-16 22:09:12 -0600

    DbSchema Change RecentIncompleteRegistrations bug if a record has been merged

--- SqlScripts/BuildDb/Creating_dbo_HasIncompleteRegistrations.sql
+++ SqlScripts/BuildDb/Creating_dbo_HasIncompleteRegistrations.sql
@@ -11,8 +11,10 @@ AS
 RETURN 
 (
 	SELECT DISTINCT  PeopleId
-	FROM dbo.RecentIncompleteRegistrations(@prog, @div, @org, @begdt, @enddt)
+	FROM dbo.RecentIncompleteRegistrations(@prog, @div, @org, @begdt, @enddt) r
+	WHERE EXISTS(SELECT NULL FROM dbo.People WHERE PeopleId = r.PeopleId)
 )
+
 GO
 IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
 GO
