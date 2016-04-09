c217259fc5e64bc4288a6f8fe1e70821993d2132
David Carroll <david@bvcms.com>
2016-04-09 16:56:48 -0500

    DbSchema Change
    RepairEnrollmentTransactions

--- SqlScripts/BuildDb/Creating_dbo_RepairEnrollmentTransactions.sql
+++ SqlScripts/BuildDb/Creating_dbo_RepairEnrollmentTransactions.sql
@@ -5,13 +5,13 @@ BEGIN
 	INSERT @t (id, typ, dt, pid)
 	SELECT TransactionId, TransactionTypeId, TransactionDate, PeopleId FROM dbo.EnrollmentTransaction WHERE OrganizationId = @oid
 
-
 	UPDATE dbo.EnrollmentTransaction
 	SET NextTranChangeDate = (SELECT TOP 1 dt FROM @t WHERE e.TransactionTypeId <= 3 AND pid = e.PeopleId AND dt > e.TransactionDate ORDER BY dt) 
 	  , EnrollmentTransactionId = (SELECT TOP 1 id FROM @t WHERE typ <= 2 AND pid = e.PeopleId AND dt < e.TransactionDate AND e.TransactionTypeId >= 3 ORDER BY dt DESC) 
 	FROM dbo.EnrollmentTransaction e
-	WHERE OrganizationId = @oid
+	WHERE OrganizationId = @oid AND e.TransactionId = TransactionId
 END
+
 GO
 IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
 GO
