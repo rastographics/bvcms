d102b1a49ce9c4e20228d00474cc366b8e1a4644
David Carroll <david@bvcms.com>
2016-04-11 16:47:05 -0500

    DbSchema Change
    for RegistrationSmallGroups function

---
+++ SqlScripts/BuildDb/Creating_dbo_RegistrationSmallGroups.sql
@@ -0,0 +1,25 @@
+CREATE FUNCTION [dbo].[RegistrationSmallGroups](@orgid int)
+RETURNS TABLE 
+AS
+RETURN 
+(
+	WITH groups AS (
+		SELECT o.OrganizationId, x.value('(.)[1]', 'varchar(100)') SmallGroup
+		FROM    dbo.Organizations o
+		CROSS APPLY o.RegSettingXml.nodes('/Settings/AskItems//SmallGroup') tt ( x )
+		WHERE @orgid IS NULL OR o.OrganizationId = @orgid
+	), summary AS (
+		SELECT groups.OrganizationId, SmallGroup, COUNT(*) Cnt
+		FROM groups
+		WHERE groups.SmallGroup NOT IN ('nocheckbox', 'comment')
+		GROUP BY groups.OrganizationId, groups.SmallGroup
+	)
+	SELECT o.OrganizationId, o.OrganizationName, s.SmallGroup, s.Cnt
+	FROM summary s
+	JOIN dbo.Organizations o ON o.OrganizationId = s.OrganizationId
+)
+GO
+IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
+GO
+IF @@TRANCOUNT=0 BEGIN INSERT INTO #tmpErrors (Error) SELECT 1 BEGIN TRANSACTION END
+GO
