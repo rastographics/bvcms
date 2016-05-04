8311c8239ee15a80dc4aa265b3c8d3db45373c72
Thorne Melcher <thorne@clearfunction.com>
2016-05-04 17:50:09 -0400

    Basic list functionality on members only page.

--- CmsWeb/Areas/Public/Models/OrgContentInfo.cs
+++ CmsWeb/Areas/Public/Models/OrgContentInfo.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
@@ -128,6 +129,12 @@ namespace CmsWeb.Models
                 o.IsLeader = true;
             }
             return o;
+        }
+
+        public List<OrganizationMember> GetMemberList()
+        {
+            var org = DbUtil.Db.LoadOrganizationById(OrgId);
+            return org.OrganizationMembers.ToList();
         }
     }
 }
