22f5d142ac9490fa61064180db4d7160b7435186
David Carroll <david@bvcms.com>
2016-08-08 07:25:28 -0500

    DbSchema Change
    For EmailQueueTo DomainFrom field

--- SqlScripts/BuildDb/Adding_foreignkeystodbo_Resource.sql
+++ SqlScripts/BuildDb/Adding_foreignkeystodbo_Resource.sql
@@ -1,8 +1,8 @@
 ALTER TABLE [dbo].[Resource] ADD CONSTRAINT [FK_Resource_Division] FOREIGN KEY ([DivisionId]) REFERENCES [dbo].[Division] ([Id])
 ALTER TABLE [dbo].[Resource] ADD CONSTRAINT [FK_Resource_Organization] FOREIGN KEY ([OrganizationId]) REFERENCES [dbo].[Organizations] ([OrganizationId])
-ALTER TABLE [dbo].[Resource] ADD CONSTRAINT [FK_Resource_ResourceCategory] FOREIGN KEY ([ResourceCategoryId]) REFERENCES [dbo].[ResourceCategory] ([ResourceCategoryId])
-ALTER TABLE [dbo].[Resource] ADD CONSTRAINT [FK_Resource_ResourceType] FOREIGN KEY ([ResourceTypeId]) REFERENCES [dbo].[ResourceType] ([ResourceTypeId])
 ALTER TABLE [dbo].[Resource] ADD CONSTRAINT [FK_Resource_Campus] FOREIGN KEY ([CampusId]) REFERENCES [lookup].[Campus] ([Id])
+ALTER TABLE [dbo].[Resource] ADD CONSTRAINT [FK_Resource_ResourceType] FOREIGN KEY ([ResourceTypeId]) REFERENCES [dbo].[ResourceType] ([ResourceTypeId])
+ALTER TABLE [dbo].[Resource] ADD CONSTRAINT [FK_Resource_ResourceCategory] FOREIGN KEY ([ResourceCategoryId]) REFERENCES [dbo].[ResourceCategory] ([ResourceCategoryId])
 ALTER TABLE [dbo].[Resource] ADD CONSTRAINT [FK_Resource_OrganizationType] FOREIGN KEY ([OrganizationTypeId]) REFERENCES [lookup].[OrganizationType] ([Id])
 GO
 IF @@ERROR<>0 AND @@TRANCOUNT>0 ROLLBACK TRANSACTION
