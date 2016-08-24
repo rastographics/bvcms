a8064babe66ea2c7c751fac436282d3919e6e3e4
David Carroll <david@bvcms.com>
2016-08-24 17:03:35 -0500

    Ability to AddEdit ExtraValues on Org

--- CmsData/API/PythonModel/ExtraValuesOrg.cs
+++ CmsData/API/PythonModel/ExtraValuesOrg.cs
@@ -1,10 +1,58 @@
 ﻿using System;
+using System.Linq;
 using UtilityExtensions;
 
 namespace CmsData
 {
     public partial class PythonModel
     {
+        public void AddExtraValueBoolOrg(int oid, string name, bool b)
+        {
+            var db2 = NewDataContext();
+            var o = db2.LoadOrganizationById(oid);
+            o.AddEditExtraBool(name, b);
+            db2.SubmitChanges();
+            db2.Dispose();
+        }
+
+        public void AddExtraValueCodeOrg(int oid, string name, string text)
+        {
+            var db2 = NewDataContext();
+            var o = db2.LoadOrganizationById(oid);
+            o.AddEditExtraCode(name, text);
+            db2.SubmitChanges();
+            db2.Dispose();
+        }
+
+        public void AddExtraValueDateOrg(int oid, string name, object dt)
+        {
+            var dt2 = dt.ToDate();
+            if (!dt2.HasValue)
+                return;
+            var db2 = NewDataContext();
+            var o = db2.LoadOrganizationById(oid);
+                o.AddEditExtraDate(name, dt2.Value);
+            db2.SubmitChanges();
+            db2.Dispose();
+        }
+
+        public void AddExtraValueIntOrg(int oid, string name, int n)
+        {
+            var db2 = NewDataContext();
+            var o = db2.LoadOrganizationById(oid);
+            o.AddEditExtraInt(name, n);
+            db2.SubmitChanges();
+            db2.Dispose();
+        }
+
+        public void AddExtraValueTextOrg(int oid, string name, string text)
+        {
+            var db2 = NewDataContext();
+            var o = db2.LoadOrganizationById(oid);
+            o.AddEditExtraText(name, text);
+            db2.SubmitChanges();
+            db2.Dispose();
+        }
         public bool ExtraValueBitOrg(int oid, string name)
         {
             var ev = Organization.GetExtraValue(db, oid, name);
