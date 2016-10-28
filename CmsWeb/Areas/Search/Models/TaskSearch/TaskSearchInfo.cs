a8f73c6e5e6d80f4954b0be1ed04a54bc6e4550a
David Carroll <david@bvcms.com>
2016-10-28 12:03:28 -0500

    Working on Task Project

--- CmsWeb/Areas/Search/Models/TaskSearch/TaskSearchInfo.cs
+++ CmsWeb/Areas/Search/Models/TaskSearch/TaskSearchInfo.cs
@@ -6,24 +6,13 @@ namespace CmsWeb.Areas.Search.Models
     [Serializable]
     public class TaskSearchInfo
     {
-        public string ContacteeName { get; set; }
-        public string ContactorName { get; set; }
-        public DateTime? StartDate { get; set; }
-        public DateTime? EndDate { get; set; }
-        public CodeInfo ContactType { get; set; }
-        public CodeInfo ContactReason { get; set; }
-        public CodeInfo Ministry { get; set; }
-        public CodeInfo ContactResult { get; set; }
-        public string CreatedBy { get; set; }
-        public bool Incomplete { get; set; }
-        public bool Private { get; set; }
+        public string Delegate { get; set; }
+        public string Originator { get; set; }
+        public string Owner { get; set; }
+        public string About { get; set; }
+        public bool Archived { get; set; }
+        public int Status { get; set; }
+        public DateTime Created { get; set; }
 
-        public TaskSearchInfo()
-        {
-            ContactType = new CodeInfo("ContactType");
-            ContactReason = new CodeInfo("ContactReason");
-            Ministry = new CodeInfo("Ministry");
-            ContactResult = new CodeInfo("ContactResult");
-        }
     }
 }
