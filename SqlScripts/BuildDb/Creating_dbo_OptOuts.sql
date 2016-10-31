c459ea487ae8cd86ce12a5dcef680fc00d440c7a
David Carroll <david@bvcms.com>
2016-10-31 15:44:47 -0500

    DbSchema Change
    TaskSearch and OrgPeopleGuests

--- SqlScripts/BuildDb/Creating_dbo_OptOuts.sql
+++ SqlScripts/BuildDb/Creating_dbo_OptOuts.sql
@@ -17,8 +17,10 @@ RETURN
 	JOIN dbo.Families f ON f.FamilyId = p.FamilyId
 	LEFT JOIN dbo.People h1 ON h1.PeopleId = f.HeadOfHouseholdId AND p.PositionInFamilyId = 30
 		AND NOT EXISTS(SELECT NULL FROM dbo.EmailOptOut WHERE ToPeopleId = h1.PeopleId AND FromEmail = @fromemail)
+		AND isnull(h1.SendEmailAddress1, 1) = 1
 	LEFT JOIN dbo.People h2 ON h2.PeopleId = f.HeadOfHouseholdSpouseId AND p.PositionInFamilyId = 30
 		AND NOT EXISTS(SELECT NULL FROM dbo.EmailOptOut WHERE ToPeopleId = h2.PeopleId AND FromEmail = @fromemail)
+		AND isnull(h2.SendEmailAddress1, 1) = 1
 	WHERE (@queueid = 0 OR EXISTS(SELECT NULL FROM dbo.EmailQueueTo WHERE PeopleId = p.PeopleId AND Id = @queueid))
 )
 GO
---
+++ SqlScripts/BuildDb/Creating_dbo_TaskSearch.sql
@@ -0,0 +1,34 @@
+CREATE VIEW [dbo].[TaskSearch] AS  
+SELECT 
+    Created = CONVERT(DATE, t.CreatedOn) , 
+    Status = ts.Description , 
+    Due = CONVERT(DATE, t.Due) , 
+    Completed = CONVERT(DATE, t.CompletedOn) , 
+    t.Archive , 
+    t.Notes , 
+    t.Description , 
+    t.ForceCompleteWContact , 
+    t.DeclineReason , 
+    t.LimitToRole , 
+	Originator = og.Name, 
+	Owner = op.Name, 
+	Delegate = de.Name, 
+	About = ab.Name, 
+ 
+	t.Id , 
+    t.StatusId , 
+    t.OwnerId , 
+    t.CoOwnerId , 
+    t.OrginatorId , 
+    t.WhoId , 
+    t.SourceContactId , 
+    t.CompletedContactId 
+FROM dbo.Task t 
+JOIN lookup.TaskStatus ts ON ts.Id = t.StatusId 
+JOIN dbo.People op ON op.PeopleId = t.OwnerId 
+LEFT JOIN dbo.People de ON de.PeopleId = t.CoOwnerId 
+LEFT JOIN dbo.People ab ON ab.PeopleId = t.WhoId 
+LEFT JOIN dbo.People og ON og.PeopleId = t.OrginatorId 
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
