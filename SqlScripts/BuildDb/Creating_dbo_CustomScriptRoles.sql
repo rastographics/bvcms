3f3ad3c92632b2c419307eb0f56e2d317749cbb1
David Carroll <david@bvcms.com>
2016-11-01 15:51:20 -0500

    DbSchema Change
    CustomScriptRoles

--- SqlScripts/BuildDb/Creating_dbo_CustomScriptRoles.sql
+++ SqlScripts/BuildDb/Creating_dbo_CustomScriptRoles.sql
@@ -1,32 +1,34 @@
-CREATE VIEW [dbo].[CustomScriptRoles] AS 
-WITH body AS (
-	SELECT c.Body, CONVERT(XML, c.Body) b 
-	FROM dbo.Content c
-	WHERE c.Name = 'CustomReports'
-), tbl AS (
-	SELECT x.value('(@name)[1]', 'varchar(100)') Name
-			,x.value('(@type)[1]', 'varchar(100)') [Type]
-			,x.value('(@role)[1]', 'varchar(100)') [Role]
-			,x.value('(@showOnOrgId)[1]', 'int') [ShowOnOrgId]
-	FROM body 
-	CROSS APPLY b.nodes('//Report') tt (x)
-), reports AS (
-	SELECT tbl.Name
-          ,ISNULL(tbl.[Type], 'Custom') [Type]
-		  ,tbl.ShowOnOrgId
-		  ,CASE WHEN tbl.Type IS NULL THEN tbl.Role ELSE REPLACE(REPLACE(dbo.RegexMatch(ISNULL(s.Body, ''), '(?<=^(#|--)roles=)(.*)$'),CHAR(10), ''),CHAR(13), '') END [Role]
-		  ,dbo.RegexMatch(ISNULL(s.Body, ''), '(?<=^(#|--)class=)(.*)$') [class]
-	FROM tbl
-	LEFT JOIN dbo.Content s ON s.Name = tbl.name
-
-)
-SELECT r.Name
-      ,r.[Type]
-      ,r.[Role]
-	  ,r.ShowOnOrgId
-	  ,r.class
-FROM reports r
-
+ 
+CREATE VIEW [dbo].[CustomScriptRoles] AS  
+WITH body AS ( 
+	SELECT c.Body, CONVERT(XML, c.Body) b  
+	FROM dbo.Content c 
+	WHERE c.Name = 'CustomReports' 
+), tbl AS ( 
+	SELECT x.value('(@name)[1]', 'varchar(100)') Name 
+			,x.value('(@type)[1]', 'varchar(100)') [Type] 
+			,x.value('(@role)[1]', 'varchar(100)') [Role] 
+			,x.value('(@showOnOrgId)[1]', 'int') [ShowOnOrgId] 
+			,x.value('(@url)[1]', 'varchar(200)') [Url] 
+	FROM body  
+	CROSS APPLY b.nodes('//Report') tt (x) 
+), reports AS ( 
+	SELECT tbl.Name 
+          ,ISNULL(tbl.[Type], 'Custom') [Type] 
+		  ,tbl.ShowOnOrgId 
+		  ,tbl.[Url] 
+		  ,IIF(tbl.Role IS NOT NULL, tbl.Role, IIF(tbl.Type IS NULL, tbl.Role, REPLACE(REPLACE(dbo.RegexMatch(ISNULL(s.Body, ''), '(?<=^(#|--)roles=)(.*)$'),CHAR(10), ''),CHAR(13), ''))) [Role] 
+		  ,dbo.RegexMatch(ISNULL(s.Body, ''), '(?<=^(#|--)class=)(.*)$') [class] 
+	FROM tbl 
+	LEFT JOIN dbo.Content s ON s.Name = tbl.name 
+) 
+SELECT r.Name 
+      ,r.[Type] 
+      ,r.[Role] 
+	  ,r.ShowOnOrgId 
+	  ,r.class 
+	  ,r.[Url] 
+FROM reports r 
 GO
 IF @@ERROR <> 0 SET NOEXEC ON
 GO
