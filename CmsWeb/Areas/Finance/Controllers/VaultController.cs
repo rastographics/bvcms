50600aa3ecb0c027c6ec3ad0b4b53df32b40277f
David Carroll <david@bvcms.com>
2016-11-02 10:16:31 -0500

    Allow Finance to delete ManagedGiving

--- CmsWeb/Areas/Finance/Controllers/VaultController.cs
+++ CmsWeb/Areas/Finance/Controllers/VaultController.cs
@@ -3,7 +3,7 @@ using CmsWeb.Areas.OnlineReg.Models;
 
 namespace CmsWeb.Areas.Finance.Controllers
 {
-    [Authorize(Roles = "Admin")]
+    [Authorize(Roles = "Admin,Finance")]
     [RouteArea("Finance", AreaPrefix= "Vault"), Route("{action}/{id?}")]
     public class VaultController : CmsController
     {
