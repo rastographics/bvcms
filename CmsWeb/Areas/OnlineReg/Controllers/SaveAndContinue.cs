46655f8c8dae6cd192c522cdfd0d4b241701252e
David Carroll <david@bvcms.com>
2016-11-15 07:23:56 -0600

    Disallow AllowSaveProgress when ComputeOrgByAge

--- CmsWeb/Areas/OnlineReg/Controllers/SaveAndContinue.cs
+++ CmsWeb/Areas/OnlineReg/Controllers/SaveAndContinue.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Web.Mvc;
 using CmsData;
+using CmsData.Codes;
 using CmsWeb.Areas.OnlineReg.Models;
 using UtilityExtensions;
 
