9fa247eeb8e237fb7adde54ff7ef7e45193759f8
David Carroll <david@bvcms.com>
2017-03-27 09:20:01 -0500

    DbSchema Change PledgeBalances view modification

--- SqlScripts/BuildDb/Creating_dbo_PledgeBalances.sql
+++ SqlScripts/BuildDb/Creating_dbo_PledgeBalances.sql
@@ -5,16 +5,18 @@ RETURN
 (
 	SELECT
 		c.CreditGiverId
-		,c.SpouseId
+		,SpouseId = c.CreditGiverId2
 		,SUM(PledgeAmount) PledgeAmt
+		,SUM(ISNULL(Amount, 0)) GivenAmt
 		,CASE WHEN SUM(ISNULL(c.PledgeAmount, 0)) > 0 
 				THEN SUM(ISNULL(PledgeAmount, 0)) - SUM(ISNULL(Amount, 0))
 				ELSE 0
 		 END Balance
 	FROM Contributions2('1/1/1', '1/1/3000', 0, NULL, NULL, 1) c
 	WHERE c.FundId = @fundid
-	GROUP BY c.CreditGiverId, c.SpouseId
+	GROUP BY c.CreditGiverId, c.CreditGiverId2
 )
+
 GO
 IF @@ERROR <> 0 SET NOEXEC ON
 GO
