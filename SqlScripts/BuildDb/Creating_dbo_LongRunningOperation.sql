366351a5d38d96ca9d39bc84a5acf6c22a8aff4f
David Carroll <david@bvcms.com>
2017-04-25 13:21:02 -0500

    DbSchema Change
    new LongRunningOperation table for CurrentOrgProject

---
+++ SqlScripts/BuildDb/Creating_dbo_LongRunningOperation.sql
@@ -0,0 +1,14 @@
+CREATE TABLE [dbo].[LongRunningOperation]
+(
+[QueryId] [uniqueidentifier] NOT NULL,
+[operation] [nvarchar] (25) NULL,
+[started] [datetime] NULL,
+[count] [int] NULL,
+[processed] [int] NULL,
+[completed] [datetime] NULL,
+[ElapsedTime] AS (substring(CONVERT([varchar],[completed]-[started],(121)),(12),(20))),
+[CustomMessage] [nvarchar] (80) NULL
+) ON [PRIMARY]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
---
+++ SqlScripts/BuildDb/Creating_pk_PK_LongRunningOperation_dbo_LongRunningOperation.sql
@@ -0,0 +1,4 @@
+ALTER TABLE [dbo].[LongRunningOperation] ADD CONSTRAINT [PK_LongRunningOperation] PRIMARY KEY CLUSTERED  ([QueryId]) ON [PRIMARY]
+GO
+IF @@ERROR <> 0 SET NOEXEC ON
+GO
