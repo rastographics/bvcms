83d1a3f104f6eead1b4337036d6cdf433b3a6a62
David Carroll <david@bvcms.com>
2017-07-12 09:34:44 -0500

    Meetings for Month should use HeadCount if > NumPresent

--- SqlScripts/BuildDb/Creating_dbo_MeetingsDataForDateRange.sql
+++ SqlScripts/BuildDb/Creating_dbo_MeetingsDataForDateRange.sql
@@ -13,13 +13,14 @@ RETURN
 		o.OrganizationName [Organization]
 		, o.LeaderName
 	    , dbo.SundayForDate(m.MeetingDate) dt
-		, SUM(m.NumPresent) cnt
+		, SUM(m.MaxCount) cnt
 	FROM dbo.Organizations o
 	JOIN dbo.Meetings m ON m.OrganizationId = o.OrganizationId
 	JOIN SplitInts(@orgs) i ON i.Value = o.OrganizationId
 	WHERE m.MeetingDate >= @startdate AND CAST(m.MeetingDate AS DATE) <= @enddate
 	GROUP BY o.OrganizationName, o.LeaderName, dbo.SundayForDate(m.MeetingDate)
 )
+
 GO
 IF @@ERROR <> 0 SET NOEXEC ON
 GO
