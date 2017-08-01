359dda1b2d694f5876226f3e5655ea9ec17e4853
David Carroll <david@bvcms.com>
2017-08-01 16:16:59 -0500

    Modify RecurringGivingDueForToday to exclude those with missing vault ids

--- SqlScripts/BuildDb/Creating_dbo_RecurringGivingDueForToday.sql
+++ SqlScripts/BuildDb/Creating_dbo_RecurringGivingDueForToday.sql
@@ -8,6 +8,17 @@ WITH amt AS (
 	WHERE f.FundStatusId = 1
 	AND f.OnlineSort IS NOT NULL
 	GROUP BY ra.PeopleId
+), VaultIds AS (
+	SELECT PeopleId
+		, VaultId = CASE 
+				WHEN s.Setting = 'Sage' AND PreferredGivingType = 'b' THEN CONVERT(VARCHAR(50), SageBankGuid)
+				WHEN s.Setting = 'Sage' AND PreferredGivingType = 'c' THEN CONVERT(VARCHAR(50), SageCardGuid)
+				WHEN s.Setting = 'AuthorizeNet' THEN CONVERT(VARCHAR(50), AuNetCustId)
+				WHEN s.Setting = 'Transnational' AND PreferredGivingType = 'b' THEN CONVERT(VARCHAR(50), TbnBankVaultId)
+				WHEN s.Setting = 'Transnational' AND PreferredGivingType = 'c' THEN CONVERT(VARCHAR(50), TbnCardVaultId)
+				END
+	FROM dbo.PaymentInfo
+	JOIN dbo.Setting s ON s.Id = 'TransactionGateway' AND NOT EXISTS(SELECT NULL FROM dbo.Setting WHERE id = 'TemporaryGatway')
 )
 SELECT 
 	mg.PeopleId
@@ -15,9 +26,11 @@ SELECT
 	,a.Amt
 FROM dbo.ManagedGiving mg
 JOIN amt a ON a.PeopleId = mg.PeopleId
+LEFT JOIN VaultIds v ON v.PeopleId = mg.PeopleId
 JOIN dbo.People p ON p.PeopleId = mg.PeopleId
 WHERE mg.NextDate = CONVERT(DATE, GETDATE())
 AND a.amt > 0
+AND (v.VaultId IS NOT NULL OR v.PeopleId = NULL)
 AND NOT EXISTS(
 	SELECT NULL 
 	FROM dbo.[Transaction] t 
