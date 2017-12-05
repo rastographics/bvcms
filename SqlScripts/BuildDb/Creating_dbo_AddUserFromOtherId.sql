73dcf31f5f7c0dc0715bd50a3956355b453700ff
David Carroll <david@bvcms.com>
2017-12-05 11:06:16 -0600

    Fix SQL procedure used for conversions

--- SqlScripts/BuildDb/Creating_dbo_AddUserFromOtherId.sql
+++ SqlScripts/BuildDb/Creating_dbo_AddUserFromOtherId.sql
@@ -1,24 +1,23 @@
 CREATE PROC [dbo].[AddUserFromOtherId](@name VARCHAR(50), @user VARCHAR(50), @password VARCHAR(100), @fieldname VARCHAR(50), @id INT, @roles VARCHAR(500))
 AS
 BEGIN
-	IF EXISTS(SELECT PeopleId FROM dbo.PeopleExtra WHERE Field = @fieldname AND IntValue = @id)
-	BEGIN
-		INSERT  INTO dbo.Users ( Username ,[Password] , TempPassword,Name ,PeopleId, MustChangePassword, IsApproved )
-		VALUES  (@user, 'thisisanencryptedpassword' ,@password ,@name
-		        ,( SELECT PeopleId FROM dbo.PeopleExtra
-		           WHERE Field = @fieldname AND IntValue = @id ), 1, 1)
+	IF NOT EXISTS(SELECT NULL FROM dbo.Users WHERE Username = @user)
+		IF EXISTS(SELECT PeopleId FROM dbo.PeopleExtra WHERE Field = @fieldname AND IntValue = @id)
+		BEGIN
+			INSERT  INTO dbo.Users ( Username ,[Password] , TempPassword,Name ,PeopleId, MustChangePassword, IsApproved )
+			VALUES  (@user, 'thisisanencryptedpassword' ,@password ,@name
+			        ,( SELECT PeopleId FROM dbo.PeopleExtra
+			           WHERE Field = @fieldname AND IntValue = @id ), 1, 1)
 
-		DECLARE @userid INT = @@IDENTITY
+			DECLARE @userid INT = @@IDENTITY
 
-		INSERT dbo.UserRole ( RoleId, UserId )
-		SELECT r.RoleId, @userid
-		FROM dbo.Split(@roles, ',') rr
-		JOIN dbo.Roles r ON r.RoleName = rr.Value
-	END
+			INSERT dbo.UserRole ( RoleId, UserId )
+			SELECT r.RoleId, @userid
+			FROM dbo.Split(@roles, ',') rr
+			JOIN dbo.Roles r ON r.RoleName = rr.Value
+		END
 
 END
-
-
 GO
 IF @@ERROR <> 0 SET NOEXEC ON
 GO
