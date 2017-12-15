768981d202aa238d1969287164e943d9a80da284
David Mohundro <drmohundro@gmail.com>
2017-12-15 15:38:12 -0600

    Change contact extra values to support config (#122)
    The earlier contact extra values support was based entirely on the three contact dropdowns; however, this was far more complicated, so instead, there is now a config file (i.e. ContactExtraLocationConfig.xml) that allows users to set up specific overrides. If those aren't specified, then the location is based on if the contact is tied to a person or org.
    
    See the unit tests for a brief overview of what is expected and how it should behave. The javascript code is essentially doing the same logic on the front end.

---
+++ UnitTests/Areas/People/Models/Contact/ContactExtraLocationConfigTests.cs
@@ -0,0 +1,104 @@
+﻿using CmsWeb.Areas.People.Models;
+using Shouldly;
+using Xunit;
+
+namespace UnitTests.Areas.People.Models.Contact
+{
+    public class ContactExtraLocationConfigTests
+    {
+        [Theory]
+        [InlineData(null, "redeemerwide", "cggroupvisit", "cghealthassessment", "redeemerwide-cggroupvisit-cghealthassessment")]
+        [InlineData(123, "redeemerwide", "cggroupvisit", "cghealthassessment", "redeemerwide-cggroupvisit-cghealthassessment")]
+        [InlineData(123, "what", "what", "what", "OrganizationStandard")]
+        [InlineData(null, "what", "what", "what", "PersonStandard")]
+        [InlineData(123, "wscommunitygroups", "what", "what", "wscommunitygroups--")]
+        [InlineData(123, "Not Match, but shows off partial matches", "This Is A Contact Type", "Test Reason", "-thisisacontacttype-testreason")]
+        public void Should_be_able_to_get_location(int? orgId, string ministry, string contactReason, string contactType, string expectedResult)
+        {
+            var config = new ContactExtraLocationConfig(ValidXmlContent());
+            var location = config.GetLocationFor(orgId, ministry, contactReason, contactType);
+
+            location.ShouldBe(expectedResult);
+        }
+
+        private static string ValidXmlContent()
+        {
+            return @"
+<ContactExtraLocations>
+    <Location>
+        <Ministry name=""redeemerwide"" />
+        <ContactType name=""cggroupvisit"" />
+        <ContactReason name=""cghealthassessment"" />
+    </Location>
+    <Location>
+        <Ministry name=""dtcommunitygroups"" />
+        <ContactType name=""cgvisit"" />
+        <ContactReason name=""cg"" />
+    </Location>
+    <Location>
+        <Ministry name=""wsgeneral"" />
+        <ContactType name=""cgvisit"" />
+        <ContactReason name=""cg"" />
+    </Location>
+    <Location>
+        <Ministry name=""dtgeneral"" />
+        <ContactType name=""phonecallmade"" />
+        <ContactReason name=""membership"" />
+    </Location>
+    <Location>
+        <Ministry name=""redeemerwide"" />
+        <ContactType name=""lettermailed"" />
+        <ContactReason name=""birthday"" />
+    </Location>
+    <Location>
+        <Ministry name=""notspecified"" />
+        <ContactType name=""emailsent"" />
+        <ContactReason name=""notspecified"" />
+    </Location>
+    <Location>
+        <Ministry name=""bvcmssupport"" />
+        <ContactType name=""cgvisit"" />
+        <ContactReason name=""cg"" />
+    </Location>
+    <Location>
+        <Ministry name=""cfw"" />
+        <ContactType name=""lettermailed"" />
+        <ContactReason name=""personal"" />
+    </Location>
+    <Location>
+        <Ministry name=""dtsst"" />
+        <ContactType name=""emailsent"" />
+        <ContactReason name=""baby"" />
+    </Location>
+    <Location>
+        <Ministry name=""cfw"" />
+        <ContactType name=""cgvisit"" />
+        <ContactReason name=""baby"" />
+    </Location>
+    <Location>
+        <Ministry name=""cfw"" />
+        <ContactType name=""lettermailed"" />
+        <ContactReason name=""bereavement"" />
+    </Location>
+    <Location>
+        <Ministry name=""escommunitygroups"" />
+        <ContactType name=""cggroupvisit"" />
+        <ContactReason name=""cghealthassessment"" />
+    </Location>
+    <Location>
+        <Ministry name=""wscommunitygroups"" />
+        <ContactType name=""cggroupvisit"" />
+        <ContactReason name=""cghealthassessment"" />
+    </Location>
+    <Location>
+        <Ministry name=""wscommunitygroups"" />
+    </Location>
+    <Location>
+        <ContactType name=""This Is A Contact Type"" />
+        <ContactReason name=""Test Reason"" />
+    </Location>
+</ContactExtraLocations>
+";
+        }
+    }
+}
