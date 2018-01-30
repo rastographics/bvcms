2daa78effc65ddd1b800562f680f70fef730f672
Keerp <32100635+Keerp@users.noreply.github.com>
2018-01-30 10:47:05 -0600

    Check Images updates. split check amounts, add new check images access from people record.

--- CmsWeb/Areas/People/Models/Person/Giving/ContributionInfo.cs
+++ CmsWeb/Areas/People/Models/Person/Giving/ContributionInfo.cs
@@ -6,6 +6,8 @@ namespace CmsWeb.Areas.People.Models
     {
         public DateTime Date { get; set; }
         public string CheckNo { get; set; }
+        public int ImageId { get; set; }
+        public int ContributionId { get; set; }
         public decimal Amount { get; set; }
         public string Fund { get; set; }
         public string Type { get; set; }
