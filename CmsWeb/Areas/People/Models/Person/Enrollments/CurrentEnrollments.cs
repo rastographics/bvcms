a60a0a6a364334cf2a682f29c5bcb3d57d4fc12d
David Carroll <david@bvcms.com>
2018-03-08 07:58:40 -0600

    Fix bug on the Sender = Inactive project

--- CmsWeb/Areas/People/Models/Person/Enrollments/CurrentEnrollments.cs
+++ CmsWeb/Areas/People/Models/Person/Enrollments/CurrentEnrollments.cs
@@ -139,8 +139,8 @@ namespace CmsWeb.Areas.People.Models
                        OrgType = om.OrgType,
                        HasDirectory = om.HasDirectory.GetValueOrDefault(),
                        IsLeaderAttendanceType = om.IsLeaderAttendanceType.GetValueOrDefault(),
-                       IsProspect = om.IsProspect == true,
-                       IsMissionTripOrg = om.IsMissionTripOrg == true
+                       IsProspect = om.IsProspect ?? false,
+                       IsMissionTripOrg = om.IsMissionTripOrg ?? false 
                    };
 
             if (DbUtil.Db.Setting("UX-ShowChildOrgsOnInvolvementTabs"))
