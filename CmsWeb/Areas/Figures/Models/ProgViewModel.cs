7ff2e0bf0d6181e716994d54b729420ba3f04541
Keerp <32100635+Keerp@users.noreply.github.com>
2018-07-12 15:33:17 -0500

    Dashboard feature.

---
+++ CmsWeb/Areas/Figures/Models/ProgViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace CmsWeb.Areas.Figures.Models
+{
+    public class ProgViewModel
+    {
+        public List<ProgModel> Proglist = new List<ProgModel>();
+        public Int32 SelectedProgId { get; set; }
+
+        public IEnumerable<SelectListItem> ProgIEnum
+        {
+            get
+            {
+                return new SelectList(Proglist, "ID", "ProgName");
+            }
+        }
+
+    }
+
+}
