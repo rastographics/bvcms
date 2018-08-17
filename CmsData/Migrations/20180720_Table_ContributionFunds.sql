3e0bc0b5f0aeffa72efc8f4af501bac5ce8cd942
Bron Davies <bron@brontech.com>
2018-08-17 10:30:50 -0500

    Update file encoding -> utf-8

--- CmsData/Migrations/20180720_Table_ContributionFunds.sql
+++ CmsData/Migrations/20180720_Table_ContributionFunds.sql
@@ -1,4 +1,4 @@
-IF NOT EXISTS(SELECT 1 FROM sys.columns 
+﻿IF NOT EXISTS(SELECT 1 FROM sys.columns 
           WHERE Name = N'FundManagerRoleId'
           AND Object_ID = Object_ID(N'dbo.ContributionFund'))
 BEGIN
