79a4a04a6b0fb1ce80ddbe1fe894df1d6635b8f2
Bron Davies <bron@brontech.com>
2018-08-29 09:34:22 -0500

    Add unique SystemFlag per SMS group trigger

--- CmsData/Migrations/20180827_sms-number-group-system-flag.sql
+++ CmsData/Migrations/20180827_sms-number-group-system-flag.sql
@@ -4,3 +4,30 @@
 BEGIN
     ALTER TABLE dbo.SMSGroups ADD SystemFlag bit NOT NULL CONSTRAINT DF_SMSGroups_SystemFlag DEFAULT 0
 END
+GO
+IF OBJECT_ID(N'[dbo].[updSMSGroups]') IS NULL 
+BEGIN
+    EXEC ('
+CREATE TRIGGER [dbo].[updSMSGroups] 
+   ON  [dbo].[SMSGroups] 
+   FOR UPDATE, INSERT
+AS 
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	IF UPDATE(SystemFlag)
+	BEGIN
+		-- Only one group can be designated as SystemFlag = 1
+		UPDATE dbo.SMSGroups SET SystemFlag = 0
+		WHERE ID NOT IN (SELECT ID FROM INSERTED)
+	END
+END')
+END
+GO
+
+ALTER TABLE [dbo].[SMSGroups] ENABLE TRIGGER [updSMSGroups]
+GO
+
+
