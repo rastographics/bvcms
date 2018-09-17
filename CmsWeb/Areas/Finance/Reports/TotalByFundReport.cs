3432e01b57c22dd9cb19c74a7084ed2e8dc601f6
Austin Lippert <6955342+aternex@users.noreply.github.com>
2018-09-17 11:27:54 -0400

    report library

--- CmsWeb/Areas/Finance/Reports/TotalByFundReport.cs
+++ CmsWeb/Areas/Finance/Reports/TotalByFundReport.cs
@@ -1,24 +1,40 @@
-﻿using Dapper;
-using System;
+﻿using System;
+using System.Data;
 using TouchpointSoftware.Cms.Reporting;
 
 namespace CmsWeb.Areas.Finance.Reports
 {
-    public class TotalByFundReportQuery : ReportQuery<TotalByFundReportLineItem>
+    public class TotalByFundReportQuery : ReportQuery<TotalByFundReportParameters>
     {
-        public TotalByFundReportQuery(TotalByFundReportLineItem parameters) : base(parameters)
+        public TotalByFundReportQuery(TotalByFundReportParameters parameters) : base(parameters)
         {
-            CommandText = "";
+            CommandText = "reports.TotalByFund";
             CommandType = System.Data.CommandType.StoredProcedure;
             CommandTimeout = 60;
         }
     }
 
-    public class TotalByFundReportLineItem : ReportParameterModel
+
+    public class TotalByFundReportParameters : ReportParameterModel
     {
-        public override DynamicParameters ToDynamicParameters()
-        {
-            throw new NotImplementedException();
-        }
+        [SqlParameter(Name = "StartDate", Direction = ParameterDirection.Input, Type = DbType.AnsiString)]
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int CampusId { get; set; }
+        public string Sort { get; set; }
+        public string Dir { get; set; }
+        public string TaxDedNonTax { get; set; }
+        public string FundSet { get; set; }
+        public int Online { get; set; }
+        public bool Pledges { get; set; }
+        public bool IncUnclosedBundles { get; set; }
+        public bool IncludeBundleType { get; set; }
+        public bool NonTaxDeductible { get; set; }
+        public bool FilterByActiveTag { get; set; }
+    }
+
+    public class TotalByFundReportLineItem
+    {
+
     }
 }
--- TouchpointSoftware.Cms.Reporting/ReportParameterModel.cs
+++ TouchpointSoftware.Cms.Reporting/ReportParameterModel.cs
@@ -1,9 +1,36 @@
 ﻿using Dapper;
+using System;
+using System.Data;
+using System.Linq;
+using System.Reflection;
 
 namespace TouchpointSoftware.Cms.Reporting
 {
+    public class SqlParameterAttribute : Attribute
+    {
+        public string Name { get; set; }
+        public ParameterDirection Direction { get; set; }
+        public DbType Type { get; set; }
+        public int Size { get; set; }
+    }
+
     public abstract class ReportParameterModel
     {
-        public abstract DynamicParameters ToDynamicParameters();
+        public virtual DynamicParameters ToDynamicParameters()
+        {
+            var parameters = new DynamicParameters();
+
+            var type = GetType();
+            var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance).Where(pi => pi.IsDefined(typeof(SqlParameterAttribute), true));
+
+            foreach (var property in properties)
+            {
+                var metadata = property.GetCustomAttribute(typeof(SqlParameterAttribute), true) as SqlParameterAttribute;
+
+                parameters.Add(name: metadata.Name, value: property.GetValue(this), dbType: metadata.Type, direction: metadata.Direction, size: metadata.Size);
+            }
+
+            return parameters;
+        }
     }
 }
