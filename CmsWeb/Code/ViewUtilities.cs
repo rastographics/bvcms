7c40fcbf624696bdd22ade92e1303bc3733f8558
Austin Lippert <6955342+aternex@users.noreply.github.com>
2018-09-17 23:36:35 -0400

    Add an admin setting (can modify the role requirements easily) to the gear menu to promote a person to head of household if the ability to override has been enabled via configuration

---
+++ CmsWeb/Code/ViewUtilities.cs
@@ -0,0 +1,21 @@
+﻿using CmsData;
+
+namespace CmsWeb.Code
+{
+    public class ViewUtilities
+    {
+        public static bool CanOverrideHeadOfHousehold(int peopleId)
+        {
+            var overrideEnabled = DbUtil.Db.GetSetting("CanOverrideHeadOfHousehold", "false") == "true";
+
+            if (!overrideEnabled)
+            {
+                return overrideEnabled;
+            }
+
+            var family = DbUtil.Db.LoadFamilyByPersonId(peopleId);
+
+            return peopleId != family.HeadOfHouseholdId;
+        }
+    }
+}
