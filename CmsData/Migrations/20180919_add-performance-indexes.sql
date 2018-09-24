0ca9099c245385ca6b40d0033d68efd2bda2e5db
Bron Davies <bron@brontech.com>
2018-09-24 13:49:13 -0500

    Database updates

--- CmsData/Migrations/20180919_add-performance-indexes.sql
+++ CmsData/Migrations/20180919_add-performance-indexes.sql
@@ -24,3 +24,10 @@ BEGIN
     INCLUDE ([PeopleId],[ContributionAmount])
 END
 GO
+
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name='IX_Users_Username' AND object_id = OBJECT_ID('dbo.Users'))
+BEGIN
+	CREATE NONCLUSTERED INDEX [IX_Users_Username]
+	ON [dbo].[Users] ([Username])
+END
+GO
