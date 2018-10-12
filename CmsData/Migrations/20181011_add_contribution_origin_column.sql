75c11fe0ecb4e0dbd79b55457d971ab80409b6bc
Tim Davila <tim@timdavila.com>
2018-10-12 11:56:29 -0500

    change the old source field to origin, add a true source field to indicate where a contribution came from

---
+++ CmsData/Migrations/20181011_add_contribution_origin_column.sql
@@ -0,0 +1,10 @@
+﻿IF NOT EXISTS(SELECT 1 FROM sys.columns 
+          WHERE Name = N'Origin'
+          AND Object_ID = Object_ID(N'dbo.Contribution'))
+BEGIN
+    EXEC sp_RENAME 'Contribution.Source', 'Origin', 'COLUMN'
+    
+    ALTER TABLE dbo.Contribution
+    ADD Source int not null default(0)
+END
+GO
