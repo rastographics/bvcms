f760e9c0a6569ae0eff93fc31425e208d65372e3
Austin Lippert <6955342+aternex@users.noreply.github.com>
2018-11-14 10:12:07 -0500

    Code cleanup
    Code cleanup, removed unused services, added abstract base for contribution batch importers

--- CmsWeb/Lifecycle/RequestManager.cs
+++ CmsWeb/Lifecycle/RequestManager.cs
@@ -6,6 +6,18 @@ using System.Web.Mvc;
 
 namespace CmsWeb.Lifecycle
 {
+    public class CMSBaseModel
+    {
+        protected IRequestManager RequestManager { get; }
+        protected HttpContext CurrentHttpContext => RequestManager.CurrentHttpContext;
+        protected CMSDataContext CurrentDatabase => RequestManager.CurrentDatabase;
+        protected CMSImageDataContext CurrentImageDatabase => RequestManager.CurrentImageDatabase;
+
+        public CMSBaseModel(IRequestManager requestManager)
+        {
+            RequestManager = requestManager;
+        }
+    }
     public class CMSBaseService
     {
         protected IRequestManager RequestManager { get; }
@@ -55,7 +67,7 @@ namespace CmsWeb.Lifecycle
         CMSImageDataContext CurrentImageDatabase { get; }
     }
 
-    public class RequestManager : IRequestManager
+    public class RequestManager : IRequestManager, IDisposable
     {
         public Guid RequestId { get; }
         public HttpContext CurrentHttpContext { get; }
@@ -70,5 +82,41 @@ namespace CmsWeb.Lifecycle
             CurrentDatabase = CmsData.DbUtil.Db;
             CurrentImageDatabase = ImageData.DbUtil.Db;
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    CurrentDatabase.Dispose();
+                    CurrentImageDatabase.Dispose();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~RequestManager() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
