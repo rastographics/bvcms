5289280c6ec9e8a5a11abb0c4f4225b7da6eaa42
Oscar Daniel Báez Lara <dahnbaez@gmail.com>
2018-11-20 11:38:00 -0600

    Combine Enable/Disable Fund in one method named SetRecurringGivingEnabled(int peopleId, int fundId, bool enable) enable = 1 for enable fund enable = 0 for disable fund

--- CmsData/API/PythonModel/ManagedGiving.cs
+++ CmsData/API/PythonModel/ManagedGiving.cs
@@ -5,7 +5,7 @@ namespace CmsData
 {
     public partial class PythonModel
     {
-        public void EnableFund(int peopleId, int fundId)
+        public void SetRecurringGivingEnabled(int peopleId, int fundId, bool enable)
         {
             using (var db2 = NewDataContext())
             {
@@ -14,25 +14,17 @@ namespace CmsData
                                       where m.FundId == fundId
                                       select m).SingleOrDefault();
                 if (ra != null)
-                {
-                    ra.Disabled = false;
-                    db2.SubmitChanges();
-                }
-            }
-        }
-
-        public void DisableFund(int peopleId, int fundId)
-        {
-            using (var db2 = NewDataContext())
-            {
-                RecurringAmount ra = (from m in db2.RecurringAmounts
-                                      where m.PeopleId == peopleId
-                                      where m.FundId == fundId
-                                      select m).SingleOrDefault();
-                if (ra != null)
-                {
-                    ra.Disabled = true;
-                    db2.SubmitChanges();
+                {                
+                    if (enable == true)
+                    {
+                        ra.Disabled = false;
+                        db2.SubmitChanges();
+                    }
+                    else
+                    {
+                        ra.Disabled = true;
+                        db2.SubmitChanges();
+                    }
                 }
             }
         }
