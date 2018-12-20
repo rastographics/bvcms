3560efc79c468ce10799492bb8fecc30cf8efd24
Oscar Daniel Báez Lara <dahnbaez@gmail.com>
2018-12-20 12:51:23 -0600

    add metro-zip script

---
+++ CmsWeb/Content/touchpoint/js/metrozip/metro-zip.js
@@ -0,0 +1,12 @@
+﻿$('document').ready(function () {
+
+    $(".zipcode").mask("00000");
+
+    var rangeForm = $('#createRange');
+    var endWithInput = rangeForm.find('input#startwith');
+    var endWithInput = rangeForm.find('input#startwith');
+
+    var notEmpty = function () {
+
+    };
+});
