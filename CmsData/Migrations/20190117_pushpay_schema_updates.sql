06941f2cd07a15dfd1c4c72b71b408072ded822a
Tim Davila <tim@timdavila.com>
2019-01-17 17:43:31 -0600

    update db schema to support pushpay updates

---
+++ CmsData/Migrations/20190117_pushpay_schema_updates.sql
@@ -0,0 +1,18 @@
+﻿IF EXISTS(SELECT 1 FROM sys.columns 
+          WHERE Name = N'BatchPage'
+          AND Object_ID = Object_ID(N'dbo.PushPayLog'))
+BEGIN
+	ALTER TABLE dbo.PushPayLog
+	DROP COLUMN BatchPage, MerchantKey
+
+    ALTER TABLE dbo.PushPayLog
+    ADD PeopleId int NULL,
+	BundleHeaderId int NULL,
+	ContributionId [nvarchar](100) NULL,
+	SettlementKey [nvarchar](100) NULL
+
+    ALTER TABLE dbo.BundleHeader
+    ADD ReferenceId int NULL,
+    ReferenceIdType int NULL
+END
+GO
