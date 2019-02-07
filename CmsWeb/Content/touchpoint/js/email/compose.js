7e5404f87b1c8037a3ccd894577fa58a8af3cdbe
Tim Davila <tim@timdavila.com>
2019-02-07 15:37:08 -0600

    update link logic

--- CmsWeb/Content/touchpoint/js/email/compose.js
+++ CmsWeb/Content/touchpoint/js/email/compose.js
@@ -274,9 +274,12 @@
         formInput: $('#special_links_form input'),
         orgRow: $('#org_id.row'),
         orgInput: $('#org_id input'),
+        meetingRow: $('#meeting_id.row'),
+        meetingInput: $('#meeting_id input'),
         messageInput: $('#message input'),
-        smallGroupInput: $('#small_group input'),
         confirmationSelect: $('#confirmation select'),
+        smallGroupRow: $('#small_group.row'),
+        smallGroupInput: $('#small_group input'),
         secondaryRows: $('#small_group.row, #message.row, #confirmation.row'),
         resultRow: $('#result.row'),
         resultInput: $('#result.row input'),
@@ -317,6 +320,7 @@
                 case 'sendlink2':
                 case 'supportlink':
                     specialLinks.orgRow.show();
+                    specialLinks.meetingRow.hide();
                     specialLinks.secondaryRows.hide();
                     if (orgId.length) {
                         linkText += '?org=' + orgId;
@@ -327,12 +331,30 @@
 
                 case 'rsvplink':
                 case 'regretslink':
+                    specialLinks.orgRow.hide();
+                    specialLinks.meetingRow.show();
+                    specialLinks.secondaryRows.show();
+                    specialLinks.smallGroupRow.hide();
+                    var message = specialLinks.messageInput.val();
+                    var meetingId = specialLinks.meetingInput.val();
+                    var confirmation = specialLinks.confirmationSelect.val();
+                    if (meetingId.length) {
+                        linkText += '?meeting=' + meetingId + '&confirm=' + confirmation;
+                        if (message.length) {
+                            linkText += '&msg=' + message;
+                        }
+                    } else {
+                        linkText = '';
+                    }
+                    break;
+
                 case 'votelink':
                     specialLinks.orgRow.show();
+                    specialLinks.meetingRow.hide();
                     specialLinks.secondaryRows.show();
-                    var message = specialLinks.messageInput.val();
+                    message = specialLinks.messageInput.val();
                     var smallGroup = specialLinks.smallGroupInput.val();
-                    var confirmation = specialLinks.confirmationSelect.val();
+                    confirmation = specialLinks.confirmationSelect.val();
                     if (orgId.length) {
                         linkText += '?org=' + orgId + '&confirm=' + confirmation;
                         if (message.length) {
@@ -350,6 +372,7 @@
                     linkText = '';
                     specialLinks.secondaryRows.hide();
                     specialLinks.orgRow.hide();
+                    specialLinks.meetingRow.hide();
                     specialLinks.formInput.val('');
                     break;
             }
