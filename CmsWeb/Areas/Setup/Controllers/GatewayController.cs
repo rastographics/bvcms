b1d2b378c80e7c8b4521abe55cef868c26f93ed4
jfescobar18@hotmail.com <jfescobar18@hotmail.comH@n1i4m0r>
2019-02-22 17:03:01 -0600

    Adding template for Gateway configuration and SQL File to create structure.

---
+++ CmsWeb/Areas/Setup/Controllers/GatewayController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CmsWeb.Lifecycle;
+
+namespace CmsWeb.Areas.Setup.Controllers
+{
+    [RouteArea("Setup", AreaPrefix = "Gateway")]
+    public class GatewayController : CmsStaffController
+    {
+        public GatewayController(IRequestManager requestManager): base(requestManager)
+        {
+
+        }
+
+        [Route("~/Gateway")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
