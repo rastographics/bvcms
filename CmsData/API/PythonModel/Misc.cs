4b5c426842d7ce805b526254b0fd11c380f2954c
David Carroll <david@bvcms.com>
2019-02-23 06:51:49 -0600

    Add some convenience debug only code for scripts.

--- CmsData/API/PythonModel/Misc.cs
+++ CmsData/API/PythonModel/Misc.cs
@@ -47,8 +47,36 @@ namespace CmsData
 #if DEBUG
             if (c == null)
             {
-                var s = System.IO.File.ReadAllText(name);
-                return s;
+                var txt = File.ReadAllText(name);
+                if (!txt.HasValue())
+                    return txt;
+                var nam = Path.GetFileNameWithoutExtension(name);
+                var ext = Path.GetExtension(name);
+                int typ = ContentTypeCode.TypeText;
+                switch (ext)
+                {
+                    case ".sql":
+                        typ = ContentTypeCode.TypeSqlScript;
+                        break;
+                    case ".text":
+                        typ = ContentTypeCode.TypeText;
+                        break;
+                    case ".html":
+                        typ = ContentTypeCode.TypeHtml;
+                        break;
+                }
+                c = db.Content(nam, typ);
+                if (c == null)
+                {
+                    c = new Content
+                    {
+                        Name = nam,
+                        TypeID = typ
+                    };
+                    db.Contents.InsertOnSubmit(c);
+                }
+                c.Body = txt;
+                db.SubmitChanges();
             }
 #endif
             return c.Body;
@@ -108,7 +136,8 @@ namespace CmsData
         }
         public string SqlContent(string name)
         {
-            return db.ContentOfTypeSql(name);
+            var sql = db.ContentOfTypeSql(name);
+            return sql;
         }
         public string TextContent(string name)
         {
@@ -357,6 +386,7 @@ namespace CmsData
 DELETE dbo.TagPerson FROM dbo.TagPerson tp JOIN dbo.Tag t ON t.Id = tp.Id WHERE t.TypeId = 101 AND t.Name LIKE @namelike
 DELETE dbo.Tag WHERE TypeId = 101 AND Name LIKE @namelike
 ", new {namelike});
+            Util2.CurrentTag = "UnNamed";
         }
 
         public void WriteContentSql(string name, string sql)
@@ -450,6 +480,16 @@ DELETE dbo.Tag WHERE TypeId = 101 AND Name LIKE @namelike
         public string SpaceCamelCase(string s)
         {
             return s.SpaceCamelCase();
+        }
+
+        public string Trim(string s)
+        {
+            return s.Trim();
+        }
+
+        public bool UserIsInRole(string role)
+        {
+            return HttpContext.Current?.User.IsInRole(role) ?? false;
         }
     }
 }
