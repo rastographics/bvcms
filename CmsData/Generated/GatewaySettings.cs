ae995954d60393da903847a4657c1f73a39e5fc3
jfescobar18 <jfescobar18@hotmail.com>
2019-02-26 10:19:14 -0600

    Adding new Views for multiple gateway configuration

---
+++ CmsData/Generated/GatewaySettings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Data;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using System.Linq.Expressions;
+using System.ComponentModel;
+using CmsData.Infrastructure;
+
+namespace CmsData.Generated
+{
+    [Table(Name = "lookup.GatewayServiceType")]
+    public partial class GatewaySettings : INotifyPropertyChanging, INotifyPropertyChanged
+    {
+        private static PropertyChangingEventArgs emptyChangingEventArgs = new PropertyChangingEventArgs(String.Empty);
+
+        public event PropertyChangingEventHandler PropertyChanging;
+        protected virtual void SendPropertyChanging()
+        {
+            if ((this.PropertyChanging != null))
+                this.PropertyChanging(this, emptyChangingEventArgs);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void SendPropertyChanged(string propertyName)
+        {
+            if ((this.PropertyChanged != null))
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
