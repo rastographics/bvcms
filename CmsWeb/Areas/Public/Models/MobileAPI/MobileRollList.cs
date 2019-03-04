eea5d048c0e3c82b546630189e0560a94eb1abe3
Steven Yarbrough <steven.yarbrough@touchpointsoftware.com>
2019-03-04 12:47:24 -0600

    Added Meeting Category APIs Added extended Giving Statement starter Updated MobileAuthentication routines to use new database object

--- CmsWeb/Areas/Public/Models/MobileAPI/MobileRollList.cs
+++ CmsWeb/Areas/Public/Models/MobileAPI/MobileRollList.cs
@@ -1,17 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using CmsWeb.Areas.Public.Models.MobileAPIv2;
 
 namespace CmsWeb.MobileAPI
 {
+	[SuppressMessage( "ReSharper", "CollectionNeverQueried.Global" )]
+	[SuppressMessage( "ReSharper", "NotAccessedField.Global" )]
 	public class MobileRollList
 	{
-		public int meetingID = 0;
+		public List<MobileAttendee> attendees;
+		public List<MobileMeetingCategory> categories;
 
-		public string headcountEnabled = "true";
+		public string categoriesEnabled = "false";
+		public string category = "";
 		public int headcount = 0;
 
-		public List<MobileAttendee> attendees;
+		public string headcountEnabled = "true";
+		public int meetingID = 0;
 	}
 }
