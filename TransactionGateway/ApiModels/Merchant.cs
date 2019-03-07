c9859f810b96aefe7076ff3312dc0bc1fc16d8bc
Oscar Daniel Báez Lara <dahnbaez@gmail.com>
2019-03-07 16:23:39 -0600

    Importing TransactionGateway library

---
+++ TransactionGateway/ApiModels/Merchant.cs
@@ -0,0 +1,8 @@
+namespace TransactionGateway.ApiModels
+{
+    public class Merchant : BaseResponse
+    {
+        public string Key { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ TransactionGateway/Gateways/PushPay/PushPayPayment.cs
@@ -0,0 +1,65 @@
+﻿using CmsData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TransactionGateway.ApiModels;
+
+namespace TransactionGateway
+{
+    public class PushpayPayment
+    {
+        private PushpayConnection _pushpay;
+        private CMSDataContext _db;
+        private string _merchantHandle;
+
+        public PushpayPayment(PushpayConnection Pushpay, CMSDataContext db)
+        {
+            _pushpay = Pushpay;
+            _db = db;
+            _merchantHandle = db.GetSetting("PushpayMerchant", null);
+            if (_merchantHandle == null)
+            {
+                throw new Exception("PushpayMerchant Not Found");
+            }
+        }
+
+        public async Task<Payment> GetPayment(string paymentToken)
+        {
+            IEnumerable<Merchant> merchants = await _pushpay.SearchMerchants(_merchantHandle);
+            return await _pushpay.GetPayment(merchants.FirstOrDefault().Key, paymentToken);
+        }
+
+        public async Task<RecurringPayment> GetRecurringPayment(string paymentToken)
+        {
+            IEnumerable<Merchant> merchants = await _pushpay.SearchMerchants(_merchantHandle);
+            return await _pushpay.GetRecurringPayment(merchants.FirstOrDefault().Key, paymentToken);
+        }
+
+        public async Task<IEnumerable<RecurringPayment>> GetRecurringPaymentsForAPayer(string payerKey)
+        {
+            if (payerKey == null)
+            {
+                return null;
+            }
+            return await _pushpay.GetRecurringPaymentsForAPayer(payerKey);
+        }
+
+        public async Task<Fund> GetFundForRegistration(string OrganizationName)
+        {
+            var pushpayOrganizations = await _pushpay.GetOrganizations();
+            Fund fund = new Fund();
+            foreach (var item in pushpayOrganizations)
+            {
+                var key = item.Key;
+                var funds = await _pushpay.GetFundsForOrganization(key);
+                fund = funds.Items.Where(f => f.Name == OrganizationName).FirstOrDefault();
+                if (fund == null)
+                {
+                    //create fund                    
+                }
+            }
+            return fund;
+        }
+    }
+}
