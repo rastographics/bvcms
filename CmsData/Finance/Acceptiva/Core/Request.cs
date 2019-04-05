7146c37c0cc4aece3cfea2dcb4c1a806cf4c6583
Oscar Daniel Báez Lara <dahnbaez@gmail.com>
2019-04-05 16:56:50 -0600

    Request class for Acceptiva

---
+++ CmsData/Finance/Acceptiva/Core/Request.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace CmsData.Finance.Acceptiva.Core
+{
+    internal abstract class Request
+    {
+        public string Url { get; private set; }
+
+        public NameValueCollection Data { get; protected set; }
+
+        protected Request(string url, string userName, string password)
+        {
+            Url = url;
+            Data = new NameValueCollection
+            {
+                {"username", userName},
+                {"password", password}
+            };
+        }
+
+        public string Execute()
+        {
+            using (var client = new WebClient())
+            {
+                //#if DEBUG
+                //                var values = Data.Cast<string>().Select(e => $"{e}: {Data[e]}"); 
+                //                var str = string.Join("\n", values); 
+                //                Debug.WriteLine(str);
+                //#endif
+                var result = client.UploadValues(Url, Data);
+                return Encoding.ASCII.GetString(result);
+            }
+        }
+    }
+}
