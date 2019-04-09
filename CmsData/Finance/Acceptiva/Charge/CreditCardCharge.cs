c27785d3ec7315304373497595329af9a68de41d
Oscar Daniel Báez Lara <dahnbaez@gmail.com>
2019-04-09 16:43:21 -0500

    Add Core and Charge Objects

---
+++ CmsData/Finance/Acceptiva/Charge/CreditCardCharge.cs
@@ -0,0 +1,21 @@
+﻿using CmsData.Finance.Acceptiva.Core;
+
+namespace CmsData.Finance.Acceptiva.Charge
+{
+    internal class CreditCardCharge : ChargeRequest
+    {
+        private const int paymentTypeCC = 1;
+
+        public CreditCardCharge(string apiKey, string cc_id, CreditCard creditCard,
+            decimal amt, string tranId, string tranDesc, string peopleId)
+            :base(apiKey, paymentTypeCC, amt, tranId, tranId, peopleId)
+        {
+            string[] expirateDate = creditCard.CardExpiration.Split('/');
+            Data["params[0][cc_num]"] = creditCard.CardNum;
+            Data["params[0][cc_exp_mo]"] = expirateDate[0];
+            Data["params[0][cc_exp_yr]"] = expirateDate[1];
+            Data["params[0][cc_cvv]"] = creditCard.CardNum;
+
+        }
+    }
+}
---
+++ CmsData/Finance/Acceptiva/Core/AcceptivaResponse.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace CmsData.Finance.Acceptiva.Core
+{
+    public class AcceptivaResponse<T>
+    {
+        [JsonProperty(PropertyName = "response")]
+        public T Response { get; set; }
+    }
+}
