41728aba7d2fd1fd2ee55a2ccf88847a24d3548d
Tim Davila <tim@timdavila.com>
2019-04-15 17:18:48 -0500

    wrapping up report

--- CmsWeb/Areas/Reports/Models/Attendance/MeetingAttendanceModel.cs
+++ CmsWeb/Areas/Reports/Models/Attendance/MeetingAttendanceModel.cs
@@ -1,10 +1,13 @@
 ﻿using CmsData;
+using CmsData.Codes;
+using CmsWeb.Code;
 using Dapper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Web;
+using System.Web.Mvc;
 using UtilityExtensions;
 
 namespace CmsWeb.Areas.Reports.Models.Attendance
@@ -13,6 +16,8 @@ namespace CmsWeb.Areas.Reports.Models.Attendance
     {
         public DateTime StartDt { get; set; }
         public DateTime EndDt { get; set; }
+        public int Division { get; set; }
+        private readonly List<DateTime> weeks;
 
         private CMSDataContext CurrentDatabase { get; set; }
 
@@ -21,25 +26,26 @@ namespace CmsWeb.Areas.Reports.Models.Attendance
             CurrentDatabase = db;
             StartDt = dt1;
             EndDt = dt2;
+            Division = division.ToInt();
+            weeks = CurrentDatabase.SundayDates(StartDt, EndDt).Select(w => w.Dt.Value).ToList();
+        }
+
+        public SelectList Divisions()
+        {
+            return new SelectList(new CodeValueModel().AllOrgDivTags(),
+                "Id", "Value", Division);
         }
 
         public class ColInfo
         {
             public string Heading { get; set; }
-            public List<TimeSpan> Times { get; set; }
-
-            public ColInfo()
-            {
-                Times = new List<TimeSpan>();
-            }
+            public DateTime StartDt { get; set; }
+            public DateTime EndDt { get; set; }
         }
 
-        public class ProgInfo
+        public class DivInfo
         {
             public string Name { get; set; }
-            public string RptGroup { get; set; }
-            public decimal? StartHour { get; set; }
-            public decimal? EndHour { get; set; }
             private List<ColInfo> cols;
 
             public List<ColInfo> Cols
@@ -49,61 +55,26 @@ namespace CmsWeb.Areas.Reports.Models.Attendance
                     if (cols == null)
                     {
                         cols = new List<ColInfo>();
-                        Regex re = null;
-                        if (RptGroup.TrimEnd().EndsWith(")"))
+                        for (int wk = 0; wk < WeekDates.Count(); wk++)
                         {
-                            re = new Regex(@"(?<re>\d+:\d+ [AP]M)");
-                        }
-                        else
-                        {
-                            re = new Regex(@"\((?<re>[^)]*)\)=(?<na>[^,)]*)|(?<re>\d+:\d+ [AP]M)");
-                        }
-
-                        var m = re.Match(RptGroup);
-                        while (m.Success)
-                        {
-                            var ci = new ColInfo();
-                            cols.Add(ci);
-                            var a = m.Groups["re"].Value.Split('|');
-                            if (m.Groups["na"].Value.HasValue())
+                            var week = WeekDates[wk];
+                            if (wk < WeekDates.Count() - 1)
                             {
-                                ci.Heading = m.Groups["na"].Value;
+                                var ci = new ColInfo();
+                                ci.Heading = "Week of " + week.Month + "/" + week.Day;
+                                ci.StartDt = week;
+                                ci.EndDt = WeekDates[wk + 1];
+                                cols.Add(ci);
                             }
-                            else
-                            {
-                                ci.Heading = m.Groups[1].Value;
-                            }
-
-                            foreach (var s in a)
-                            {
-                                var dt = DateTime.Parse(s);
-                                ci.Times.Add(dt.TimeOfDay);
-                            }
-                            m = m.NextMatch();
                         }
                     }
                     return cols;
                 }
             }
 
-            private int? line;
-
-            public int Line
-            {
-                get
-                {
-                    if (!line.HasValue)
-                    {
-                        line = Regex.Match(RptGroup, @"\A\d+").Value.ToInt();
-                    }
-
-                    return line.Value;
-                }
-            }
-
             public List<DateTime> WeekDates { get; set; }
 
-            public List<DivInfo> Divs;
+            public List<OrgInfo> Orgs;
 
             public List<WeekInfo> Weeks
             {
@@ -113,44 +84,27 @@ namespace CmsWeb.Areas.Reports.Models.Attendance
                             select new WeekInfo
                             {
                                 Sunday = w,
-                                Meetings = (from d in Divs
+                                Meetings = (from d in Orgs
                                             from m in d.Meetings
-                                            where m.Date >= w.AddHours((double)(StartHour ?? 0))
-                                            where m.Date <= w.AddHours((double)(EndHour ?? 0))
+                                            where m.Date >= w
+                                            where m.Date <= w
                                             select m).ToList()
                             };
                     return q.Where(w => w.Meetings.Sum(m => m.Present) > 0).ToList();
                 }
             }
 
-            public Average Total()
+            public Cell Total()
             {
-                var a = new Average();
-                var q = (from w in Weeks
-                         from m in w.Meetings
-                         group m by w.Sunday
-                         into g
-                         select g.Sum(mm => mm.Present)).ToList();
-                a.Avg = !q.Any() ? 0 : q.Average();
-                a.TotalPeople = q.Sum();
-                a.TotalMeetings = q.Count;
+                var a = new Cell();
+                var meetings = (from d in Orgs
+                                from m in d.Meetings
+                                select m).ToList();
+                a.TotalMeetings = meetings.Count;
+                a.TotalPeople = meetings.Sum(mm => mm.Present);
                 return a;
             }
 
-            public Average Column(ColInfo c)
-            {
-                var a = new Average();
-                var q = (from w in Weeks
-                         from m in w.Meetings
-                         where c.Times.Contains(m.Date.TimeOfDay)
-                         group m by w.Sunday
-                         into g
-                         select g.Sum(mm => mm.Present)).ToList();
-                a.Avg = !q.Any() ? 0 : q.Average();
-                a.TotalMeetings = q.Count; //q.Where(g => g > 0).Count();
-                a.TotalPeople = q.Sum();
-                return a;
-            }
         }
 
         public class WeekInfo
@@ -159,12 +113,9 @@ namespace CmsWeb.Areas.Reports.Models.Attendance
             public List<MeetInfo> Meetings;
         }
 
-        public class DivInfo
+        public class OrgInfo
         {
-            public ProgInfo Prog { get; set; }
-            public int DivId { get; set; }
             public string Name { get; set; }
-            public int Line { get; set; }
             public List<MeetInfo> Meetings;
             public List<DateTime> WeekDates { get; set; }
 
@@ -177,47 +128,38 @@ namespace CmsWeb.Areas.Reports.Models.Attendance
                             {
                                 Sunday = w,
                                 Meetings = (from m in Meetings
-                                            where m.Date >= w.AddHours((double)(Prog.StartHour ?? 0))
-                                            where m.Date <= w.AddHours((double)(Prog.EndHour ?? 0))
+                                            where m.Date >= w
+                                            where m.Date <= w
                                             select m).ToList()
                             };
                     return q.Where(w => w.Meetings.Sum(m => m.Present) > 0).ToList();
                 }
             }
 
-            public Average Total()
+
+            public Cell Column(ColInfo c)
             {
-                var a = new Average();
-                var q = (from w in Weeks
-                         from m in w.Meetings
-                         group m by w.Sunday
-                         into g
-                         select g.Sum(mm => mm.Present)).ToList();
-                a.Avg = q.Any() ? q.Average() : 0;
+                var a = new Cell();
+                var q = (from m in Meetings
+                         where m.Date >= c.StartDt
+                         where m.Date < c.EndDt
+                         select m).ToList();
                 a.TotalMeetings = q.Count;
-                a.TotalPeople = q.Sum();
+                a.TotalPeople = q.Sum(mm => mm.Present);
                 return a;
             }
 
-            public Average Column(ColInfo c)
+            public Cell Total()
             {
-                var a = new Average();
-                var q = (from w in Weeks
-                         from m in w.Meetings
-                         where c.Times.Contains(m.Date.TimeOfDay)
-                         group m by w.Sunday
-                         into g
-                         select g.Sum(mm => mm.Present)).ToList();
-                a.Avg = q.Any() ? q.Average() : 0;
-                a.TotalPeople = q.Sum();
-                a.TotalMeetings = q.Count();
+                var a = new Cell();
+                a.TotalMeetings = Meetings.Count;
+                a.TotalPeople = Meetings.Sum(mm => mm.Present);
                 return a;
             }
         }
 
-        public class Average
+        public class Cell
         {
-            public double Avg { get; set; }
             public int TotalMeetings { get; set; }
             public int TotalPeople { get; set; }
         }
@@ -226,117 +168,41 @@ namespace CmsWeb.Areas.Reports.Models.Attendance
         {
             public int OrgId { get; set; }
             public string OrgName { get; set; }
-            public DateTime Date { get; set; }
+            public DateTime? Date { get; set; }
             public int Present { get; set; }
         }
 
-        public List<ProgInfo> FetchInfo()
+        public List<DivInfo> FetchInfo()
         {
-            string sql = $@"
-WITH progs AS (
-	SELECT * FROM dbo.Program
-	WHERE LEN(RptGroup) > 0
-),
-progdivs AS (
-	SELECT 
-		p.Id ProgId
-		,p.Name Prog
-		,d.Id DivId
-		,d.Name Div
-		,p.RptGroup
-		,d.ReportLine
-		,p.StartHoursOffset
-		,p.EndHoursOffset
-	FROM dbo.Division d
-	JOIN dbo.ProgDiv pd ON pd.DivId = d.Id 
-	JOIN progs p ON p.Id = pd.ProgId
-	WHERE d.ReportLine >= 0
-),
-meetings AS (
-	SELECT m.MeetingId
-          ,m.MaxCount
-          ,m.MeetingDate
-		  ,pd.ProgId
-          ,pd.Prog
-          ,pd.DivId
-          ,pd.Div
-          ,pd.RptGroup
-          ,pd.ReportLine
-          ,pd.StartHoursOffset
-          ,pd.EndHoursOffset
-          ,o.OrganizationId
-		  ,o.OrganizationName
+            var q = from div in CurrentDatabase.Divisions
+                            where div.Id == Division
+                            select new DivInfo
+                            {
+                                Name = div.Name,
+                                WeekDates = weeks,
+                                Orgs = (from org in CurrentDatabase.Organizations
+                                        where org.DivisionId == div.Id
+                                        where org.OrganizationStatusId == OrgStatusCode.Active
+                                        select new OrgInfo
+                                        {
+                                            Name = org.OrganizationName,
+                                            Meetings = (from m in CurrentDatabase.Meetings
+                                                        where org.OrganizationId == m.OrganizationId
+                                                        where m.MeetingDate >= StartDt
+                                                        where m.MeetingDate < EndDt
+                                                        select new MeetInfo
+                                                        {
+                                                            Date = m.MeetingDate,
+                                                            OrgId = org.OrganizationId,
+                                                            OrgName = org.OrganizationName,
+                                                            Present = m.MaxCount ?? 0
+                                                        }).ToList()
+                                        }).ToList()
 
-	FROM dbo.Meetings m
-	JOIN dbo.Organizations o ON o.OrganizationId = m.OrganizationId
-	JOIN dbo.DivOrg dd ON dd.OrgId = o.OrganizationId
-	JOIN progdivs pd ON pd.DivId = dd.DivId
-	WHERE m.MeetingDate >= @Dt1
-	AND CONVERT(DATE, m.MeetingDate) <= @Dt2
-	AND m.MaxCount > 0
-)
-SELECT * FROM meetings
-ORDER BY RptGroup, ReportLine
-";
-            var j = CurrentDatabase.Connection.Query<Data>(sql, new { StartDt, EndDt }).ToList();
-            var q = from prog in j
-                    group prog by prog.RptGroup
-                    into g
-                    orderby g.Key
-                    let p = g.First()
-                    select new ProgInfo
-                    {
-                        Name = p.Prog,
-                        RptGroup = p.RptGroup,
-                        StartHour = p.StartHoursOffset,
-                        EndHour = p.EndHoursOffset,
-                        Divs = (from div in g
-                                group div by div.ReportLine
-                                into lines
-                                let d1 = lines.First()
-                                select new DivInfo
-                                {
-                                    DivId = d1.DivId,
-                                    Name = d1.Div,
-                                    Meetings = (from m in lines
-                                                select new MeetInfo
-                                                {
-                                                    Date = m.MeetingDate,
-                                                    OrgId = m.OrgId,
-                                                    OrgName = m.OrganizationName,
-                                                    Present = m.MaxCount
-                                                }).ToList()
-                                }).ToList()
-                    };
-
+                            };
             var list = q.ToList();
-            foreach (var p in list)
-            {
-                p.WeekDates = weeks;
-                foreach (var d in p.Divs)
-                {
-                    d.Prog = p;
-                    d.WeekDates = weeks;
-                }
-            }
             return list;
         }
-
-        public class Data
-        {
-            public int MeetingId { get; set; }
-            public int MaxCount { get; set; }
-            public DateTime MeetingDate { get; set; }
-            public int ProgId { get; set; }
-            public string Prog { get; set; }
-            public int DivId { get; set; }
-            public string Div { get; set; }
-            public string RptGroup { get; set; }
-            public string ReportLine { get; set; }
-            public int StartHoursOffset { get; set; }
-            public int EndHoursOffset { get; set; }
-            public int OrgId { get; set; }
-            public string OrganizationName { get; set; }
-        }
+        
     }
 }
