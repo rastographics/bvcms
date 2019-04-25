20f2e48b84138e8f931bd69a4584a2f77882068f
Bron Davies <bron@brontech.com>
2019-04-25 10:53:30 -0500

    Code cleanup

--- CmsData/DbUtil/Context.cs
+++ CmsData/DbUtil/Context.cs
@@ -151,11 +151,6 @@ namespace CmsData
 
         private static string CreateConnectionString(string host)
         {
-            var hostOverride = ConfigurationManager.AppSettings["host"];
-            if (!string.IsNullOrEmpty(hostOverride)) // default to the host from url, however, override it via web.config for debugging against live data
-            {
-                host = hostOverride;
-            }
             var cs = ConfigurationManager.ConnectionStrings["CMS"];
             var cb = new SqlConnectionStringBuilder(cs.ConnectionString);
             cb.InitialCatalog = $"CMS_{host}";
