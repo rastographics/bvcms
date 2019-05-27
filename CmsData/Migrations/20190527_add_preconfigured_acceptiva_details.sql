14375db97baa49362de0650806355d4cde24f618
Oscar Daniel Báez Lara <dahnbaez@gmail.com>
2019-05-27 12:58:02 -0500

    add migration that populates preconfigured acceptiva details

---
+++ CmsData/Migrations/20190527_add_preconfigured_acceptiva_details.sql
@@ -0,0 +1,23 @@
+﻿IF NOT EXISTS (SELECT * FROM [dbo].[GatewayDetails] t
+    JOIN [dbo].[GatewayAccount] g  on t.GatewayAccountId = g.GatewayAccountId
+    WHERE g.GatewayAccountName = 'Acceptiva')
+	BEGIN
+		INSERT INTO [dbo].[GatewayDetails]
+		([GatewayAccountId]
+		,[GatewayDetailName]
+		,[GatewayDetailValue]
+		,[IsBoolean]
+		,[IsDefault])
+		SELECT [dbo].[GatewayAccount].[GatewayAccountId]
+		,[lookup].[GatewayConfigurationTemplate].[GatewayDetailName]
+		,[lookup].[GatewayConfigurationTemplate].[GatewayDetailValue]
+		,[lookup].[GatewayConfigurationTemplate].[IsBoolean]
+		,1
+		FROM [dbo].[GatewayAccount]
+		INNER JOIN [lookup].[Gateways]
+		ON [dbo].[GatewayAccount].[GatewayId] = [lookup].[Gateways].[GatewayId]
+		INNER JOIN [lookup].[GatewayConfigurationTemplate]
+		ON [lookup].[Gateways].[GatewayId] = [lookup].[GatewayConfigurationTemplate].[GatewayId]
+		WHERE [dbo].[GatewayAccount].[GatewayAccountName] = 'Acceptiva'
+	END
+GO
