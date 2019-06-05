c44e3448e033746ac876c129101956b378c9e1a8
Tim Davila <tim@timdavila.com>
2019-06-05 11:19:51 -0500

    update migration

--- CmsData/Migrations/20190603_add_role_priority.sql
+++ CmsData/Migrations/20190603_add_role_priority.sql
@@ -2,10 +2,11 @@ IF NOT EXISTS(SELECT 1 FROM sys.columns
           WHERE Name = N'Priority'
           AND Object_ID = Object_ID(N'dbo.Roles'))
 BEGIN
-    ALTER TABLE dbo.Roles
-    ADD Priority INT NULL
-
-    UPDATE dbo.Roles
-    SET Priority = RoleId
+    EXEC dbo.sp_executesql N'
+        ALTER TABLE dbo.Roles
+        ADD Priority INT NULL;'
+    EXEC dbo.sp_executesql N'
+        UPDATE dbo.Roles
+        SET Priority = RoleId;'
 END
 GO
