eddae35b855a4a461f55410e985100dbd4134d92
Tim Davila <tim@timdavila.com>
2019-06-05 11:44:59 -0500

    working snackbar

--- CmsWeb/Content/touchpoint/js/snackbar.js
+++ CmsWeb/Content/touchpoint/js/snackbar.js
@@ -1,10 +1,8 @@
-﻿$(function () {
-    function snackbar(msg, type = 'success', delay = '1000') {
-        var el = $('.snackbar');
-        el.text(msg);
-        el.addClass(type + ' visible');
-        setTimeout(function () {
-            el.removeClass('visible');
-        }, delay);
-    }
-});
+﻿function snackbar(msg, type = 'success', delay = '2000') {
+    var el = $('.snackbar');
+    el.text(msg);
+    el.addClass(type + ' visible');
+    setTimeout(function () {
+        el.removeClass('visible');
+    }, delay);
+}
