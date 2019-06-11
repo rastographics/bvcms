bc91f10efcbbe8ddb2d4958ff233a9526426e937
Tim Davila <tim@timdavila.com>
2019-06-11 11:01:38 -0500

    update default role priorities, highlight changed settings

--- CmsData/Migrations/20190603_add_role_priority.sql
+++ CmsData/Migrations/20190603_add_role_priority.sql
@@ -8,5 +8,14 @@ BEGIN
     EXEC dbo.sp_executesql N'
         UPDATE dbo.Roles
         SET Priority = RoleId;'
+    -- Admin should be on top, Access on bottom by default
+    EXEC dbo.sp_executesql N'
+        UPDATE dbo.Roles
+        SET Priority = 0
+        WHERE RoleName = ''Admin'';'
+    EXEC dbo.sp_executesql N'
+        UPDATE dbo.Roles
+        SET Priority = (select MAX(Priority) + 1 from Roles)
+        WHERE RoleName = ''Access'';'
 END
 GO
