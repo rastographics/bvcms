843ea1eb274efe74cd096c272feedc0f61f747a8
Tim Davila <tim@timdavila.com>
2019-07-08 15:29:50 -0500

    add some tests

---
+++ IntegrationTests/Areas/General/GeneralTests.cs
@@ -0,0 +1,41 @@
+﻿using IntegrationTests.Support;
+using Shouldly;
+using Xunit;
+
+namespace IntegrationTests.Areas.Manage
+{
+    [Collection("WebApp Collection")]
+    public class GeneralTests : AccountTestBase
+    {
+        [Fact]
+        public void Page_NotFound_Test()
+        {
+            username = RandomString();
+            password = RandomString();
+            
+            var user = CreateUser();
+
+            Login();
+
+            Open($"{rootUrl}Invalid");
+
+            PageSource.ShouldContain("Not Found");
+        }
+
+        [Fact]
+        public void Page_NotAuthorized_Test()
+        {
+            username = RandomString();
+            password = RandomString();
+            
+            var user = CreateUser();
+
+            Login();
+
+            Open($"{rootUrl}Bundles");
+
+            PageSource.ShouldContain("Sign In");
+        }
+        
+    }
+}
