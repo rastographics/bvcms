6eb1f773ce7bd794a8e4b843d8c67b826da6c078
Tim Davila <tim@timdavila.com>
2019-07-19 17:17:32 -0500

    ui, family and member data

--- CmsWeb/Content/touchpoint/js/checkin.js
+++ CmsWeb/Content/touchpoint/js/checkin.js
@@ -1,15 +1,28 @@
 ﻿var CheckInApp = new Vue({
     el: '#CheckInApp',
     data: {
-        identity: false,
         view: false,
+        identity: false,
         profile: false,
+        loading: false,
+        families: [],
+        members: [],
+        attendance: [],
         user: {
             name: '',
             password: ''
         },
         search: {
             phone: ''
+        }
+    },
+    watch: {
+        loading: function (loading) {
+            if (loading) {
+                $.block();
+            } else {
+                $.unblockUI();
+            }
         }
     },
     computed: {
@@ -49,18 +62,20 @@
             let vm = this;
             var token = vm.generateToken();
             if (token) {
+                vm.loading = true;
                 vm.$http.post('/CheckInApiV2/Authenticate', {}, {
                     headers: {
                         'Authorization': token
                     }
                 }).then(
                     response => {
+                        vm.loading = false;
                         if (response.status === 200) {
                             if (response.data.error === 0) {
                                 var profile = JSON.parse(response.data.data);
                                 profile = {
                                     userName: profile.userName,
-                                    userId: profile.userId
+                                    userId: profile.userID
                                 };
                                 localStorage.setItem('identity', token);
                                 localStorage.setItem('profile', JSON.stringify(profile));
@@ -69,16 +84,16 @@
                                 vm.view = 'landing';
                             } else {
                                 // invalid creds
+                                vm.view = 'login';
                                 warning_swal('Login Failed', response.data.data);
                             }
                         }
                         else {
-                            console.log(response);
                             warning_swal('Warning!', 'Something went wrong, try again later');
                         }
                     },
                     err => {
-                        console.log(err);
+                        vm.loading = false;
                         error_swal('Error', 'Unable to connect to TouchPoint');
                     }
                 );
@@ -95,42 +110,56 @@
         },
         find() {
             let vm = this;
-            // todo: set campus from profile
+            // todo: set campus and date from profile
             var payload = vm.generatePayload({
                 search: vm.search.phone,
                 campus: 0,
-                date: '2019-07-21 08:00:00'
+                date: '2019-07-21 08:00:00' // todo: remove, debug only
             });
+            vm.loading = true;
             vm.$http.post('/CheckInApiV2/Search', payload, vm.apiHeaders).then(
                 response => {
+                    vm.loading = false;
                     if (response.status === 200) {
                         if (response.data.error === 0) {
                             var results = JSON.parse(response.data.data);
-                            console.log(JSON.parse(response.data.data));
+                            console.log(results);
                             vm.search.phone = '';
                             if (results.length > 1) {
+                                vm.families = results;
                                 vm.view = 'families';
                             } else if (results.length === 1) {
+                                vm.families = [];
+                                vm.members = results[0].members;
                                 vm.view = 'checkin';
                             } else {
-                                // no results
+                                vm.view = 'landing';
+                                warning_swal('No results', 'No families found with that number, please try again.');
                             }
                         } else {
                             vm.search.phone = '';
+                            vm.view = 'landing';
                             warning_swal('Search Failed', response.data.data);
                         }
-                    }
-                    else {
+                    } else {
                         vm.search.phone = '';
-                        console.log(response);
+                        vm.view = 'landing';
                         warning_swal('Warning!', 'Something went wrong, try again later');
                     }
                 },
                 err => {
-                    console.log(err);
+                    vm.loading = false;
+                    vm.view = 'landing';
                     error_swal('Error', 'Something went wrong');
                 }
             );
+        },
+        selectFamily(family) {
+            this.members = family.members;
+            this.view = 'checkin';
+        },
+        updateAttendance() {
+            // todo: post attendance bundle to /UpdateAttend
         }
     },
     mounted() {
@@ -145,7 +174,6 @@
         var profile = localStorage.getItem('profile');
         if (profile && profile.length) {
             this.profile = JSON.parse(profile);
-            console.log(this.profile);
         }
     }
 });
