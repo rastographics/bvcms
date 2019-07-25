3f5f157ecf57a408a9c33539a64393b518cd3ccc
Oscar Daniel Báez Lara <dahnbaez@gmail.com>
2019-07-24 19:34:06 -0500

    Add migration and context entities

---
+++ CmsData/Migrations/20190724_add_checkinprofiles_settings_tables.sql
@@ -0,0 +1,50 @@
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES where 
+	TABLE_NAME = 'CheckinProfiles' AND 
+	TABLE_SCHEMA = 'dbo')
+	BEGIN
+		DROP TABLE [dbo].[CheckinProfiles]
+	END
+GO
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES where 
+	TABLE_NAME = 'CheckinProfileSettings' AND 
+	TABLE_SCHEMA = 'dbo')
+	BEGIN
+		DROP TABLE [dbo].[CheckinProfileSettings]
+	END
+GO
+
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES where 
+	TABLE_NAME = 'CheckinProfiles' AND 
+	TABLE_SCHEMA = 'dbo')
+	BEGIN				
+		CREATE TABLE [dbo].[CheckinProfiles](
+			[CheckinProfileId][int] IDENTITY(1,1) PRIMARY KEY,
+			[Name] NVARCHAR(100) NOT NULL
+		);
+	END
+GO
+
+IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES where 
+	TABLE_NAME = 'CheckinProfileSettings' AND 
+	TABLE_SCHEMA = 'dbo')
+	BEGIN				
+		CREATE TABLE [dbo].CheckinProfileSettings(
+			[CheckinProfileId][int] FOREIGN KEY REFERENCES [dbo].[CheckinProfiles](CheckinProfileId) NOT NULL UNIQUE,
+			[EarlyCheckin][int] NULL,
+			[LateCheckin][int] NULL,
+			[Testing] BIT NOT NULL,
+			[TestDay][int] NULL,
+			[AdminPIN] NVARCHAR(MAX) NOT NULL,
+			[PINTimeout][int] NOT NULL,
+			[DisableJoin] BIT NOT NULL,
+			[DisableTimer] BIT NOT NULL,
+			[BackgroundImage][int] NULL,
+			[CutoffAge][int] NOT NULL,
+			[Logout] NVARCHAR(MAX) NOT NULL,
+			[Guest] BIT NOT NULL,
+			[Location] BIT NOT NULL,
+			[SecurityType][INT] NULL
+		);
+	END
+GO
