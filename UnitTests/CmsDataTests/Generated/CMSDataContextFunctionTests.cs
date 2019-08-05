fdb85328e72416c98cfdb5252286ff4336b2b24f
jfescobar18 <jfescobar18@hotmail.com>
2019-08-05 09:38:13 -0500

    Unit Test Fix delte Contributions at the End

--- UnitTests/CmsDataTests/Generated/CMSDataContextFunctionTests.cs
+++ UnitTests/CmsDataTests/Generated/CMSDataContextFunctionTests.cs
@@ -20,17 +20,19 @@ namespace CmsDataTests
             var toDate = new DateTime(2019, 7, 31);
             using (var db = CMSDataContext.Create(Util.Host))
             {
-                db.ExecuteCommand("DELETE FROM [dbo].[BundleDetail] DBCC CHECKIDENT ([BundleDetail], RESEED, 0)");
-                db.ExecuteCommand("DELETE FROM [Contribution] DBCC CHECKIDENT ([Contribution], RESEED, 0)");
-
                 var bundleHeader = CreateBundle(db);
-                CreateContribution(db, bundleHeader, fromDate, 120, peopleId: 1);
-                CreateContribution(db, bundleHeader, fromDate, 500, peopleId: 1, contributionType: ContributionTypeCode.Pledge);
+                var FirstContribution = CreateContribution(db, bundleHeader, fromDate, 120, peopleId: 1);
+                var SecondContribution = CreateContribution(db, bundleHeader, fromDate, 500, peopleId: 1, contributionType: ContributionTypeCode.Pledge);
 
                 var results = db.GetTotalContributionsDonor(fromDate, toDate, null, null, true, null, null).ToList();
                 var actual = results.First();
 
                 actual.Amount.ShouldBe(120);
+
+                var bundleDetail = db.BundleDetails.Where(x => x.ContributionId == FirstContribution.ContributionId && x.ContributionId == SecondContribution.ContributionId).FirstOrDefault();
+                db.BundleDetails.DeleteOnSubmit(bundleDetail);
+                db.Contributions.DeleteOnSubmit(FirstContribution);
+                db.Contributions.DeleteOnSubmit(SecondContribution);
                 //This fails => actual.PledgeAmount.ShouldBe(500);
             }
         }
