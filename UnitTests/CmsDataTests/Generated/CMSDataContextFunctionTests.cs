434e9857b7514765ea1577e038ba2b7aba5d88f0
jfescobar18 <jfescobar18@hotmail.com>
2019-08-06 10:40:24 -0500

    Cleaning Contributions at the end of test

--- UnitTests/CmsDataTests/Generated/CMSDataContextFunctionTests.cs
+++ UnitTests/CmsDataTests/Generated/CMSDataContextFunctionTests.cs
@@ -20,6 +20,8 @@ namespace CmsDataTests
             var toDate = new DateTime(2019, 7, 31);
             using (var db = CMSDataContext.Create(Util.Host))
             {
+                var TotalAmmountContributions = db.Contributions.Where(x => x.ContributionTypeId == 1).Sum(x => x.ContributionAmount);
+
                 var bundleHeader = CreateBundle(db);
                 var FirstContribution = CreateContribution(db, bundleHeader, fromDate, 120, peopleId: 1);
                 var SecondContribution = CreateContribution(db, bundleHeader, fromDate, 500, peopleId: 1, contributionType: ContributionTypeCode.Pledge);
@@ -27,10 +29,14 @@ namespace CmsDataTests
                 var results = db.GetTotalContributionsDonor(fromDate, toDate, null, null, true, null, null).ToList();
                 var actual = results.First();
 
-                actual.Amount.ShouldBe(120);
+                actual.Amount.ShouldBe(TotalAmmountContributions + 120);
 
-                var bundleDetail = db.BundleDetails.Where(x => x.ContributionId == FirstContribution.ContributionId && x.ContributionId == SecondContribution.ContributionId).FirstOrDefault();
-                db.BundleDetails.DeleteOnSubmit(bundleDetail);
+                var FirstbundleDetail = db.BundleDetails.Where(x => x.ContributionId == FirstContribution.ContributionId).FirstOrDefault();
+                var SecondbundleDetail = db.BundleDetails.Where(x => x.ContributionId == SecondContribution.ContributionId).FirstOrDefault();
+
+                db.BundleDetails.DeleteOnSubmit(FirstbundleDetail);
+                db.BundleDetails.DeleteOnSubmit(SecondbundleDetail);
+
                 db.Contributions.DeleteOnSubmit(FirstContribution);
                 db.Contributions.DeleteOnSubmit(SecondContribution);
                 //This fails => actual.PledgeAmount.ShouldBe(500);
