f1eae31212ce910d3e6efce490b6526d83dc4a26
jfescobar18 <jfescobar18@hotmail.com>
2019-08-08 13:38:53 -0500

    Typo fixed

--- UnitTests/CMSWebTests/Areas/People/Models/Person/Enrollments/RegistrationsModelTests.cs
+++ UnitTests/CMSWebTests/Areas/People/Models/Person/Enrollments/RegistrationsModelTests.cs
@@ -2,6 +2,7 @@
 using CmsWeb.Areas.People.Models;
 using CmsData;
 using Shouldly;
+using UtilityExtensions;
 
 namespace CMSWebTests.Areas.People.Models.Person.Enrollments
 {
@@ -11,8 +12,9 @@ namespace CMSWebTests.Areas.People.Models.Person.Enrollments
         [Fact]
         public void Should_Run_FulfillmentList()
         {
-            DbUtil.Db = CMSDataContext.Create(host: "localhost");
+            DbUtil.Db = CMSDataContext.Create(Util.Host);
             var m = new RegistrationsModel();
+            m.PeopleId = 1;
             var fulfillmentlist = m.FulfillmentList();
 
             fulfillmentlist.ShouldNotBeNull();
