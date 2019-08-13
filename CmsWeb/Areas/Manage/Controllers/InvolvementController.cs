0b159bacbc37efd3fea7be23584ac7c7aabfc0aa
Ben <ben@clearfunction.com>
2019-08-13 15:35:44 -0500

    Wire up save button for Customize Involvement Tab and update database

--- CmsWeb/Areas/Manage/Controllers/InvolvementController.cs
+++ CmsWeb/Areas/Manage/Controllers/InvolvementController.cs
@@ -1,6 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using CmsWeb.Lifecycle;
 using System.Web.Mvc;
+using CmsData;
+using CmsData.Codes;
 using CmsWeb.Areas.Manage.Models;
 using CmsWeb.Areas.Manage.Models.Involvement;
 
@@ -24,6 +27,28 @@ namespace CmsWeb.Areas.Manage.Controllers
             model.Previous.ReadXml(CurrentDatabase.Contents.SingleOrDefault(c => c.Name == model.Previous.Name)?.Body ?? Resource1.InvolvementTablePrevious);
 
             return View(model);
+        }
+
+        public JsonResult Update(InvolvementTabModel model)
+        {
+            var involvementTabContent = CurrentDatabase.Contents.SingleOrDefault(c => c.Name == model.Name);
+            if (involvementTabContent == null)
+            {
+                involvementTabContent = new Content
+                {
+                    Title = "Edit Text Content",
+                    Name = model.Name,
+                    TypeID = ContentTypeCode.TypeText,
+                    DateCreated = DateTime.Now
+                };
+                CurrentDatabase.Contents.InsertOnSubmit(involvementTabContent);
+            }
+
+            involvementTabContent.Body = model.BuildXml();
+
+            CurrentDatabase.SubmitChanges();
+
+            return Json(new {success = true});
         }
     }
 }
--- CmsWeb/Areas/Manage/Models/Involvement/InvolvementTabModel.cs
+++ CmsWeb/Areas/Manage/Models/Involvement/InvolvementTabModel.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 using System.Xml.Linq;
 
 namespace CmsWeb.Areas.Manage.Models.Involvement
@@ -6,6 +8,11 @@ namespace CmsWeb.Areas.Manage.Models.Involvement
     public class InvolvementTabModel
     {
         private const int COLUMN_MAX = 6;
+
+        public InvolvementTabModel()
+        {
+            Types = new List<InvolvementType>();
+        }
 
         public InvolvementTabModel(string name)
         {
@@ -43,6 +50,153 @@ namespace CmsWeb.Areas.Manage.Models.Involvement
                 }
                 Types.Add(involvementType);
             }
+        }
+
+        public string BuildXml()
+        {
+            var xDoc = new XDocument(
+                new XElement("InvolvementTable",
+                    Types.Select(type => new XElement("Columns",
+                            type.Name != null ? new XAttribute("orgtype", type.Name) : null,
+                            type.Columns.Select(column => !IsColumnNull(column) ? new XElement("Column",
+                                column.Field != null ? new XAttribute("field", column.Field) : null,
+                                column.Label != null ? new XAttribute("label", column.Label) : null,
+                                column.Role != null ? new XAttribute("role", column.Role) : null,
+                                column.Sortable != null && column.Sortable.Value ? new XAttribute("sortable", column.Sortable) : null) : null)
+                            )
+                        )
+                    )
+                );
+
+            return xDoc.ToString();
+        }
+
+        private bool IsColumnNull(InvolvementColumn column)
+        {
+            return string.IsNullOrWhiteSpace(column.Field) &&
+                   string.IsNullOrWhiteSpace(column.Label) &&
+                   string.IsNullOrWhiteSpace(column.Role) &&
+                   column.Sortable == null;
+        }
+
+        public IEnumerable<SelectListItem> TypeSelectList(string selectedValue)
+        {
+            var selectList = new List<SelectListItem>
+            {
+                new SelectListItem
+                {
+                    Text = "Mailing List",
+                    Value = "Mailing List"
+                },
+                new SelectListItem
+                {
+                    Text = "Leader Coach Group",
+                    Value = "Leader Coach Group"
+                },
+                new SelectListItem
+                {
+                    Text = "Community Group",
+                    Value = "Community Group"
+                },
+                new SelectListItem
+                {
+                    Text = "Registration",
+                    Value = "Registration"
+                },
+                new SelectListItem
+                {
+                    Text = "Volunteer Group",
+                    Value = "Volunteer Group"
+                },
+                new SelectListItem
+                {
+                    Text = "Children and Youth Group",
+                    Value = "Children and Youth Group"
+                }
+            };
+
+            var selected = selectList.FirstOrDefault(x => x.Value == selectedValue);
+            if (selected != null)
+            {
+                selected.Selected = true;
+            }
+
+            return selectList;
+        }
+
+        public IEnumerable<SelectListItem> ColumnSelectList(string selectedValue)
+        {
+            var selectList = new List<SelectListItem>
+            {
+                new SelectListItem
+                {
+                    Text = "Organization",
+                    Value = "Organization"
+                },
+                new SelectListItem
+                {
+                    Text = "Leader",
+                    Value = "Leader"
+                },
+                new SelectListItem
+                {
+                    Text = "Enroll Date",
+                    Value = "Enroll Date"
+                },
+                new SelectListItem
+                {
+                    Text = "MemberType",
+                    Value = "MemberType"
+                },
+                new SelectListItem
+                {
+                    Text = "Unsubscribe",
+                    Value = "Leave"
+                },
+                new SelectListItem
+                {
+                    Text = "View Calendar",
+                    Value = "ViewCalendar"
+                },
+                new SelectListItem
+                {
+                    Text = "Location",
+                    Value = "Location"
+                },
+                new SelectListItem
+                {
+                    Text = "Schedule",
+                    Value = "Schedule"
+                },
+                new SelectListItem
+                {
+                    Text = "Drop Date",
+                    Value = "Drop Date"
+                },
+                new SelectListItem
+                {
+                    Text = "Last Visit",
+                    Value = "Last Visit"
+                },
+                new SelectListItem
+                {
+                    Text = "Health",
+                    Value = "Health"
+                },
+                new SelectListItem
+                {
+                    Text = "AttendPct",
+                    Value = "AttendPct"
+                }
+            };
+
+            var selected = selectList.FirstOrDefault(x => x.Value == selectedValue);
+            if (selected != null)
+            {
+                selected.Selected = true;
+            }
+
+            return selectList;
         }
     }
 }
