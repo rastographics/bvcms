03278f67433c114718fff17672f7624ae62ca395
jfescobar18 <jfescobar18@hotmail.com>
2019-08-20 00:23:29 -0500

    Issue fixed Unit Test added

--- UnitTests/CMSWebTests/TestUtils/FakeOrganizationUtils.cs
+++ UnitTests/CMSWebTests/TestUtils/FakeOrganizationUtils.cs
@@ -1,38 +1,49 @@
 ﻿using CmsData;
-using ImageData;
-using System;
-using System.Security.Principal;
-using System.Web;
-using CmsWeb.Lifecycle;
 using CmsWeb.Areas.Dialog.Models;
 using System.Collections.Generic;
 using CmsWeb.Areas.OnlineReg.Models;
 using UtilityExtensions;
+using System.Linq;
+using Xunit;
 
 namespace CMSWebTests
 {
+    [Collection("Database collection")]
     public class FakeOrganizationUtils
     {
         public static NewOrganizationModel FakeNewOrganizationModel { get; set; }
         public static Organization FakeOrganization { get; set; }
+        public static int? OrgId { get; set; }
 
-        public static NewOrganizationModel MakeFakeOrganization()
+        public static NewOrganizationModel MakeFakeOrganization(Organization Orgconfig = null)
         {
-            if(FakeNewOrganizationModel == null)
+            if (FakeNewOrganizationModel == null)
             {
                 var controller = new CmsWeb.Areas.Dialog.Controllers.AddOrganizationController(FakeRequestManager.FakeRequest());
                 var routeDataValues = new Dictionary<string, string> { { "controller", "AddOrganization" } };
                 controller.ControllerContext = ControllerTestUtils.FakeContextController(controller, routeDataValues);
 
                 var NewOrganizationIndex = controller.Index();
-                var OrgId = ((CmsWeb.Areas.Dialog.Models.NewOrganizationModel)((System.Web.Mvc.ViewResultBase)NewOrganizationIndex).Model).OrganizationId;
+                OrgId = ((NewOrganizationModel)((System.Web.Mvc.ViewResultBase)NewOrganizationIndex).Model).OrganizationId;
 
                 FakeNewOrganizationModel = new NewOrganizationModel();
-                FakeOrganization = new Organization() { OrganizationName = "MockName", RegistrationTitle = "MockTitle", Location = "MockLocation", RegistrationTypeId = 8 };
+                if(Orgconfig.IsNull())
+                {
+                    FakeOrganization = new Organization() { OrganizationName = "MockName", RegistrationTitle = "MockTitle", Location = "MockLocation", RegistrationTypeId = 8 };
+                }
+                else
+                {
+                    FakeOrganization = Orgconfig;
+                }
 
                 FakeNewOrganizationModel.org = FakeOrganization;
 
                 controller.Submit((int)OrgId, FakeNewOrganizationModel);
+            }
+            else if (!OrgExists(OrgId))
+            {
+                FakeNewOrganizationModel = null;
+                MakeFakeOrganization();
             }
             return FakeNewOrganizationModel;
         }
@@ -46,11 +57,17 @@ namespace CMSWebTests
 
         public static void DeleteOrg(int OrgId)
         {
-            var controller = new CmsWeb.Areas.Org.Controllers.OrgController(FakeRequestManager.FakeRequest());
-            var routeDataValues = new Dictionary<string, string> { { "controller", "Org" } };
-            controller.ControllerContext = ControllerTestUtils.FakeContextController(controller, routeDataValues);
+            if (OrgExists(OrgId))
+            {
+                FakeOrganization.PurgeOrg(DbUtil.Db);
+                FakeNewOrganizationModel = null;
+            }
+        }
 
-            controller.Delete(OrgId);
+        private static bool OrgExists(int? OrgId)
+        {
+            DbUtil.Db = CMSDataContext.Create(Util.Host);
+            return DbUtil.Db.Organizations.Where(x => x.OrganizationId == OrgId).IsNotNull() ? true : false;
         }
     }
 }
