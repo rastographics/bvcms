e3f64fb12c748a2ea80fb3294f4781aaec1c5301
Ben <ben@clearfunction.com>
2019-08-21 12:04:22 -0500

    Add unit tests for SettingsModel

---
+++ UnitTests/CMSWebTests/Areas/Setup/Models/SettingModelTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CmsData;
+using CmsWeb.Areas.Setup.Models;
+using Shouldly;
+using Xunit;
+
+namespace CMSWebTests.Areas.Setup.Models
+{
+    public class SettingModelTests
+    {
+        [Fact]
+        public void ShouldFindEnumTypeValue()
+        {
+            var boolean = (int) SettingDataType.Boolean;
+            boolean.ShouldBe(1);
+
+            var date = (int)SettingDataType.Date;
+            date.ShouldBe(2);
+        }
+
+        [Fact]
+        public void ShouldInstantiateListProperties()
+        {
+            var model = new SettingModel();
+
+            model.Settings.ShouldBeOfType<List<Setting>>();
+            model.Settings.Count().ShouldBe(0);
+
+            model.SettingTypes.ShouldBeOfType<List<SettingTypeModel>>();
+            model.SettingTypes.Count().ShouldBe(0);
+        }
+    }
+}
