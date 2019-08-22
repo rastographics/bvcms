e22aa0c43d509fa1129ea9524e929067733dfa48
Jose Armando Hernandez Flores <josearmanhf@gmail.com>
2019-08-22 13:26:08 -0500

    removing parameter after using latest version of MakeFakeOrganization

--- UnitTests/CMSWebTests/Areas/OnlineReg/Models/NewAskChurchQuestionTest.cs
+++ UnitTests/CMSWebTests/Areas/OnlineReg/Models/NewAskChurchQuestionTest.cs
@@ -29,7 +29,7 @@ namespace CMSWebTests.Areas.OnlineReg.Models.AskChurch
             var routeDataValues = new Dictionary<string, string> { { "controller", "OnlineReg" } };
             controller.ControllerContext = ControllerTestUtils.FakeContextController(controller, routeDataValues);
 
-            var FakeOrg = FakeOrganizationUtils.MakeFakeOrganization(RegistrationTypeCode.OnlineGiving);
+            var FakeOrg = FakeOrganizationUtils.MakeFakeOrganization();
             OrgId = FakeOrg.org.OrganizationId;
 
             var model = FakeOrganizationUtils.GetFakeOnlineRegModel(OrgId);
