a2d8b2e5f32c3693ca07771b28f32662dc9759ec
Jose Armando Hernandez Flores <josearmanhf@gmail.com>
2019-08-22 11:57:19 -0500

    Adding unit test for Volunteer calendar Updating method MakeFakeOrganization to accept RegistrationTypeCode param. Adding method SetFakeSettings for Organization Settings details.

---
+++ UnitTests/CMSWebTests/Areas/Manage/Models/VolunteerCommitmentsModelTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CmsData.Codes;
+using CmsWeb.Areas.Manage.Models;
+
+namespace CMSWebTests.Areas.Manage.Models
+{
+    public class VolunteerCommitmentsModelTest
+    {
+        [Fact]
+        public void ShouldCalculateFirstSundayOfTheMonth()
+        {            
+            var FakeOrg = FakeOrganizationUtils.MakeFakeOrganization(RegistrationTypeCode.ChooseVolunteerTimes);            
+            var m = new VolunteerCommitmentsModel(FakeOrg.org.OrganizationId);            
+            var calculatedSunday = m.GetFirstSundayOfTheMonth(DateTime.Now.Year, DateTime.Now.Month);
+            Assert.Equal(DayOfWeek.Sunday, calculatedSunday.DayOfWeek);
+            Assert.True(calculatedSunday.Day < 8, "The calculated Sunday did not fall in the first week of the month");
+            FakeOrganizationUtils.DeleteOrg(FakeOrg.org.OrganizationId);
+        }
+    }
+}
--- UnitTests/CMSWebTests/TestUtils/FakeOrganizationUtils.cs
+++ UnitTests/CMSWebTests/TestUtils/FakeOrganizationUtils.cs
@@ -1,4 +1,6 @@
 ﻿using CmsData;
+using CmsData.Codes;
+using CmsData.Registration;
 using CmsWeb.Areas.Dialog.Models;
 using System.Collections.Generic;
 using CmsWeb.Areas.OnlineReg.Models;
@@ -15,7 +17,7 @@ namespace CMSWebTests
         public static Organization FakeOrganization { get; set; }
         public static int? OrgId { get; set; }
 
-        public static NewOrganizationModel MakeFakeOrganization()
+        public static NewOrganizationModel MakeFakeOrganization(int registrationType)
         {
             if (FakeNewOrganizationModel == null)
             {
@@ -27,20 +29,37 @@ namespace CMSWebTests
                 OrgId = ((NewOrganizationModel)((System.Web.Mvc.ViewResultBase)NewOrganizationIndex).Model).OrganizationId;
 
                 FakeNewOrganizationModel = new NewOrganizationModel();
-                FakeOrganization = new Organization() { OrganizationName = "MockName", RegistrationTitle = "MockTitle", Location = "MockLocation", RegistrationTypeId = 8 };
+                FakeOrganization = new Organization() { OrganizationName = "MockName", RegistrationTitle = "MockTitle", Location = "MockLocation", RegistrationTypeId = registrationType };
+
+                //Add fake registration settings
+                SetFakeSettings(registrationType, FakeOrganization.OrganizationId);
 
                 FakeNewOrganizationModel.org = FakeOrganization;
-
                 controller.Submit((int)OrgId, FakeNewOrganizationModel);
             }
             else if (!OrgExists(OrgId))
             {
                 FakeNewOrganizationModel = null;
-                MakeFakeOrganization();
+                MakeFakeOrganization(registrationType );
             }
             return FakeNewOrganizationModel;
         }
+        public static Settings SetFakeSettings(int regType, int orgId)
+        {
+            var m = new Settings();
 
+            if (regType == RegistrationTypeCode.ChooseVolunteerTimes)
+            {
+                var timeSlots = new TimeSlots();
+                var ts1 = new TimeSlots.TimeSlot() { DayOfWeek = 0, Name = "MockTimeSlot", Time = System.DateTime.Now };
+                timeSlots.list.Add(ts1);
+                m.TimeSlots = timeSlots;
+            }
+
+            m.OrgId = orgId;
+
+            return m;
+        }
         public static OnlineRegModel GetFakeOnlineRegModel(int OrgId)
         {
             var m = new OnlineRegModel(ContextTestUtils.FakeHttpContext().Request, CMSDataContext.Create(Util.Host), OrgId, null, null, null, null);
