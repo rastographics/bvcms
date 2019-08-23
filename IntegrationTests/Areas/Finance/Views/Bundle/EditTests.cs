61161d4087ce03b10f03dd3f39388be5698811a9
jfescobar18 <jfescobar18@hotmail.com>
2019-08-22 19:37:53 -0500

    Integration Test: Create User Admin and go to Bundles

---
+++ IntegrationTests/Areas/Finance/Views/Bundle/EditTests.cs
@@ -0,0 +1,29 @@
+﻿using IntegrationTests.Support;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IntegrationTests.Areas.Finance.Views.Bundle
+{
+    [Collection("WebApp Collection")]
+    public class EditTests : AccountTestBase
+    {
+        [Fact]
+        public void Should_Open_Datepicker_On_Mobile_Resolutions()
+        {
+            driver.Manage().Window.Size = new Size(768, 1024);
+
+            username = RandomString();
+            password = RandomString();
+            string roleName = "role_" + RandomString();
+            var user = CreateUser(roles: new string[] { "Access", "Edit", "Admin" });
+            Login();
+
+            Open($"{rootUrl}Bundles/");
+        }
+    }
+}
