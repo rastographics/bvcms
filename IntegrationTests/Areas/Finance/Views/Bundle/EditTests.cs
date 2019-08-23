b54a12fb13793a9e5945f592c5d85799e335bcc3
jfescobar18 <jfescobar18@hotmail.com>
2019-08-23 10:25:04 -0500

    Find calendar control and click it

--- IntegrationTests/Areas/Finance/Views/Bundle/EditTests.cs
+++ IntegrationTests/Areas/Finance/Views/Bundle/EditTests.cs
@@ -1,4 +1,8 @@
-﻿using IntegrationTests.Support;
+﻿using CmsData;
+using IntegrationTests.IntegrationTestsUtils;
+using IntegrationTests.Support;
+using OpenQA.Selenium;
+using Shouldly;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -15,15 +19,19 @@ namespace IntegrationTests.Areas.Finance.Views.Bundle
         [Fact]
         public void Should_Open_Datepicker_On_Mobile_Resolutions()
         {
-            driver.Manage().Window.Size = new Size(768, 1024);
+            driver.Manage().Window.Size = new Size(375, 667);
 
             username = RandomString();
             password = RandomString();
             string roleName = "role_" + RandomString();
-            var user = CreateUser(roles: new string[] { "Access", "Edit", "Admin" });
+            var user = CreateUser(roles: new string[] { "Access", "Edit", "Admin", "Finance" });
             Login();
 
-            Open($"{rootUrl}Bundles/");
+            Open($"{rootUrl}Bundle/Edit/{new FinanceTestUtils(db).BundleHeader.BundleHeaderId}");
+            PageSource.ShouldContain("Contribution Bundle");
+
+            Find(css: "span.input-group-addon").Click();
+            Find(css: "div.bootstrap-datetimepciker-widget").ShouldNotBeNull();
         }
     }
 }
---
+++ IntegrationTests/IntegrationTestsUtils/FinanceTestUtils.cs
@@ -0,0 +1,30 @@
+﻿using CmsData;
+using CmsDataTests.Support;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IntegrationTests.IntegrationTestsUtils
+{
+    [Collection("Database collection")]
+    public class FinanceTestUtils : FinanceTestBase
+    {
+        private CMSDataContext db;
+
+        public FinanceTestUtils(CMSDataContext db)
+        {
+            this.db = db;
+        }
+
+        public BundleHeader BundleHeader
+        {
+            get
+            {
+                return CreateBundle(db, null);
+            }
+        }
+    }
+}
