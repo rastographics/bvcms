c4e482e5c6d027a5a91389af70786525314fa812
David Carroll <david@bvcms.com>
2019-08-27 15:45:25 -0500

    Tests ModelBinding with DbBinder and SmartBinder

---
+++ IntegrationTests/Areas/General/TestQueryController.cs
@@ -0,0 +1,33 @@
+﻿using CmsData;
+using IntegrationTests.Support;
+using Shouldly;
+using UtilityExtensions;
+using Xunit;
+using IntegrationTests.Resources;
+using SharedTestFixtures;
+
+namespace IntegrationTests.Areas.Manage
+{
+    [Collection(Collections.Webapp)]
+    public class TestQueryController : AccountTestBase
+    {
+        /// <summary>
+        /// This verifies that when QueryModel m is bound in POST operation
+        /// that it no longer needs to have m.Db = CurrentDatabase
+        /// as this is now done in SmartBinder with an inherited class called DbBinder
+        /// This is applicable wherever this pattern is found on any model binding that needs CurrentDatabase
+        /// The POST ActionResult exercised is QueryController.Results(QueryModel m)
+        /// </summary>
+        [Fact]
+        public void DbInQueryModelShouldBeInstantiatedByModelBinding()
+        {
+            username = RandomString();
+            password = RandomString();
+            CreateUser(roles: new[] { "Access", "Edit", "Admin" });
+            Login();
+            Open($"{rootUrl}QueryCode?code=Age>65");
+            WaitForElement("input[id=totcnt]");
+            PageSource.ShouldContain("David Carroll");
+        }
+    }
+}
