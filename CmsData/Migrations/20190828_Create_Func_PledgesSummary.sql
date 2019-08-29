adf9205e6fb725465f25fb61304e8ce4b03367f5
Oscar Daniel Báez Lara <dahnbaez@gmail.com>
2019-08-29 09:55:26 -0500

    Add db function PledgesSummary Add Data Object for PledgesSummary

---
+++ CmsData/Migrations/20190828_Create_Func_PledgesSummary.sql
@@ -0,0 +1,40 @@
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE FUNCTION [dbo].[PledgesSummary] ( @pid INT )
+RETURNS TABLE 
+AS
+RETURN 
+(
+	WITH Pledged(FundId, FundName, AmountPledged, ContributionDescription)
+	AS
+	(
+		SELECT cf.FundId, cf.FundName, SUM(c.ContributionAmount), ct.Description
+		FROM Contribution c
+		JOIN ContributionFund cf ON cf.FundId = c.FundId
+		JOIN lookup.ContributionType ct ON c.ContributionTypeId = ct.Id 
+		WHERE ct.Description = 'Pledge'
+		AND c.PeopleId = @pid
+		GROUP BY cf.FundId, cf.FundName, ct.Description
+	)
+	,Contributed(FundId, FundName, AmountContributed) AS
+	(
+		SELECT cf.FundId, cf.FundName, SUM(c.ContributionAmount)
+		FROM Contribution c
+		JOIN ContributionFund cf ON cf.FundId = c.FundId
+		JOIN lookup.ContributionType ct ON c.ContributionTypeId = ct.Id 
+		WHERE ct.Description <> 'Pledge'
+		AND c.PeopleId = @pid
+		GROUP BY cf.FundId, cf.FundName, ct.Description
+	)
+	SELECT 
+	ple.FundId AS FundId, 
+	ple.FundName AS FundName, 
+	ple.AmountPledged AS AmountPledged, 
+	con.AmountContributed AS AmountContributed,  
+	(IIF(ple.AmountPledged - con.AmountContributed < 0, 0, ple.AmountPledged - con.AmountContributed)) AS Balance
+	FROM Pledged ple
+	JOIN Contributed con ON con.FundId = ple.FundId
+)
