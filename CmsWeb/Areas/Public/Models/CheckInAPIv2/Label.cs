a52fae1c3ff614e25a8da443e2bb08bd32530759
Steven Yarbrough <steven.yarbrough@touchpointsoftware.com>
2019-09-04 11:12:03 -0500

    Updates to printing routines for Check-In API v2 Added AttendanceCacheSet to streamline data access Revised function/method locations for better flow

--- CmsWeb/Areas/Public/Models/CheckInAPIv2/Label.cs
+++ CmsWeb/Areas/Public/Models/CheckInAPIv2/Label.cs
@@ -1,32 +1,28 @@
 ﻿using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using CmsWeb.Areas.Public.Models.CheckInAPIv2.Caching;
 
 namespace CmsWeb.Areas.Public.Models.CheckInAPIv2
 {
-	[SuppressMessage( "ReSharper", "CollectionNeverQueried.Global" )]
-	[SuppressMessage( "ReSharper", "UnusedMember.Global" )]
-	[SuppressMessage( "ReSharper", "NotAccessedField.Global" )]
-	[SuppressMessage( "ReSharper", "MemberCanBePrivate.Global" )]
+	[SuppressMessage( "ReSharper", "CollectionNeverQueried.Global" ), SuppressMessage( "ReSharper", "UnusedMember.Global" ), SuppressMessage( "ReSharper", "NotAccessedField.Global" ), SuppressMessage( "ReSharper", "MemberCanBePrivate.Global" )]
 	public class Label
 	{
 		public readonly List<LabelEntry> entries = new List<LabelEntry>();
 
-		public static List<Label> generate( Dictionary<int, LabelFormat> formats, Type type, Attendance attendance, List<AttendanceGroup> groups )
+		public static List<Label> generate( AttendanceCacheSet cacheSet, Type type, Attendance attendance, List<AttendanceGroup> groups )
 		{
 			List<Label> labels = new List<Label>();
-			LabelFormat format = formats[(int) type];
+			LabelFormat format = cacheSet.formats[(int) type];
 
 			List<AttendanceGroup> groupsCopy = new List<AttendanceGroup>( groups );
 
 			if( format.canRepeat ) {
 				do {
-					labels.Add( getLabel( format, attendance, nextGroups( groupsCopy, format.maxRepeat() ) ) );
+					labels.Add( getLabel( cacheSet, format, attendance, nextGroups( groupsCopy, format.maxRepeat() ) ) );
 				} while( groupsCopy.Any() );
 			} else {
-				labels.Add( getLabel( format, attendance, nextGroups( groupsCopy, 1 ) ) );
+				labels.Add( getLabel( cacheSet, format, attendance, nextGroups( groupsCopy, 1 ) ) );
 			}
 
 			return labels;
@@ -43,21 +39,23 @@ namespace CmsWeb.Areas.Public.Models.CheckInAPIv2
 			return groupsCopy;
 		}
 
-		private static Label getLabel( LabelFormat format, Attendance attendance, List<AttendanceGroup> groups )
+		private static Label getLabel( AttendanceCacheSet cacheSet, LabelFormat format, Attendance attendance, List<AttendanceGroup> groups )
 		{
 			Label label = new Label();
 
 			foreach( LabelFormatEntry formatEntry in format.entries ) {
 				if( formatEntry.repeat > 1 ) {
 					for( int iX = 0; iX < formatEntry.repeat; iX++ ) {
-						if( groups.Count <= iX ) break;
+						if( groups.Count <= iX ) {
+							break;
+						}
 
-						LabelEntry entry = new LabelEntry( formatEntry, attendance, groups[iX], iX );
+						LabelEntry entry = new LabelEntry( cacheSet, formatEntry, attendance, groups[iX], iX );
 
 						label.entries.Add( entry );
 					}
 				} else {
-					label.entries.Add( new LabelEntry( formatEntry, attendance, groups[0] ) );
+					label.entries.Add( new LabelEntry( cacheSet, formatEntry, attendance, groups[0] ) );
 				}
 			}
 
