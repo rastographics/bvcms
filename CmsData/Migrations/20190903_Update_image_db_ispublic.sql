bc1e507c8acff17be60f68d690d99107b38f8312
Bron Davies <bron@brontech.com>
2019-09-06 15:18:27 -0500

    Fix issues in last deployment

--- CmsData/Migrations/20190903_Update_image_db_ispublic.sql
+++ CmsData/Migrations/20190903_Update_image_db_ispublic.sql
@@ -2,11 +2,11 @@
 DECLARE @s nvarchar(1000);
 
 SELECT @dbname = SUBSTRING(db_name(), 5, 100);
-SELECT @s = ('IF NOT EXISTS(SELECT 1 FROM CMSi_'+@dbname+'.INFORMATION_SCHEMA.COLUMNS 
+SELECT @s = ('IF NOT EXISTS(SELECT 1 FROM [CMSi_'+@dbname+'].INFORMATION_SCHEMA.COLUMNS 
 	WHERE TABLE_NAME = ''Image'' 
 	AND COLUMN_NAME=''IsPublic'')
 BEGIN
-    ALTER TABLE CMSi_'+@dbname+'.dbo.Image
+    ALTER TABLE [CMSi_'+@dbname+'].dbo.Image
     ADD IsPublic BIT NOT NULL DEFAULT 0
 END');
 
