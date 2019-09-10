9d4f9e5f141a29b13c0fb7595f5ca5ecbf6930b0
Bron Davies <bron@brontech.com>
2019-09-09 22:17:02 -0500

    Fix test

--- IntegrationTests/Areas/Setup/SetupTests.cs
+++ IntegrationTests/Areas/Setup/SetupTests.cs
@@ -16,7 +16,7 @@ namespace IntegrationTests.Areas.Manage
             username = RandomString();
             password = RandomString();
             string roleName = "role_" + RandomString();
-            var user = CreateUser(roles: new string[] { "Access", "Edit", "Admin" });
+            var user = CreateUser(username, password, roles: new string[] { "Access", "Edit", "Admin" });
             Login();
 
             Open($"{rootUrl}Lookups/");
@@ -44,7 +44,7 @@ namespace IntegrationTests.Areas.Manage
         {
             username = RandomString();
             password = RandomString();
-            var user = CreateUser(roles: new string[] { "Access", "Edit", "Admin" });
+            var user = CreateUser(username, password, roles: new string[] { "Access", "Edit", "Admin" });
             Login();
             Toggle_NavTabs_Setting();   // test both states
             Toggle_NavTabs_Setting();
