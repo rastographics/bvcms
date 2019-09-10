7e4c80e1cb77f756b3604ae6ebfd2ba1ba81c4cc
jfescobar18 <jfescobar18@hotmail.com>
2019-09-10 16:44:17 -0500

    Integration Test Added

---
+++ IntegrationTests/Areas/People/Views/Person/PersonViewsTests.cs
@@ -0,0 +1,40 @@
+﻿using IntegrationTests.Support;
+using SharedTestFixtures;
+using Shouldly;
+using System;
+using Xunit;
+using CMSWebTests;
+
+namespace IntegrationTests.Areas.People.Views.Person
+{
+    [Collection(Collections.Webapp)]
+    public class PersonViewsTests : AccountTestBase
+    {
+        [Fact]
+        public void Should_Hide_Giving_Tab()
+        {
+            SettingUtils.InsertSetting("HideGivingTabMyDataUsers", "false");
+
+            username = RandomString();
+            password = RandomString();
+
+            var user = CreateUser(username, password);
+            Login();
+
+            Open($"{rootUrl}Person2/{user.PeopleId}#tab-personal");
+            WaitForElement(".active:nth-child(2) > a", 5);
+            PageSource.ShouldContain("<a href=\"#giving\" aria-controls=\"giving\" data-toggle=\"tab\">Giving</a>");
+
+            SettingUtils.EditSetting("HideGivingTabMyDataUsers", "true");
+
+            Open($"{rootUrl}Person2/{user.PeopleId}");
+            WaitForElement(".active:nth-child(2) > a", 5);
+            PageSource.ShouldNotContain("<a href=\"#giving\" aria-controls=\"giving\" data-toggle=\"tab\">Giving</a>");
+        }
+
+        public override void Dispose()
+        {
+            SettingUtils.DeleteSetting("HideGivingTabMyDataUsers");
+        }
+    }
+}
---
+++ UnitTests/CMSWebTests/TestUtils/SettingUtils.cs
@@ -0,0 +1,50 @@
+﻿using CmsData;
+using CmsWeb.Lifecycle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMSWebTests
+{
+    public static class SettingUtils
+    {
+        public static void InsertSetting(string id, string value = null)
+        {
+            var requestManager = FakeRequestManager.Create();
+            var db = requestManager.CurrentDatabase;
+
+            if (!db.Settings.Any(s => s.Id == id))
+            {
+                var m = new Setting { Id = id };
+                db.Settings.InsertOnSubmit(m);
+                db.SubmitChanges();
+                db.SetSetting(id, value);
+            }
+            else
+            {
+                EditSetting(id, value);
+            }
+        }
+
+        public static void EditSetting(string id, string value)
+        {
+            var requestManager = FakeRequestManager.Create();
+            var db = requestManager.CurrentDatabase;
+
+            db.SetSetting(id, value);
+            db.SubmitChanges();
+        }
+
+        public static void DeleteSetting(string id)
+        {
+            var requestManager = FakeRequestManager.Create();
+            var db = requestManager.CurrentDatabase;
+
+            var set = db.Settings.SingleOrDefault(m => m.Id == id);
+            db.Settings.DeleteOnSubmit(set);
+            db.SubmitChanges();
+        }
+    }
+}
