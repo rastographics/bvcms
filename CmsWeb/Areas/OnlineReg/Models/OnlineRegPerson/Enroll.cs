31fca3bd40825ccdf718451c84a423f82855427b
Bron Davies <bron@brontech.com>
2019-09-09 22:02:36 -0500

    Compiler fixes

--- CmsWeb/Areas/OnlineReg/Models/OnlineRegPerson/Enroll.cs
+++ CmsWeb/Areas/OnlineReg/Models/OnlineRegPerson/Enroll.cs
@@ -11,7 +11,7 @@ namespace CmsWeb.Areas.OnlineReg.Models
 {
     public partial class OnlineRegPersonModel
     {
-        public CMSDataContext db => CurrentDatabase ?? (CurrentDatabase = DbUtil.Db);
+        private CMSDataContext db => CurrentDatabase ?? (CurrentDatabase = DbUtil.Db);
 
         public OrganizationMember Enroll(Transaction transaction, string payLink)
         {
