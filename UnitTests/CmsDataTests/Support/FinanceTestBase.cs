77c352021a36cf935fbe1df2f3178a9de7beaa5c
Jose Armando Hernandez Flores <josearmanhf@gmail.com>
2019-09-11 20:02:55 -0500

    Adding unit test for TotalsByFund exports some datacontext refactor

--- UnitTests/CmsDataTests/Support/FinanceTestBase.cs
+++ UnitTests/CmsDataTests/Support/FinanceTestBase.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UtilityExtensions;
 
 namespace CmsDataTests.Support
 {
@@ -83,10 +84,50 @@ namespace CmsDataTests.Support
             foreach (var item in bundleDetails)
             {
                 var contribution = db.Contributions.SingleOrDefault(c => c.ContributionId == item.ContributionId);
-                db.Contributions.DeleteOnSubmit(contribution);    
+                db.Contributions.DeleteOnSubmit(contribution);
             }
             db.BundleDetails.DeleteAllOnSubmit(bundleDetails);
             db.BundleHeaders.DeleteOnSubmit(bundleHeader);
+            db.SubmitChanges();
+        }
+
+        protected static Contribution CreateContributionRecord(Contribution c)
+        {
+            var now = Util.Now;
+            var r = new Contribution
+            {
+                ContributionStatusId = ContributionStatusCode.Recorded,
+                CreatedBy = Util.UserId1,
+                CreatedDate = now,
+                PeopleId = c.PeopleId,
+                ContributionAmount = c.ContributionAmount,
+                ContributionDate = now.Date,
+                PostingDate = now,
+                FundId = c.FundId,
+            };
+            return r;
+        }
+
+        protected void ReturnContribution(CMSDataContext db, int cid)
+        {
+            var c = db.Contributions.Single(ic => ic.ContributionId == cid);
+            var r = CreateContributionRecord(c);
+            c.ContributionStatusId = ContributionStatusCode.Returned;
+            r.ContributionTypeId = ContributionTypeCode.ReturnedCheck;
+            r.ContributionDesc = "Returned Check for Contribution Id = " + c.ContributionId;
+
+            db.Contributions.InsertOnSubmit(r);
+            db.SubmitChanges();
+        }
+
+        protected void ReverseContribution(CMSDataContext db, int cid)
+        {
+            var c = db.Contributions.Single(ic => ic.ContributionId == cid);
+            var r = CreateContributionRecord(c);
+            c.ContributionStatusId = ContributionStatusCode.Reversed;
+            r.ContributionTypeId = ContributionTypeCode.Reversed;
+            r.ContributionDesc = "Reversed Contribution Id = " + c.ContributionId;
+            db.Contributions.InsertOnSubmit(r);
             db.SubmitChanges();
         }
     }
