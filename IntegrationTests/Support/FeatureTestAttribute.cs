095c624d5c1236b2e68170e436487381e3171941
Bron Davies <bron@brontech.com>
2019-09-25 12:17:28 -0500

    Always take a screenshot after feature test

--- IntegrationTests/Support/FeatureTestAttribute.cs
+++ IntegrationTests/Support/FeatureTestAttribute.cs
@@ -1,38 +1,13 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 using Xunit.Sdk;
 
 namespace IntegrationTests.Support
 {
-    [AttributeUsage(AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
     public class FeatureTestAttribute : BeforeAfterTestAttribute
-    {
-        private bool takeScreenshot;
-
-        public FeatureTestAttribute()
-        {
-        }
-
-        public override void Before(MethodInfo methodUnderTest)
-        {
-            try
-            {
-                base.Before(methodUnderTest);
-            }
-            catch
-            {
-                takeScreenshot = true;
-                throw;
-            }
-        }
-
+    {   
         public override void After(MethodInfo methodUnderTest)
         {
-            if (takeScreenshot)
-            {
-                FeatureTestBase.Current?.SaveScreenshot();
-            }
-            base.After(methodUnderTest);
+            FeatureTestBase.Current?.SaveScreenshot();
         }
     }
 }
