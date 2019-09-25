ed4f7c1b491d33d8c1f87feea722abb6e255f221
Bron Davies <bron@brontech.com>
2019-09-25 14:49:40 -0500

    Merge develop into feature/2fa-with-google-authenticator

--- IntegrationTests/Support/FeatureTestBase.cs
+++ IntegrationTests/Support/FeatureTestBase.cs
@@ -245,7 +245,7 @@ namespace IntegrationTests.Support
             string file = $"{name}_" + DateTime.Now.ToString("yyyy-MM-dd_HH.mm.ss_") + RandomString() + ".png";
             string filename = Path.Combine(Settings.ScreenShotLocation, file);
             screenshot.SaveAsFile(filename, ScreenshotImageFormat.Png);
-            Console.WriteLine("Screen shot saved: {0}", Path.Combine(Settings.ScreenShotUrl, filename));
+            Console.WriteLine("Screen shot saved: {0}", Path.Combine(Settings.ScreenShotUrl, file));
         }
 
         protected IEnumerable<IWebElement> FindAll(By by = null,
