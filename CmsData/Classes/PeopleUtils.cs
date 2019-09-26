2f66eb75477d2c9e88006231b05f4db9b0b09147
Oscar Daniel Báez Lara <dahnbaez@gmail.com>
2019-09-25 20:01:51 -0500

    Add static method to GetParentsIds from a People IQueryble

---
+++ CmsData/Classes/PeopleUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CmsData
+{
+    public class PeopleUtils
+    {
+        public static IQueryable<int> GetParentsIds(IQueryable<Person> q)
+        {
+            var q2 = from p in q
+                     from m in p.Family.People
+                     where m.PositionInFamilyId == 10
+                     //					 where (m.PositionInFamilyId == 10 && p.PositionInFamilyId != 10)
+                     //					 || (m.PeopleId == p.PeopleId && p.PositionInFamilyId == 10)
+                     where m.DeceasedDate == null
+                     select m.PeopleId;
+
+            return q2;
+        }
+    }
+}
