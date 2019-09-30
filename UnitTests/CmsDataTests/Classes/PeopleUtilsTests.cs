2758e26d20bbe25d82967be0939742a651d614f6
Oscar Daniel Báez Lara <dahnbaez@gmail.com>
2019-09-30 14:31:48 -0500

    Added ids to mock people query.

--- UnitTests/CmsDataTests/Classes/PeopleUtilsTests.cs
+++ UnitTests/CmsDataTests/Classes/PeopleUtilsTests.cs
@@ -31,7 +31,7 @@ namespace CmsDataTests
         public static IEnumerable<object[]> Data_GetParentsIdsTest =>
             new List<object[]>
             {
-                new object[] { GenerateChildrenWithParents(10), 20 },
+                new object[] { GenerateChildrenWithParents(0, 10), 20 },
                 new object[] { GeneratePeople(10, 5, 3), 28 }
             };
 
@@ -43,52 +43,60 @@ namespace CmsDataTests
 
         private static IQueryable<Person> GeneratePeople(int numberOfChilden, int numberOfPrimary, int numberOfSecondary)
         {
-            var people = GenerateChildrenWithParents(numberOfChilden).ToList();
-            var primary = GenerateAdults(numberOfPrimary, PositionInFamily.PrimaryAdult);
-            var secondary = GenerateAdults(numberOfSecondary, PositionInFamily.SecondaryAdult);
+            var id = 0;
+            var people = GenerateChildrenWithParents(id, numberOfChilden).ToList();
+            id += numberOfChilden;
+            var primary = GenerateAdults(id, numberOfPrimary, PositionInFamily.PrimaryAdult);
+            id += numberOfPrimary;
+            var secondary = GenerateAdults(id, numberOfSecondary, PositionInFamily.SecondaryAdult);
             people.AddRange(primary);
             people.AddRange(secondary);
             return people.AsQueryable();
         }
 
-        private static List<Person> GenerateAdults(int numberOfAdults, int positionInFamily)
+        private static List<Person> GenerateAdults(int initialId, int numberOfAdults, int positionInFamily)
         {
+            var id = initialId;
             var adults = new List<Person>();
             for (int i = 0; i < numberOfAdults; i++)
             {
-                adults.Add(CreatePerson(positionInFamily));
+                adults.Add(CreatePerson(id, positionInFamily));
+                id++;
             }
             return adults;
         }
 
-        public static IQueryable<Person> GenerateChildrenWithParents(int numberOfChildren)
+        public static IQueryable<Person> GenerateChildrenWithParents(int initialId, int numberOfChildren)
         {
+            var id = initialId;
             List<Person> children = new List<Person>();
             for (int i = 0; i < numberOfChildren; i++)
             {
-                var child = CreatePerson(PositionInFamily.Child);
+                var child = CreatePerson(id, PositionInFamily.Child);
                 children.Add(child);
-                var father = CreatePerson(PositionInFamily.PrimaryAdult, child.Family);
+                var father = CreatePerson(id, PositionInFamily.PrimaryAdult, child.Family);
                 father.Family.HeadOfHousehold = father;
                 children.Add(father);
-                var mother = CreatePerson(PositionInFamily.PrimaryAdult, child.Family);
+                var mother = CreatePerson(id, PositionInFamily.PrimaryAdult, child.Family);
                 mother.Family.HeadOfHouseholdSpouseId = mother.PeopleId;
                 children.Add(mother);
+                id++;
             }
             return children.AsQueryable();
         }
 
-        public static Person CreatePerson(int positionInFamily, Family family = null)
+        public static Person CreatePerson(int id, int positionInFamily, Family family = null)
         {
             if (family == null)
             {
                 family = new Family()
                 {
-                    CreatedDate = DateTime.Now                    
+                    CreatedDate = DateTime.Now
                 };
             }
             var child = new Person
             {
+                PeopleId = id,
                 Family = family,
                 FirstName = DatabaseTestBase.RandomString(),
                 LastName = DatabaseTestBase.RandomString(),
