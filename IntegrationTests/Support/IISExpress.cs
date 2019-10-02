1aa24caf882fd49fa5b76e90f309c1ce4f5fe3f2
Bron Davies <bron@brontech.com>
2019-10-02 18:23:54 -0500

    New test file to define test runs on build server

--- IntegrationTests/Support/IISExpress.cs
+++ IntegrationTests/Support/IISExpress.cs
@@ -68,14 +68,22 @@ namespace IntegrationTests.Support
                 if (!string.IsNullOrEmpty(AppPool))
                     arguments.AppendFormat("/{0}:{1} ", APP_POOL, AppPool);
 
+                var hostExe = Environment.GetEnvironmentVariable("IISEXPRESS_HOST") ?? IIS_EXPRESS;
+                var hostArguments = string.Format(Environment.GetEnvironmentVariable("IISEXPRESS_ARGS") ?? "{0}", arguments.ToString().Trim());
+                File.WriteAllText(@"C:\temp\test.log", $"EXE:{hostExe}\r\nARG:{hostArguments}");
                 process = Process.Start(new ProcessStartInfo()
                 {
-                    FileName = IIS_EXPRESS,
-                    Arguments = arguments.ToString(),
+                    FileName = hostExe,
+                    Arguments = hostArguments,
                     UseShellExecute = true,
                     WindowStyle = ProcessWindowStyle.Minimized,
-                    WorkingDirectory = Path.GetDirectoryName(IIS_EXPRESS)
+                    WorkingDirectory = Path.GetDirectoryName(hostExe)
                 });
+                if (processExited(process))
+                {
+                    Console.WriteLine("Failed to launch iisexpress");
+                    Environment.Exit(-1001);
+                }
             }
         }
 
---
+++ test.cmd
@@ -0,0 +1,36 @@
+setlocal
+nuget install Codecov -OutputDirectory packages || exit 1
+nuget install OpenCover -OutputDirectory packages || exit 2
+nuget update Selenium.WebDriver.ChromeDriver
+for /f %%f in ('dir /b packages\opencover.*') do set OpenCover=%~dp0packages\%%f\tools\opencover.console.exe
+for /f %%f in ('dir /b packages\xunit.runner.console.*') do set xunit=%~dp0packages\%%f\tools\net461\xunit.console.x86.exe
+for /f %%f in ('dir /b packages\codecov.*') do set codecov=%~dp0packages\%%f\tools\codecov.exe
+set test_coverage=%~dp0test_coverage.xml
+set opencover_filters="+[*]* -[*Tests]* -[xunit.*]*"
+set iisexpress="%ProgramFiles%\IIS Express\iisexpress.exe"
+echo using %OpenCover%
+echo using %xunit%
+echo using %codecov%
+set target_tests=.\UnitTests\CMSDataTests\bin\Debug\CMSDataTests.dll
+set target_tests=%target_tests% .\UnitTests\CMSWebTests\bin\Debug\CMSWebTests.dll
+set target_tests=%target_tests% .\UnitTests\UtilityExtensionsTests\bin\Debug\UtilityExtensionsTests.dll
+set integration_tests=.\IntegrationTests\bin\Debug\IntegrationTests.dll
+echo quit | sqlcmd -S localhost -q "drop database cms_localhost"
+echo quit | sqlcmd -S localhost -q "drop database cmsi_localhost"
+REM %OpenCover% -register:user -target:"%xunit%" -targetargs:"%target_tests% -noshadow -teamcity" -filter:%opencover_filters% -output:"%test_coverage%" || exit 7
+REM IF NOT EXIST %test_coverage% (
+REM   echo File not found: %test_coverage%
+REM   exit 8
+REM )
+IF "%CodeCovToken%" NEQ "" %codecov% -f "%test_coverage%" -t "%CodeCovToken%"
+set "IISEXPRESS_HOST=%OpenCover%"
+set placeholder="{0}"
+set "IISEXPRESS_ARGS=-register:user -target:%iisexpress% -targetargs:%placeholder% -output:%test_coverage%"
+del %test_coverage%
+%OpenCover% -register:user -target:"%xunit%" -targetargs:"%integration_tests% -noshadow -teamcity" -filter:%opencover_filters% || exit 9
+IF NOT EXIST %test_coverage% (
+  echo File not found: %test_coverage%
+  exit 10
+)
+IF "%CodeCovToken%" NEQ "" %codecov% -f "%test_coverage%" -t "%CodeCovToken%"
+endlocal
