528665b764f02d642e0910468ed94d94775c6121
Bron Davies <bron@brontech.com>
2019-10-14 16:14:20 -0500

    Clean up duplicate Contstants.cs

--- CmsWeb/Code/Contstants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CmsWeb.Constants
-{
-    public class Errors
-    {
-        public const string ModelBindingConstructorError = "Parameterless constructor should only be used for data binding";
-    }
-}
