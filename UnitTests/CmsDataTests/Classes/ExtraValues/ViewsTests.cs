e604ecb052d239e52bb00606bebae8dd71ee29cf
Oscar Daniel Báez Lara <dahnbaez@gmail.com>
2019-10-14 20:27:19 -0500

    Edit Tests

---
+++ UnitTests/CmsDataTests/Classes/ExtraValues/ViewsTests.cs
@@ -0,0 +1,49 @@
+﻿using CmsData;
+using CmsData.Classes.ProtectMyMinistry;
+using CmsData.Codes;
+using CmsData.ExtraValue;
+using SharedTestFixtures;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace CmsDataTests.Classes.ExtraValues
+{
+    public class ViewsTests
+    {
+        //[Fact]
+        //public void Should_GetViews()
+        //{
+        //    using (var db = CMSDataContext.Create(DatabaseFixture.Host))
+        //    {
+        //        var actual = Views.GetViews(db);
+        //        actual.ShouldNotBeNull();
+        //    }
+        //}
+
+        //[Fact]
+        //public void Should_GetStandardExtraValuesOrdered()
+        //{
+        //    using (var db = CMSDataContext.Create(DatabaseFixture.Host))
+        //    {
+        //        var actual = Views.GetStandardExtraValuesOrdered(db, "People", "Entry");
+        //        actual.ShouldNotBeNull();                
+        //    }
+        //}
+
+        //[Fact]
+        //public void Should_Name_Be_Timmed_GetStandardExtraValuesOrdered()
+        //{
+        //    using (var db = CMSDataContext.Create(DatabaseFixture.Host))
+        //    {
+        //        var actual = Views.GetStandardExtraValuesOrdered(db, "People", "Entry");
+        //        foreach (var item in actual)
+        //        {
+        //            item.Name.ShouldBe(item.Name.Trim());
+        //        }
+        //    }
+        //}
+    }
+}
