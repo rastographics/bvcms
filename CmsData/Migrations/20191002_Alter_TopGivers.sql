aa979d9f67618c11cd46946eb55d3c30685ee4ee
jfescobar18 <jfescobar18@hotmail.com>
2019-10-16 09:43:25 -0500

    Remove comments

--- CmsData/Migrations/20191002_Alter_TopGivers.sql
+++ CmsData/Migrations/20191002_Alter_TopGivers.sql
@@ -1,15 +1,6 @@
 ALTER PROCEDURE [dbo].[TopGivers](@top INT, @sdate DATETIME, @edate DATETIME, @fundids VARCHAR(MAX))
 AS
 BEGIN
-
-	--SELECT TOP (@top) c.PeopleId, Name, SUM(ContributionAmount) FROM dbo.People p
-	--JOIN dbo.Contribution c ON p.PeopleId = c.PeopleId
-	--WHERE c.ContributionDate >= @sdate
-	--AND c.ContributionDate <= @edate
-	--AND c.ContributionTypeId NOT IN (6,7,8)
-	--GROUP BY c.PeopleId, Name
-	--ORDER BY SUM(ContributionAmount) DESC
-
 	SELECT TOP (@top)
 	c.CreditGiverId PeopleId, HeadName Name, SUM(Amount) Amount
     FROM dbo.Contributions2(@sdate, @edate, 0, 0, NULL, 1, @fundids) c 
