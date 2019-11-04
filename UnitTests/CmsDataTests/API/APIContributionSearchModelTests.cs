b4b943da0338393521933d6d788f64a3d02384ff
Oscar Daniel Báez Lara <dahnbaez@gmail.com>
2019-11-04 12:36:50 -0600

    Adding unit test

---
+++ UnitTests/CmsDataTests/API/APIContributionSearchModelTests.cs
@@ -0,0 +1,32 @@
+﻿using CmsData;
+using CmsData.API;
+using CmsData.Codes;
+using SharedTestFixtures;
+using Shouldly;
+using System;
+using Xunit;
+
+namespace CmsDataTests.API
+{
+    [Collection(Collections.Database)]
+    public class APIContributionSearchModelTests : IDisposable
+    {
+        [Fact]
+        public void Should_APIContributionSearchModel()
+        {
+            using (var db = CMSDataContext.Create(DatabaseFixture.Host))
+            {
+                ContributionSearchInfo SearchInfo = new ContributionSearchInfo
+                {
+                    Name = "TestName"
+                };
+                var api = new APIContributionSearchModel(db, SearchInfo);
+                api.model.Name.ShouldBe("TestName");
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
