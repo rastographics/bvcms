e6bcd0a5eb43e4043083722077f1893001a32475
Bron Davies <bron@brontech.com>
2019-11-07 09:51:23 -0600

    Add some constructors

--- CmsData/API/PythonModel/PythonModel.Docusign.cs
+++ CmsData/API/PythonModel/PythonModel.Docusign.cs
@@ -1,4 +1,6 @@
-﻿namespace CmsData
+﻿using System.Net;
+
+namespace CmsData
 {
     public partial class PythonModel
     {
@@ -7,9 +9,49 @@
 
         public class ApiDocuSign
         {
+            public DocuSign.eSign.Client.ApiClient ApiClient(string basePath = "https://www.docusign.net/restapi", WebProxy proxy = null)
+            {
+                return new DocuSign.eSign.Client.ApiClient(basePath, proxy);
+            }
+
+            public DocuSign.eSign.Client.Configuration Configuration(DocuSign.eSign.Client.ApiClient apiClient = null)
+            {
+                return new DocuSign.eSign.Client.Configuration(apiClient);
+            }
+
+            public DocuSign.eSign.Model.EnvelopeDefinition EnvelopeDefinition()
+            {
+                return new DocuSign.eSign.Model.EnvelopeDefinition();
+            }
+
+            public DocuSign.eSign.Api.EnvelopesApi EnvelopesApi(DocuSign.eSign.Client.Configuration configuration = null)
+            {
+                return new DocuSign.eSign.Api.EnvelopesApi(configuration);
+            }
+
+            public DocuSign.eSign.Api.EnvelopesApi EnvelopesApi(string basePath)
+            {
+                return new DocuSign.eSign.Api.EnvelopesApi(basePath);
+            }
+
             public DocuSign.eSign.Model.Document Document()
             {
                 return new DocuSign.eSign.Model.Document();
+            }
+
+            public DocuSign.eSign.Api.EnvelopesApi.ListStatusChangesOptions ListStatusChangesOptions()
+            {
+                return new DocuSign.eSign.Api.EnvelopesApi.ListStatusChangesOptions();
+            }
+
+            public DocuSign.eSign.Model.Recipients Recipients()
+            {
+                return new DocuSign.eSign.Model.Recipients();
+            }
+
+            public DocuSign.eSign.Model.RecipientViewRequest RecipientViewRequest()
+            {
+                return new DocuSign.eSign.Model.RecipientViewRequest();
             }
 
             public DocuSign.eSign.Model.Signer Signer()
@@ -22,39 +64,9 @@
                 return new DocuSign.eSign.Model.SignHere();
             }
 
-            public DocuSign.eSign.Model.Recipients Recipients()
-            {
-                return new DocuSign.eSign.Model.Recipients();
-            }
-
-            public DocuSign.eSign.Model.EnvelopeDefinition EnvelopeDefinition()
-            {
-                return new DocuSign.eSign.Model.EnvelopeDefinition();
-            }
-
-            public DocuSign.eSign.Model.RecipientViewRequest RecipientViewRequest()
-            {
-                return new DocuSign.eSign.Model.RecipientViewRequest();
-            }
-
             public DocuSign.eSign.Model.TemplateRole TemplateRole()
             {
                 return new DocuSign.eSign.Model.TemplateRole();
-            }
-
-            public DocuSign.eSign.Api.EnvelopesApi.ListStatusChangesOptions ListStatusChangesOptions()
-            {
-                return new DocuSign.eSign.Api.EnvelopesApi.ListStatusChangesOptions();
-            }
-
-            public DocuSign.eSign.Client.ApiClient ApiClient()
-            {
-                return new DocuSign.eSign.Client.ApiClient();
-            }
-
-            public DocuSign.eSign.Api.EnvelopesApi EnvelopesApi()
-            {
-                return new DocuSign.eSign.Api.EnvelopesApi();
             }
         }
     }
