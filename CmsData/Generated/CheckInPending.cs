b50acb8871437b16765951be97592057b9ab1d6d
Bron Davies <bron@brontech.com>
2019-11-21 17:43:30 -0600

    Merge develop into td-check-in-ui

--- CmsData/Generated/CheckInPending.cs
+++ CmsData/Generated/CheckInPending.cs
@@ -1,150 +1,118 @@
-using System; 
-using System.Data.Linq;
-using System.Data.Linq.Mapping;
-using System.Data;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Linq;
-using System.Linq.Expressions;
+using System;
 using System.ComponentModel;
-using CmsData.Infrastructure;
+using System.Data.Linq.Mapping;
 
 namespace CmsData
 {
-	[Table(Name="dbo.CheckInPending")]
-	public partial class CheckInPending : INotifyPropertyChanging, INotifyPropertyChanged
-	{
-		private static PropertyChangingEventArgs emptyChangingEventArgs = new PropertyChangingEventArgs(String.Empty);
-		
-	#region Private Fields
-		
-		private int _Id;
-		
-		private DateTime _Stamp;
-		
-		private string _Data;
-		
-   		
-    	
-	#endregion
-	
-    #region Extensibility Method Definitions
-    partial void OnLoaded();
-    partial void OnValidate(System.Data.Linq.ChangeAction action);
-    partial void OnCreated();
-		
-		partial void OnIdChanging(int value);
-		partial void OnIdChanged();
-		
-		partial void OnStampChanging(DateTime value);
-		partial void OnStampChanged();
-		
-		partial void OnDataChanging(string value);
-		partial void OnDataChanged();
-		
-    #endregion
-		public CheckInPending()
-		{
-			
-			
-			OnCreated();
-		}
+    [Table(Name = "dbo.CheckInPending")]
+    public partial class CheckInPending : INotifyPropertyChanging, INotifyPropertyChanged
+    {
+        private static PropertyChangingEventArgs emptyChangingEventArgs => new PropertyChangingEventArgs(string.Empty);
 
-		
-    #region Columns
-		
-		[Column(Name="Id", UpdateCheck=UpdateCheck.Never, Storage="_Id", AutoSync=AutoSync.OnInsert, DbType="int NOT NULL IDENTITY", IsPrimaryKey=true, IsDbGenerated=true)]
-		public int Id
-		{
-			get { return this._Id; }
+        #region Private Fields
 
-			set
-			{
-				if (this._Id != value)
-				{
-				
-                    this.OnIdChanging(value);
-					this.SendPropertyChanging();
-					this._Id = value;
-					this.SendPropertyChanged("Id");
-					this.OnIdChanged();
-				}
+        private int _Id;
 
-			}
+        private DateTime _Stamp;
 
-		}
+        private string _Data;
 
-		
-		[Column(Name="Stamp", UpdateCheck=UpdateCheck.Never, Storage="_Stamp", DbType="datetime NOT NULL")]
-		public DateTime Stamp
-		{
-			get { return this._Stamp; }
+        #endregion
 
-			set
-			{
-				if (this._Stamp != value)
-				{
-				
-                    this.OnStampChanging(value);
-					this.SendPropertyChanging();
-					this._Stamp = value;
-					this.SendPropertyChanged("Stamp");
-					this.OnStampChanged();
-				}
+        #region Extensibility Method Definitions
+        partial void OnLoaded();
+        partial void OnValidate(System.Data.Linq.ChangeAction action);
+        partial void OnCreated();
 
-			}
+        partial void OnIdChanging(int value);
+        partial void OnIdChanged();
 
-		}
+        partial void OnStampChanging(DateTime value);
+        partial void OnStampChanged();
 
-		
-		[Column(Name="Data", UpdateCheck=UpdateCheck.Never, Storage="_Data", DbType="nvarchar")]
-		public string Data
-		{
-			get { return this._Data; }
+        partial void OnDataChanging(string value);
+        partial void OnDataChanged();
 
-			set
-			{
-				if (this._Data != value)
-				{
-				
-                    this.OnDataChanging(value);
-					this.SendPropertyChanging();
-					this._Data = value;
-					this.SendPropertyChanged("Data");
-					this.OnDataChanged();
-				}
+        #endregion
 
-			}
+        public CheckInPending()
+        {
+            OnCreated();
+        }
 
-		}
+        #region Columns
 
-		
-    #endregion
-        
-    #region Foreign Key Tables
-   		
-	#endregion
-	
-	#region Foreign Keys
-    	
-	#endregion
-	
-		public event PropertyChangingEventHandler PropertyChanging;
-		protected virtual void SendPropertyChanging()
-		{
-			if ((this.PropertyChanging != null))
-				this.PropertyChanging(this, emptyChangingEventArgs);
-		}
+        [Column(Name = "Id", UpdateCheck = UpdateCheck.Never, Storage = "_Id", AutoSync = AutoSync.OnInsert, DbType = "int NOT NULL IDENTITY", IsPrimaryKey = true, IsDbGenerated = true)]
+        public int Id
+        {
+            get => _Id;
+            set
+            {
+                if (_Id != value)
+                {
+                    OnIdChanging(value);
+                    SendPropertyChanging();
+                    _Id = value;
+                    SendPropertyChanged("Id");
+                    OnIdChanged();
+                }
+            }
+        }
 
-		public event PropertyChangedEventHandler PropertyChanged;
-		protected virtual void SendPropertyChanged(String propertyName)
-		{
-			if ((this.PropertyChanged != null))
-				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-		}
+        [Column(Name = "Stamp", UpdateCheck = UpdateCheck.Never, Storage = "_Stamp", DbType = "datetime NOT NULL")]
+        public DateTime Stamp
+        {
+            get => _Stamp;
+            set
+            {
+                if (_Stamp != value)
+                {
+                    OnStampChanging(value);
+                    SendPropertyChanging();
+                    _Stamp = value;
+                    SendPropertyChanged("Stamp");
+                    OnStampChanged();
+                }
+            }
+        }
 
-   		
-	}
+        [Column(Name = "Data", UpdateCheck = UpdateCheck.Never, Storage = "_Data", DbType = "nvarchar")]
+        public string Data
+        {
+            get => _Data;
+            set
+            {
+                if (_Data != value)
+                {
+                    OnDataChanging(value);
+                    SendPropertyChanging();
+                    _Data = value;
+                    SendPropertyChanged("Data");
+                    OnDataChanged();
+                }
+            }
+        }
 
+        #endregion
+
+        #region Foreign Key Tables
+
+        #endregion
+
+        #region Foreign Keys
+
+        #endregion
+
+        public event PropertyChangingEventHandler PropertyChanging;
+        protected virtual void SendPropertyChanging()
+        {
+            PropertyChanging?.Invoke(this, emptyChangingEventArgs);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void SendPropertyChanged(String propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
 }
-
