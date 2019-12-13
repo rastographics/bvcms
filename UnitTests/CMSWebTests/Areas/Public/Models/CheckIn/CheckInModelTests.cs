18ced673cad0ddf70efcdaa27c674c04d1df1c3d
Bron Davies <bron@brontech.com>
2019-12-12 21:30:04 -0600

    Refactor CheckinModel and PrintJobs to use truly unique primary key

---
+++ UnitTests/CMSWebTests/Areas/Public/Models/CheckIn/CheckInModelTests.cs
@@ -0,0 +1,21 @@
+﻿using CmsWeb.Models;
+using System.Linq;
+using Xunit;
+using SharedTestFixtures;
+using Shouldly;
+
+namespace CmsWeb.ModelsTests
+{
+    [Collection(Collections.Database)]
+    public class CheckInModelTests : DatabaseTestBase
+    {
+        [Fact]
+        public void SavePrintJobTest()
+        {
+            var model = new CheckInModel(db);
+            model.SavePrintJob("Kiosk1", null, "{\"fake\":\"json string test\"}");
+
+            db.PrintJobs.Count().ShouldBeGreaterThan(0);
+        }
+    }
+}
