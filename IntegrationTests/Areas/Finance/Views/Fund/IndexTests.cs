7f2b57ba97a298dabe8692a9632d6fb44be2151c
Oscar Daniel Báez Lara <dahnbaez@gmail.com>
2019-12-17 16:58:43 -0600

    Alter finance bundle integration test

---
+++ IntegrationTests/Areas/Finance/Views/Fund/IndexTests.cs
@@ -0,0 +1,21 @@
+﻿using IntegrationTests.Support;
+using Shouldly;
+using System.Drawing;
+using Xunit;
+using SharedTestFixtures;
+
+namespace IntegrationTests.Areas.Finance.Views.Fund
+{
+    [Collection(Collections.Webapp)]
+    public class IndexTests : AccountTestBase
+    {
+        [Fact]
+        public void Should_Edit_ShowList_Fund()
+        {
+            //username = RandomString();
+            //password = RandomString();
+            //var user = CreateUser(username, password, roles: new string[] { "Access", "Edit", "Admin", "Finance" });
+            //Login();
+        }
+    }
+}
