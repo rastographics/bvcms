17d035416435c82a1007b4baf8ea627303996d10
Oscar Daniel Báez Lara <dahnbaez@gmail.com>
2019-12-23 00:36:00 -0600

    edit migration

--- CmsData/Migrations/20191214_alter_ContributionFund_table_to_add_ShowList_column.sql
+++ CmsData/Migrations/20191214_alter_ContributionFund_table_to_add_ShowList_column.sql
@@ -2,18 +2,18 @@ IF NOT EXISTS(SELECT 1 FROM sys.columns
           WHERE Name = N'ShowList'
           AND Object_ID = Object_ID(N'dbo.ContributionFund'))
 BEGIN
-    ALTER TABLE [dbo].[ContributionFund]
-    ADD [ShowList] INT NOT NULL DEFAULT 0
+    ALTER TABLE dbo.ContributionFund
+    ADD ShowList INT NOT NULL DEFAULT 0
 
-	UPDATE [dbo].[ContributionFund]
+	UPDATE dbo.ContributionFund
 	SET ShowList = 1
 	WHERE OnlineSort < 100
 
-	UPDATE [dbo].[ContributionFund]
+	UPDATE dbo.ContributionFund
 	SET ShowList = 2
 	WHERE OnlineSort > 100
 
-	UPDATE [dbo].[ContributionFund]
+	UPDATE dbo.ContributionFund
 	SET ShowList = 3
 	WHERE OnlineSort IS NULL
 END
