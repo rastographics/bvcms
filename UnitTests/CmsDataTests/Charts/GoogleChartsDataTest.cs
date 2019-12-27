90c6da0a4d521a9cae713d216daf0e3f2985024b
Bron Davies <bron@brontech.com>
2019-12-27 10:56:55 -0600

    Doing some housekeeping

--- UnitTests/CmsDataTests/Charts/GoogleChartsDataTest.cs
+++ UnitTests/CmsDataTests/Charts/GoogleChartsDataTest.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Shouldly;
-using Xunit;
-using UtilityExtensions;
-using CmsData;
+﻿using CmsData;
 using CmsData.Codes;
-using System.Reflection;
 using SharedTestFixtures;
-using CmsData;
+using Shouldly;
+using System.Linq;
+using UtilityExtensions;
+using Xunit;
 
 namespace CmsDataTests.Charts
 {
@@ -32,12 +28,12 @@ namespace CmsDataTests.Charts
         [Theory]
         [InlineData(new int[] { 101, 102, 201, 211, 212, 302, 306, 317, 319, 509 }, 2018)]
         [InlineData(new int[] { 211, 212 }, 2019)]
-        [InlineData(new int[] {  }, null)]
+        [InlineData(new int[] { }, null)]
         public void GetContributionsPerYear_Should_Not_GetReturnedReversedPledges(int[] fundids, int? year)
         {
             using (var db = CMSDataContext.Create(DatabaseFixture.Host))
             {
-                var contributions = (from c in Contribution.GetContributionsPerYear(db,year, fundids) select c);
+                var contributions = (from c in Contribution.GetContributionsPerYear(db, year, fundids) select c);
                 var returnedReversedPledges = from c2 in contributions
                                               where ContributionTypeCode.ReturnedReversedTypes.Contains(c2.ContributionTypeId)
                                               select c2;
@@ -45,7 +41,5 @@ namespace CmsDataTests.Charts
                 returnedReversedPledges.Count().ShouldBeLessThan(1);
             }
         }
-
-        
     }
 }
