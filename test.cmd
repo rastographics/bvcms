76d54ed0b41372c7cd3740aba601592fc450e028
Bron Davies <bron@brontech.com>
2020-01-18 12:36:03 -0600

    Try to include all tests in codecov

--- test.cmd
+++ test.cmd
@@ -11,19 +11,22 @@ set iisexpress="%ProgramFiles%\IIS Express\iisexpress.exe"
 echo using %OpenCover%
 echo using %xunit%
 echo using %codecov%
-set target_tests=.\UnitTests\CMSDataTests\bin\Debug\CMSDataTests.dll
-set target_tests=%target_tests% .\UnitTests\ImageDataTests\bin\Debug\ImageDataTests.dll
-set target_tests=%target_tests% .\UnitTests\CMSWebTests\bin\Debug\CMSWebTests.dll
-set target_tests=%target_tests% .\UnitTests\UtilityExtensionsTests\bin\Debug\UtilityExtensionsTests.dll
-set integration_tests=.\IntegrationTests\bin\Debug\IntegrationTests.dll
+set "target_tests=.\UnitTests\CMSDataTests\bin\Debug\CMSDataTests.dll"
+set "target_tests=%target_tests% .\UnitTests\ImageDataTests\bin\Debug\ImageDataTests.dll"
+set "target_tests=%target_tests% .\UnitTests\CMSWebTests\bin\Debug\CMSWebTests.dll"
+set "target_tests=%target_tests% .\UnitTests\UtilityExtensionsTests\bin\Debug\UtilityExtensionsTests.dll"
+::clean up
 echo quit | sqlcmd -S (local) -q "IF DB_ID('CMS_localhost') IS NOT NULL ALTER DATABASE CMS_localhost SET SINGLE_USER WITH ROLLBACK IMMEDIATE;DROP DATABASE CMS_localhost"
 echo quit | sqlcmd -S (local) -q "IF DB_ID('CMSi_localhost') IS NOT NULL ALTER DATABASE CMSi_localhost SET SINGLE_USER WITH ROLLBACK IMMEDIATE;DROP DATABASE CMSi_localhost"
+::Unit tests
 %OpenCover% -register:user -target:"%xunit%" -targetargs:"%target_tests% -noshadow -teamcity" -filter:%opencover_filters% -output:"%test_coverage%" || exit 7
 IF NOT EXIST %test_coverage% (
   echo File not found: %test_coverage%
   exit 8
 )
 IF "%CodeCovToken%" NEQ "" %codecov% -f "%test_coverage%" -t "%CodeCovToken%"
+::Integration tests
+set "integration_tests=.\IntegrationTests\bin\Debug\IntegrationTests.dll""
 set "IISEXPRESS_HOST=%OpenCover%"
 set placeholder="{0}"
 set "IISEXPRESS_ARGS=-register:user -target:%iisexpress% -targetargs:%placeholder% -filter:%opencover_filters% -output:%test_coverage%"
