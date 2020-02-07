bfb688455786dd6c9688027cbeb4d9355ea48c2a
jfescobar18 <jfescobar18@hotmail.com>
2020-02-06 20:37:07 -0600

    Pledges and Contributions grouped

--- CmsWeb/Areas/Reports/Models/Export/ExportPeople.cs
+++ CmsWeb/Areas/Reports/Models/Export/ExportPeople.cs
@@ -225,7 +225,7 @@ namespace CmsWeb.Models
             }
         }
         public DataTable ExcelDonorTotals(DateTime startdt, DateTime enddt,
-            int campusid, bool pledges, bool? nontaxdeductible, bool includeUnclosed, int? tagid, string fundids, bool includePledges)
+            int campusid, bool? pledges, bool? nontaxdeductible, bool includeUnclosed, int? tagid, string fundids, bool includePledges)
         {
 #if DEBUG2
             // for reconciliation by developer
@@ -237,7 +237,9 @@ namespace CmsWeb.Models
                   tw.WriteLine(s);
 #endif
             var nontaxded = includePledges ? 2 : (nontaxdeductible.HasValue ? (nontaxdeductible.Value ? 1 : 0) : (int?)null);
-            var q2 = from r in CurrentDatabase.GetTotalContributionsDonor(startdt, enddt, campusid, nontaxded, includeUnclosed, tagid, fundids, nontaxded == 2 ? true : (bool?)null)
+            pledges = nontaxded == 2 ? true : (bool?)null;
+
+            var q2 = from r in CurrentDatabase.GetTotalContributionsDonor(startdt, enddt, campusid, nontaxded, includeUnclosed, tagid, fundids, pledges)
                      select new
                      {
                          GiverId = r.CreditGiverId,
