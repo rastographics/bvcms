d70336759be4199dbd53244e4b3747429b0e84f6
Asher Lloyd <asher@clearfunction.com>
2020-02-13 16:20:37 -0600

    TPD-253 Amends Description column for AutoSyncBatchDates setting in migration

--- CmsData/Migrations/20200213_AutoSyncBatchDates_setting.sql
+++ CmsData/Migrations/20200213_AutoSyncBatchDates_setting.sql
@@ -7,7 +7,7 @@ BEGIN
 
     INSERT INTO dbo.SettingMetadata
     (SettingId, Description, DataType, SettingCategoryId, DefaultValue) VALUES
-    ('AutoSyncBatchDates', 'Set this to <i>true</i> to automatically retrieve batch transaction data.', @dataBool, @contributionsCategoryId, 1),
+    ('AutoSyncBatchDates', 'Set this to <i>true</i> to automatically retrieve batch transaction data nightly.', @dataBool, @contributionsCategoryId, 1),
     ('AutoSyncBatchDatesWindow', 'The range of days in which to search for transactions that have not yet been retrieved (prior to the current date).', @dataInt, @contributionsCategoryId, 7)
 END
 GO
