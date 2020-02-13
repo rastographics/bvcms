2a4b7a098f5ca6d18dff805f4f04b0bf6c566527
Bron Davies <bron@brontech.com>
2020-02-13 09:19:18 -0600

    Add eSpace days to sync setting

--- CmsData/Migrations/20200205_eSpace-integration-settings.sql
+++ CmsData/Migrations/20200205_eSpace-integration-settings.sql
@@ -1,6 +1,6 @@
 IF NOT EXISTS(select 1 from dbo.SettingCategory where Name = 'eSpace')
 BEGIN
-    DECLARE @Integrations INT = 3, @eSpace INT = 15, @dataBool INT = 1, @dataPassword INT = 4;
+    DECLARE @Integrations INT = 3, @eSpace INT = 15, @dataBool INT = 1, @dataPassword INT = 4, @dataInt INT = 5;
 
     SET IDENTITY_INSERT dbo.SettingCategory ON;
     
@@ -14,7 +14,8 @@ BEGIN
     (SettingId, Description, DataType, SettingCategoryId) VALUES
     ('eSpaceEnabled', 'Set this to <i>true</i> to enable eSpace integration features', @dataBool, @eSpace),
     ('eSpaceUserName', 'The eSpace user name (email) to use for the integration', @dataPassword, @eSpace),
-    ('eSpacePassword', 'The eSpace password to use for the integration', @dataPassword, @eSpace)
+    ('eSpacePassword', 'The eSpace password to use for the integration', @dataPassword, @eSpace),
+    ('eSpaceDaysToSync', 'The number of days in the future to search for data from eSpace that will be synced (The default is 60 days)', @dataInt, @eSpace)
 END
 GO
 
