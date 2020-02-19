75c088ae8419297a1201d3547430ca57bc843486
Asher Lloyd <asher@clearfunction.com>
2020-02-19 08:06:05 -0600

    TPD-251 Removes unnecessary identity insert sql setting

--- CmsData/Migrations/20200214_ShowSyncBatchDatesOption_setting.sql
+++ CmsData/Migrations/20200214_ShowSyncBatchDatesOption_setting.sql
@@ -2,8 +2,6 @@
 BEGIN
     DECLARE @contributionsCategoryId INT = 3, @dataBool INT = 1;
 
-    SET IDENTITY_INSERT dbo.SettingCategory ON;
-
     INSERT INTO dbo.SettingMetadata
     (SettingId, Description, DataType, SettingCategoryId, DefaultValue) VALUES
     ('ShowSyncBatchDatesOption', 'Set this to <i>true</i> to enable options to retrieve batch transaction data on demand.', @dataBool, @contributionsCategoryId, NULL)
