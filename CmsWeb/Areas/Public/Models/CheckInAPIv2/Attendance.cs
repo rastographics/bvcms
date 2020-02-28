4af352b367de772f95c957ae7ccf10c9e4a26c36
Tim Davila <tim@timdavila.com>
2020-02-28 16:43:33 -0600

    reformat updated attendance class file

--- CmsWeb/Areas/Public/Models/CheckInAPIv2/Attendance.cs
+++ CmsWeb/Areas/Public/Models/CheckInAPIv2/Attendance.cs
@@ -3,167 +3,181 @@ using System.Data.SqlClient;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using CmsData;
-using CmsData.Codes;
 using CmsWeb.Areas.Public.Models.CheckInAPIv2.Caching;
 
 namespace CmsWeb.Areas.Public.Models.CheckInAPIv2
 {
-	[SuppressMessage( "ReSharper", "CollectionNeverQueried.Global" )]
-	[SuppressMessage( "ReSharper", "UnusedMember.Global" )]
-	[SuppressMessage( "ReSharper", "NotAccessedField.Global" )]
-	[SuppressMessage( "ReSharper", "MemberCanBePrivate.Global" )]
-	[SuppressMessage( "ReSharper", "FieldCanBeMadeReadOnly.Global" )]
-	[SuppressMessage( "ReSharper", "UnassignedField.Global" )]
-	[SuppressMessage( "ReSharper", "ClassNeverInstantiated.Global" )]
-	public class Attendance
-	{
-		public const int SECURITY_LABELS_NONE = 0;
-		public const int SECURITY_LABELS_PER_MEETING = 1;
-		public const int SECURITY_LABELS_PER_CHILD = 2;
-		public const int SECURITY_LABELS_PER_FAMILY = 3;
+    [SuppressMessage("ReSharper", "CollectionNeverQueried.Global")]
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    [SuppressMessage("ReSharper", "NotAccessedField.Global")]
+    [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+    [SuppressMessage("ReSharper", "FieldCanBeMadeReadOnly.Global")]
+    [SuppressMessage("ReSharper", "UnassignedField.Global")]
+    [SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+    public class Attendance
+    {
+        public const int SECURITY_LABELS_NONE = 0;
+        public const int SECURITY_LABELS_PER_MEETING = 1;
+        public const int SECURITY_LABELS_PER_CHILD = 2;
+        public const int SECURITY_LABELS_PER_FAMILY = 3;
 
-		public int peopleID;
+        public int peopleID;
 
-		public List<AttendanceGroup> groups = new List<AttendanceGroup>();
+        public List<AttendanceGroup> groups = new List<AttendanceGroup>();
 
-		public List<Label> getSecurityLabel( AttendanceCacheSet cacheSet )
-		{
-			return Label.generate( cacheSet, Label.Type.SECURITY, this, groups );
-		}
+        public List<Label> getSecurityLabel(AttendanceCacheSet cacheSet)
+        {
+            return Label.generate(cacheSet, Label.Type.SECURITY, this, groups);
+        }
 
-		public List<Label> getLabels( AttendanceCacheSet cacheSet )
-		{
-			List<Label.Type> labelTypes = new List<Label.Type>();
-			List<Label> labels = new List<Label>();
-			CmsData.Person person = cacheSet.getPerson( peopleID );
+        public List<Label> getLabels(AttendanceCacheSet cacheSet)
+        {
+            List<Label.Type> labelTypes = new List<Label.Type>();
+            List<Label> labels = new List<Label>();
+            CmsData.Person person = cacheSet.getPerson(peopleID);
 
-			// Create label type list
-			if( groups.Count > 0 && hasAttends() ) {
-				// TODO: Client size option for age cutoff for Name Tag.  Server will override and will send it to the client and disable field
-				if( (person.Age ?? 0) < cacheSet.nameTagAge ) {
-					labelTypes.Add( Label.Type.MAIN );
-
-					foreach( AttendanceGroup group in groups ) {
-						Organization org = cacheSet.getOrganization( group.groupID );
-            bool isLeader = cacheSet.dataContext.OrganizationMembers.Any(o => o.PeopleId == person.PeopleId && o.MemberType.AttendanceTypeId == AttendTypeCode.Leader);
-
-            if (org != null && isLeader && org.NumWorkerCheckInLabels > 0 && group.present)
+            // Create label type list
+            if (groups.Count > 0 && hasAttends())
             {
-                for (int iX = 0; iX < org.NumWorkerCheckInLabels; iX++)
+                // TODO: Client size option for age cutoff for Name Tag.  Server will override and will send it to the client and disable field
+                if ((person.Age ?? 0) < cacheSet.nameTagAge)
+                {
+                    labelTypes.Add(Label.Type.MAIN);
+
+                    foreach (AttendanceGroup group in groups)
+                    {
+                        Organization org = cacheSet.getOrganization(group.groupID);
+
+                        if (org != null && org.NumCheckInLabels > 1 && group.present)
+                        {
+                            for (int iX = 0; iX < org.NumCheckInLabels - 1; iX++)
+                            {
+                                labelTypes.Add(Label.Type.EXTRA);
+                            }
+                        }
+                    }
+
+                    if (hasVisits(cacheSet))
+                    {
+                        if (cacheSet.guestLabels)
+                        {
+                            foreach (AttendanceGroup _ in getVisitGroups(cacheSet))
+                            {
+                                labelTypes.Add(Label.Type.GUEST);
+                            }
+                        }
+
+                        if (cacheSet.locationLabels)
+                        {
+                            labelTypes.Add(Label.Type.LOCATION);
+                        }
+                    }
+
+                    switch (cacheSet.securityLabels)
+                    {
+                        // case SECURITY_LABELS_NONE: 0 = No Security Labels
+                        // case SECURITY_LABELS_PER_FAMILY: Security Label Per Family (Handled outside this routine)
+
+                        case SECURITY_LABELS_PER_MEETING:
+                            {
+                                for (int iX = 0; iX < groups.Count; iX++)
+                                {
+                                    labelTypes.Add(Label.Type.SECURITY);
+                                }
+
+                                break;
+                            }
+
+                        case SECURITY_LABELS_PER_CHILD:
+                            {
+                                labelTypes.Add(Label.Type.SECURITY);
+
+                                break;
+                            }
+                    }
+                }
+                else
                 {
                     labelTypes.Add(Label.Type.NAME_TAG);
                 }
             }
-            else if ( org != null && org.NumCheckInLabels > 1 && group.present ) {
-							for( int iX = 0; iX < org.NumCheckInLabels - 1; iX++ ) {
-								labelTypes.Add( Label.Type.EXTRA );
-							}
-						}
-					}
 
-					if( hasVisits( cacheSet ) ) {
-						if( cacheSet.guestLabels ) {
-							foreach( AttendanceGroup _ in getVisitGroups( cacheSet ) ) {
-								labelTypes.Add( Label.Type.GUEST );
-							}
-						}
+            // Generate labels from type list
+            foreach (Label.Type labelType in labelTypes)
+            {
+                if (labelType == Label.Type.GUEST)
+                {
+                    labels.AddRange(Label.generate(cacheSet, labelType, this, getVisitGroups(cacheSet).ToList()));
+                }
+                else
+                {
+                    labels.AddRange(Label.generate(cacheSet, labelType, this, groups));
+                }
+            }
 
-						if( cacheSet.locationLabels ) {
-							labelTypes.Add( Label.Type.LOCATION );
-						}
-					}
+            return labels;
+        }
 
-					switch( cacheSet.securityLabels ) {
-						// case SECURITY_LABELS_NONE: 0 = No Security Labels
-						// case SECURITY_LABELS_PER_FAMILY: Security Label Per Family (Handled outside this routine)
+        private bool hasVisits(AttendanceCacheSet cacheSet)
+        {
+            OrganizationMember member;
 
-						case SECURITY_LABELS_PER_MEETING: {
-							for( int iX = 0; iX < groups.Count; iX++ ) {
-								labelTypes.Add( Label.Type.SECURITY );
-							}
+            foreach (AttendanceGroup group in groups)
+            {
+                member = cacheSet.getOrganizationMember(group.groupID, peopleID);
 
-							break;
-						}
+                if (member == null)
+                {
+                    return true;
+                }
+            }
 
-						case SECURITY_LABELS_PER_CHILD: {
-							labelTypes.Add( Label.Type.SECURITY );
+            return false;
+        }
 
-							break;
-						}
-					}
-				} else {
-          bool needsMemberNameTag = true;
-          foreach (AttendanceGroup group in groups)
-          {
-              Organization org = cacheSet.getOrganization(group.groupID);
+        private bool hasAttends()
+        {
+            foreach (AttendanceGroup group in groups)
+            {
+                if (group.present)
+                {
+                    return true;
+                }
+            }
 
-              bool isLeader = cacheSet.dataContext.OrganizationMembers.Any(o => o.PeopleId == person.PeopleId && o.MemberType.AttendanceTypeId == AttendTypeCode.Leader);
-              if (!isLeader && group.present && needsMemberNameTag)
-              {
-                  // members get one name tag
-                  labelTypes.Add(Label.Type.NAME_TAG);
-                  needsMemberNameTag = false;
-              } 
-              else if (org != null && isLeader && org.NumWorkerCheckInLabels > 0 && group.present)
-              {
-                  for (int iX = 0; iX < org.NumWorkerCheckInLabels; iX++)
-                  {
-                      labelTypes.Add( Label.Type.NAME_TAG );
-                  }
-              }
-          }
-				}
-			}
+            return false;
+        }
 
-			// Generate labels from type list
-			foreach( Label.Type labelType in labelTypes ) {
-				if( labelType == Label.Type.GUEST ) {
-					labels.AddRange( Label.generate( cacheSet, labelType, this, getVisitGroups( cacheSet ).ToList() ) );
-				} else {
-					labels.AddRange( Label.generate( cacheSet, labelType, this, groups ) );
-				}
-			}
+        private IEnumerable<AttendanceGroup> getVisitGroups(AttendanceCacheSet cacheSet)
+        {
+            OrganizationMember member;
 
-			return labels;
-		}
+            foreach (AttendanceGroup group in groups)
+            {
+                member = cacheSet.getOrganizationMember(group.groupID, peopleID);
 
-		private bool hasVisits( AttendanceCacheSet cacheSet )
-		{
-			OrganizationMember member;
+                if (member == null)
+                {
+                    yield return group;
+                }
+            }
+        }
 
-			foreach( AttendanceGroup group in groups ) {
-				member = cacheSet.getOrganizationMember( group.groupID, peopleID );
+        public void populateSubgroups(SqlConnection db, AttendanceCacheSet cacheSet)
+        {
+            foreach (AttendanceGroup group in groups)
+            {
+                Organization org = cacheSet.getOrganization(group.groupID);
+                if (org != null)
+                {
+                    var subgroups = Subgroup.forGroupID(db, group.groupID, peopleID);
+                    if (subgroups.Count > 0)
+                    {
+                        group.subgroupName = subgroups.First().name;
+                    }
+                }
+            }
+        }
 
-				if( member == null ) {
-					return true;
-				}
-			}
-
-			return false;
-		}
-
-		private bool hasAttends()
-		{
-			foreach( AttendanceGroup group in groups ) {
-				if( group.present ) {
-					return true;
-				}
-			}
-
-			return false;
-		}
-
-		private IEnumerable<AttendanceGroup> getVisitGroups( AttendanceCacheSet cacheSet )
-		{
-			OrganizationMember member;
-
-			foreach( AttendanceGroup group in groups ) {
-				member = cacheSet.getOrganizationMember( group.groupID, peopleID );
-
-				if( member == null ) {
-					yield return group;
-				}
-			}
-		}
-	}
+    }
 }
