e9985542b5d4f772002463e9773069d8a5419060
Bron Davies <bron@brontech.com>
2020-03-17 13:16:55 -0500

    Handle user-provided deposit date and account number, custom file name

---
+++ CmsWeb/Areas/Dialog/Models/RemoteDeposit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CmsWeb.Areas.Dialog.Models
+{
+    public class RemoteDeposit
+    {
+        public string AccountNumber { get; set; }
+        public int BundleHeaderId { get; set; }
+        public DateTime? DepositDate { get; set; }
+    }
+}
