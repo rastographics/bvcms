5ad7ed85e8bbead9c63e2b9f1e51ef44acd434e1
Jason Rice <jason.rice@pursuant.com>
2020-04-14 14:52:42 -0500

    added a unit test. the purpose of the test is to ensure that data can be inserted and deleted from our two new tables using our new models, GivingPage.cs and GivingPageFund.cs, and to ensure the foreign keys are all working correctly. everything appears to be working correctly!

---
+++ UnitTests/CmsDataTests/GivingSettings/GivingPageCRUD.cs
@@ -0,0 +1,30 @@
+﻿using CmsData.View;
+using SharedTestFixtures;
+using System.Linq;
+using Xunit;
+
+namespace CmsDataTests.GivingSettings
+{
+    [Collection(Collections.Database)]
+    public class GivingPageCRUD : DatabaseTestBase
+    {
+        [Theory]
+        [InlineData("Giving Page One")]
+        [InlineData("Giving Page Two")]
+        [InlineData("Giving Page Three")]
+        public void GivingPageCRUDTest(string givingPageName)
+        {
+            var contributionFund = CreateContributionFund();
+            var givingPage = CreateGivingPage(givingPageName, contributionFund.FundId);
+            var givingPageFund = CreateGivingPageFund(givingPage.GivingPageId, contributionFund.FundId);
+
+            var expectedName = givingPageName;
+            var actualName = givingPage.PageName;
+            Assert.Equal(expectedName, actualName);
+
+            DeleteGivingPageFund(givingPageFund);
+            DeleteGivingPage(givingPage);
+            DeleteContributionFund(contributionFund);
+        }
+    }
+}
