9ec76591e20850d7f1d22a48aac4429714cdbc72
Jason Rice <jason.rice@pursuant.com>
2020-04-15 18:38:40 -0500

    unit testing is successful! the test works and the sql function now works too

--- UnitTests/CmsDataTests/Organization/OrganizationTests.cs
+++ UnitTests/CmsDataTests/Organization/OrganizationTests.cs
@@ -1,8 +1,10 @@
 ﻿using CmsData;
 using CmsData.Codes;
+using DocuSign.eSign.Model;
 using SharedTestFixtures;
 using Shouldly;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
@@ -11,7 +13,41 @@ namespace CmsDataTests
     [Collection(Collections.Database)]
     public class OrganizationTests : DatabaseTestBase
     {
+        [Theory]
+        [InlineData("e191", 5)]
+        [InlineData("191", 5)]
+        [InlineData("2179950", 1)]
+        public void OrgSearchByName(string searchString, int count)
+        {
+            List<Organization> orgList = new List<Organization>();
+            for(int i = 0; i < count; i++)
+            {
+                string orgName = searchString + " Organization Testing " + i.ToString();
+                Organization org = MockOrganizations.CreateOrganization(db, orgName);
+                orgList.Add(org);
+            }
+            
+            int? programId = 0;
+            int? divisionId = 0;
+            int? typeId = 0;
+            int? campusId = 0;
+            int? scheduleId = 0;
+            int? statusId = 30;
+            int? onlineReg = -1;
+            int? userId = db.Users.Where(u => u.Username == "Admin").FirstOrDefault().UserId;
+            int? tagDiv = null;
 
+            int expectedCount = orgList.Count();
+
+            var actualCount = db.OrgSearch(searchString, programId, divisionId, typeId, campusId, scheduleId, statusId, onlineReg, userId, tagDiv);
+
+            Assert.Equal(expectedCount, actualCount.Count());
+
+            foreach(var item in orgList)
+            {
+                MockOrganizations.DeleteOrganization(db, item);
+            }
+        }
 
 
 
--- UnitTests/SharedTestFixtures/MockOrganizations.cs
+++ UnitTests/SharedTestFixtures/MockOrganizations.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CmsData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,39 @@ using System.Threading.Tasks;
 
 namespace SharedTestFixtures
 {
-    class MockOrganizations
+    public class MockOrganizations
     {
+        public static Organization CreateOrganization(CMSDataContext db, string orgName = null, int? campusId = null, int? fromId = null, int? type = null)
+        {
+            Organization org = null;
+            var newOrg = new Organization();
+            if (fromId != null)
+            {
+                org = db.LoadOrganizationById(fromId);
+            }
+            if (org == null)
+            {
+                org = db.Organizations.First();
+            }
+
+            newOrg.CreatedDate = DateTime.Now;
+            newOrg.CreatedBy = 0;
+            newOrg.OrganizationName = orgName ?? DatabaseTestBase.RandomString();
+            newOrg.EntryPointId = org.EntryPointId;
+            newOrg.OrganizationTypeId = type ?? org.OrganizationTypeId;
+            newOrg.OrganizationStatusId = 30;
+            newOrg.DivisionId = org.DivisionId;
+            newOrg.CampusId = campusId;
+
+            db.Organizations.InsertOnSubmit(newOrg);
+            db.SubmitChanges();
+
+            return newOrg;
+        }
+        public static void DeleteOrganization(CMSDataContext db, Organization organization)
+        {
+            db.Organizations.DeleteOnSubmit(organization);
+            db.SubmitChanges();
+        }
     }
 }
