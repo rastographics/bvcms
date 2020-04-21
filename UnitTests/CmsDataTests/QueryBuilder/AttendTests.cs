2fc6424ec033fa0f9d8d3e2906fa709fd9740884
David Carroll <david@bvcms.com>
2020-04-21 12:45:26 -0500

    Add Unit Test for RecentVisitNumberOrgs

--- UnitTests/CmsDataTests/QueryBuilder/AttendTests.cs
+++ UnitTests/CmsDataTests/QueryBuilder/AttendTests.cs
@@ -13,11 +13,29 @@ namespace CmsDataTests.QueryBuilder
         [Fact]
         public void RecentAttendCount_Should_Work()
         {
-            var code = "RecentAttendCount( Days=1, Org=36[App Testing Org] ) = 1";
+            var code = "RecentAttendCount( Days=2, Org=36[App Testing Org] ) = 1";
             
             var org = 36;
             var pid = 2;
-            var dt = DateTime.Today.AddHours(10);
+            var dt = DateTime.Today.AddHours(-10);
+
+            Attend.RecordAttend(db, pid, org, true, dt);
+            db.SubmitChanges();
+            var qb = db.PeopleQuery2(code);
+            var count = qb.Count();
+            db.ExecuteCommand("DELETE dbo.attend where OrganizationId = {0} AND MeetingDate = {1}", org, dt);
+            db.ExecuteCommand("DELETE dbo.meetings where OrganizationId = {0} AND MeetingDate = {1}", org, dt);
+
+            count.ShouldBe(1);
+        }
+        [Fact]
+        public void RecentVisitNumberOrgs()
+        {
+            var code = "RecentVisitNumberOrgs( Days=2, Org=36[App Testing Org] ) = 1";
+
+            var org = 36;
+            var pid = 2;
+            var dt = DateTime.Today.AddHours(-10);
 
             Attend.RecordAttend(db, pid, org, true, dt);
             db.SubmitChanges();
