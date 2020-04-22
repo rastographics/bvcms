d5f8b4fc0d4024b30da3c2535ae0f67adc742da7
Bron Davies <bron@brontech.com>
2020-04-22 12:42:16 -0500

    TPD-656 Allow SetDevicePIN to work with 6-digit sign in codes

--- CmsWeb/Areas/Public/Models/MobileAPIv2/MobileAuthentication.cs
+++ CmsWeb/Areas/Public/Models/MobileAPIv2/MobileAuthentication.cs
@@ -255,7 +255,9 @@ namespace CmsWeb.Areas.Public.Models.MobileAPIv2
                     return Error.USER_NOT_FOUND;
                 }
 
-                if (user.Person.EmailAddress != device.CodeEmail && user.Person.EmailAddress2 != device.CodeEmail)
+                if (user.Person.EmailAddress != device.CodeEmail &&
+                    user.Person.EmailAddress2 != device.CodeEmail &&
+                    user.Person.CellPhone != device.CodeEmail)
                 {
                     user = null;
 
