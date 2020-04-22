f0a843a0ff618201f44dd9c0cdaef7c02d69b6b2
David Carroll <david@bvcms.com>
2020-04-22 09:47:48 -0500

    Improve RecentVisitNumberOrgs test

--- UnitTests/CmsDataTests/QueryBuilder/AttendTests.cs
+++ UnitTests/CmsDataTests/QueryBuilder/AttendTests.cs
@@ -31,18 +31,20 @@ namespace CmsDataTests.QueryBuilder
         [Fact]
         public void RecentVisitNumberOrgs()
         {
-            var code = "RecentVisitNumberOrgs( Days=2, Org=36[App Testing Org] ) = 1";
+            var code = "RecentVisitNumberOrgs( VisitNumber='2', Days=2, Org=36[App Testing Org] ) = 1";
 
             var org = 36;
             var pid = 2;
-            var dt = DateTime.Today.AddHours(-10);
+            var dt1 = DateTime.Today.AddHours(-10);
+            var dt2 = dt1.AddDays(-1);
 
-            Attend.RecordAttend(db, pid, org, true, dt);
+            Attend.RecordAttend(db, pid, org, true, dt1);
+            Attend.RecordAttend(db, pid, org, true, dt2);
             db.SubmitChanges();
             var qb = db.PeopleQuery2(code);
             var count = qb.Count();
-            db.ExecuteCommand("DELETE dbo.attend where OrganizationId = {0} AND MeetingDate = {1}", org, dt);
-            db.ExecuteCommand("DELETE dbo.meetings where OrganizationId = {0} AND MeetingDate = {1}", org, dt);
+            db.ExecuteCommand("DELETE dbo.attend where OrganizationId = {0} AND MeetingDate IN ({1}, {2})", org, dt1, dt2);
+            db.ExecuteCommand("DELETE dbo.meetings where OrganizationId = {0} AND MeetingDate IN ({1}, {2})", org, dt1, dt2);
 
             count.ShouldBe(1);
         }
