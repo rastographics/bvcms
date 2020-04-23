153491d70f0094defc57c66f3278107f1eb0025a
John Redfield <redfieldjr@gmail.com>
2020-04-23 18:23:13 -0400

    TPD-598 - added null check in SendEmail method in case from address is missing.

---
+++ UnitTests/CmsDataTests/Email/SendEmailTests.cs
@@ -0,0 +1,37 @@
+﻿using CmsData;
+using CmsData.Codes;
+using DocuSign.eSign.Model;
+using SharedTestFixtures;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UtilityExtensions;
+using Xunit;
+
+namespace CmsDataTests.Email
+{
+    [Collection(Collections.Database)]
+    public class SendEmailTests : DatabaseTestBase
+    {
+        [Fact]
+        public void Send_Email_Without_From_Address_Does_Not_Throw_Exception()
+        {
+            bool success = true;
+
+            try
+            {
+                var from = Util.TryGetMailAddress("");
+                var person = db.People.Where(p => p.PeopleId == 1).FirstOrDefault();
+
+                db.EmailFinanceInformation(from, person, null, "This is a test", "This is a test");
+            }
+            catch
+            {
+                success = false;
+            }
+
+            Assert.True(success == true);
+        }
+    }
+}
