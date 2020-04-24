7cfb99f2695e39c222e12e6078712c31a4682343
Tim Davila <tim@timdavila.com>
2020-04-24 13:13:26 -0500

    add login metrics widget

--- CmsData/Migrations/20200330_homepage_widgets.sql
+++ CmsData/Migrations/20200330_homepage_widgets.sql
@@ -926,7 +926,7 @@ BEGIN
     <div class="box-title hidden-xs">
         <h5><a href="#">{{WidgetName}}</a></h5>
     </div>
-    <a class="visible-xs-block" id="giving-fc-collapse" data-toggle="collapse" href="#{{WidgetId}}-section" aria-expanded="true" aria-controls="{{WidgetId}}-section">
+    <a class="visible-xs-block" id="{{WidgetId}}-collapse" data-toggle="collapse" href="#{{WidgetId}}-section" aria-expanded="true" aria-controls="{{WidgetId}}-section">
         <div class="box-title">
             <h5>
                 <i class="fa fa-chevron-circle-right"></i>&nbsp;&nbsp;{{WidgetName}}
@@ -1202,6 +1202,199 @@ INSERT INTO [dbo].[DashboardWidgetRoles]
            ,(select min(RoleId) from Roles where RoleName like 'FinanceViewOnly')),
            ((select SCOPE_IDENTITY())
            ,(select min(RoleId) from Roles where RoleName like 'FinanceDataEntry')),
+           ((select SCOPE_IDENTITY())
+           ,(select min(RoleId) from Roles where RoleName like 'Admin'))
+END
+GO
+
+-- add login metrics widget
+IF (select count(*) from DashboardWidgets where [Name] like 'Login Metrics' and [System] = 1) = 0
+BEGIN
+  INSERT INTO [dbo].[Content]
+           ([Name],[Title],
+       [Body],
+       [DateCreated],[TypeID],[ThumbID],[RoleID],[OwnerID],[CreatedBy])
+     VALUES
+           ('WidgetLoginMetricsHTML','Edit Text Content',
+           '<div class="box">
+    <div class="box-title hidden-xs">
+        <h5><a href="#">{{WidgetName}}</a></h5>
+    </div>
+    <a class="visible-xs-block" id="giving-fc-collapse" data-toggle="collapse" href="#{{WidgetId}}-section" aria-expanded="true" aria-controls="{{WidgetId}}-section">
+        <div class="box-title">
+            <h5>
+                <i class="fa fa-chevron-circle-right"></i>&nbsp;&nbsp;{{WidgetName}}
+            </h5>
+        </div>
+    </a>
+    <div class="collapse in" id="{{WidgetId}}-section">
+        <div class="box-content center">
+            <h4 class="text-center">Login Metrics - Last {{days}} days</h4>
+            <div class="chart">
+            </div>
+        </div>
+    </div>
+</div>
+<script type="text/javascript">
+    var data = {{{counts}}};
+    google.charts.load("current", {packages:["corechart"]});
+    google.charts.setOnLoadCallback(function() {
+        data = google.visualization.arrayToDataTable([
+            [''Item'', ''Logins''],
+            [''Web'', {{counts.web}}],
+            [''Mobile'', {{counts.mobile}}],
+            [''Total'', {{counts.web}}]
+        ]);
+        var options = {
+            colors: [''#9575cd''],
+            vAxis: {
+                title: ''Unique logins''
+            },
+            legend: {
+                position: ''none''
+            }
+        };
+        var chart = new google.visualization.ColumnChart(document.querySelector(''#{{WidgetId}}-section .chart''));
+        chart.draw(data, options);
+    });
+</script>',
+           GETDATE(),1,0,0,0,'admin')
+           
+INSERT INTO [dbo].[ContentKeyWords]
+           ([Id]
+           ,[Word])
+     VALUES
+           ((select SCOPE_IDENTITY())
+           ,'widget')
+
+INSERT INTO [dbo].[Content]
+           ([Name],[Title],
+       [Body],
+       [DateCreated],[TypeID],[ThumbID],[RoleID],[OwnerID],[CreatedBy])
+     VALUES
+           ('WidgetLoginMetricsPython','Edit Python Script',
+           'from System.DateTime import Now
+Days = 30 # Report on logins for the past # of days entered here
+
+def Get():
+    today = Now
+    StartDate = today.AddDays(-Days).Date
+    strToday = today.ToString(''d'')
+    strStartDate = StartDate.ToString(''d'')
+    template = Data.HTMLContent
+    
+    appquery = ''''''
+    SELECT count(distinct p.peopleid) as Count
+    FROM dbo.MobileAppDevices mad
+    INNER JOIN dbo.People p on mad.PeopleId = p.PeopleId
+    WHERE mad.lastseen > ''{}''
+    ''''''
+    
+    webquery = ''''''
+    WITH loginUsers (userid) AS
+    (
+        SELECT distinct userid
+        FROM dbo.ActivityLog log
+        WHERE ActivityDate > ''{}''
+        AND Activity like ''%logged in''
+    )
+    SELECT count(distinct lu.userid) as Count
+    FROM loginUsers lu INNER JOIN dbo.Users u ON lu.Userid = u.UserId
+    INNER JOIN dbo.People p ON u.PeopleId = p.PeopleId
+    ''''''
+    
+    combinedquery = ''''''
+    WITH loginUsers (userid) AS
+    (
+        SELECT distinct userid
+        FROM dbo.ActivityLog log
+        WHERE ActivityDate > ''{}''
+        AND Activity like ''%logged in''
+    )
+    ,webUsers (web_userid) AS
+    (
+        SELECT lu.UserId
+        FROM loginUsers lu INNER JOIN dbo.Users u ON lu.Userid = u.UserId
+        INNER JOIN dbo.People p ON u.PeopleId = p.PeopleId
+    )
+    ,appUsers (app_userid) AS
+        (SELECT distinct userid
+        FROM dbo.MobileAppDevices mad
+        INNER JOIN dbo.People p on mad.PeopleId = p.PeopleId
+        WHERE mad.lastseen > ''{}''
+    )
+    SELECT count(*) as Count
+    FROM webUsers FULL OUTER JOIN appUsers ON webUsers.web_userid = appUsers.app_userId
+    ''''''
+    
+    counts = {}
+    
+    # Mobile app logins
+    rowcolquery = appquery.format(strStartDate)
+    data = q.QuerySql(rowcolquery)
+    for i in data:
+        counts["mobile"] =  int(i.Count or 0)
+    
+    # Web user logins
+    rowcolquery = webquery.format(strStartDate)
+    data = q.QuerySql(rowcolquery)
+    for i in data:
+        counts["web"] =  int(i.Count or 0)
+    
+    # Total unique login users
+    rowcolquery = combinedquery.format(strStartDate, strStartDate)
+    data = q.QuerySql(rowcolquery)
+    
+    for i in data:
+        counts["total"] =  int(i.Count or 0)
+        
+    Data.days = Days
+    Data.counts = model.DynamicData(counts)
+    print model.RenderTemplate(template)
+    
+Get()',
+           GETDATE(),5,0,0,0,'admin')
+          
+INSERT INTO [dbo].[ContentKeyWords]
+           ([Id]
+           ,[Word])
+     VALUES
+           ((select SCOPE_IDENTITY())
+           ,'widget')
+END
+GO
+
+IF (select count(*) from DashboardWidgets where [Name] like 'Login Metrics' and [System] = 1) = 0
+BEGIN
+INSERT INTO [dbo].[DashboardWidgets]
+           ([Name]
+           ,[Description]
+           ,[HTMLContentId]
+           ,[PythonContentId]
+           ,[SQLContentId]
+           ,[Enabled]
+           ,[Order]
+           ,[System]
+           ,[CachePolicy]
+           ,[CacheHours])
+     VALUES
+           ('Login Metrics'
+           ,'Shows a bar chart with login stats for the time period chosen'
+           ,(select max(Id) from Content where [Name] like 'WidgetLoginMetricsHTML')
+           ,(select max(Id) from Content where [Name] like 'WidgetLoginMetricsPython')
+           ,NULL
+           ,1
+           ,(select isnull(max([Order]), 0)+1 from DashboardWidgets)
+           ,1
+           ,1
+           ,12)
+
+INSERT INTO [dbo].[DashboardWidgetRoles]
+           ([WidgetId]
+           ,[RoleId])
+     VALUES
+           ((select SCOPE_IDENTITY())
+           ,(select min(RoleId) from Roles where RoleName like 'Edit')),
            ((select SCOPE_IDENTITY())
            ,(select min(RoleId) from Roles where RoleName like 'Admin'))
 END
