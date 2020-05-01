d0b5584836ae83f0067facc05d78cda8b490dde3
David Carroll <david@bvcms.com>
2020-05-01 18:27:47 -0500

    Parameterless Constructor on SmsReplyWordsModel

--- CmsWeb/Areas/Setup/Models/SmsReplyWordsModel.cs
+++ CmsWeb/Areas/Setup/Models/SmsReplyWordsModel.cs
@@ -12,6 +12,10 @@ namespace CmsWeb.Areas.Setup.Models
     {
         public CMSDataContext CurrentDatabase { get; set; }
 
+        public SmsReplyWordsModel()
+        {
+            
+        }
         public int GroupId { get; set; }
         public List<SmsActionModel> Actions { get; set; }
 
