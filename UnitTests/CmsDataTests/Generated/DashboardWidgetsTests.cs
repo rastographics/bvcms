f06a453baeae8a9d162b9d8dd7791383a6d7a54d
Jason Rice <jason.rice@pursuant.com>
2020-05-11 12:00:59 -0500

    unit test for checking to see if creating a python script content works, and then delete it. success

---
+++ UnitTests/CmsDataTests/Generated/DashboardWidgetsTests.cs
@@ -0,0 +1,26 @@
+﻿using SharedTestFixtures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CmsDataTests.Generated
+{
+    [Collection(Collections.Database)]
+    public class DashboardWidgetsTests : DatabaseTestBase
+    {
+        [Theory]
+        [InlineData(5)]
+        public void CreateWidgetContent(int pythonTypeId)
+        {
+            var pythonContent = MockContent.CreatePythonContent(db);
+            var expectedPythonTypeId = pythonTypeId;
+
+            Assert.Equal(pythonContent.TypeID, expectedPythonTypeId);
+
+            MockContent.DeletePythonContent(db, pythonContent);
+        }
+    }
+}
