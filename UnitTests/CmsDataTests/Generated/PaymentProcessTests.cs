603a6284ee4f928eebcb21aa2d0f9f6b7d178129
Bron Davies <bron@brontech.com>
2020-05-15 15:06:22 -0500

    TPD-813 Unit test

---
+++ UnitTests/CmsDataTests/Generated/PaymentProcessTests.cs
@@ -0,0 +1,26 @@
+﻿using SharedTestFixtures;
+using Shouldly;
+using System.Linq;
+using Xunit;
+
+namespace CmsDataTests.Generated
+{
+    [Collection(Collections.Database)]
+    public class PaymentProcessTests : DatabaseTestBase
+    {
+        [Fact]
+        public void PaymentProcess_UpdateTest()
+        {
+            db.PaymentProcess.Count().ShouldBe(4);
+            var process = db.PaymentProcess.First(p => p.ProcessId == 1);
+            process.ProcessName.ShouldBe("One-Time Giving");
+            process.AcceptACH = true;
+            process.AcceptCredit = false;
+            db.SubmitChanges();
+
+            var newdb = db.Copy();
+            var actual = newdb.PaymentProcess.First(p => p.ProcessId == 1);
+            actual.ShouldBeEquivalentTo(process);
+        }
+    }
+}
