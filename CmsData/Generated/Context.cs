0303561d0f485065efe9793e585e760289cd5788
Bron Davies <bron@brontech.com>
2020-05-16 20:53:12 -0500

    TPD-821 PaymentMethod schema

--- CmsData/Generated/Context.cs
+++ CmsData/Generated/Context.cs
@@ -1158,6 +1158,8 @@ namespace CmsData
 
         public Table<PaymentInfo> PaymentInfos => GetTable<PaymentInfo>();
 
+        public Table<PaymentMethod> PaymentMethods => GetTable<PaymentMethod>();
+
         public Table<Person> People => GetTable<Person>();
 
         public Table<PeopleCanEmailFor> PeopleCanEmailFors => GetTable<PeopleCanEmailFor>();
