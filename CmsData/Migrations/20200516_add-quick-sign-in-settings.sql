00ee6d23bdaecb8bbfdf1f958cf40014b8f50cd1
Bron Davies <bron@brontech.com>
2020-05-18 15:50:33 -0500

    TPD-656 Quick sign-in settings

--- CmsData/Migrations/20200516_add-quick-sign-in-settings.sql
+++ CmsData/Migrations/20200516_add-quick-sign-in-settings.sql
@@ -1,13 +1,13 @@
 IF NOT EXISTS(SELECT 1 FROM dbo.SettingMetadata WHERE SettingId = 'UseMobileQuickSignInCodes')
 BEGIN
 
-DECLARE @Security INT = 4, @dataBool INT = 1, @dataText INT = 3;
+DECLARE @MobileApp INT = 7, @dataBool INT = 1, @dataText INT = 3;
 
     INSERT INTO dbo.SettingMetadata
     (SettingId, Description, DataType, SettingCategoryId) VALUES
-    ('UseMobileQuickSignInCodes', 'Set this to <i>true</i> to enable quick mobile app sign in without a password using a 6-digit code. See <a href="https://docs.touchpointsoftware.com/MobileApp/QuickSignIn.html" target="_blank">this help article</a> for more information.', @dataBool, @Security),
-    ('MobileQuickSignInCodeSMS', 'This is the SMS message template that will be sent to users when they sign in to the mobile app using quick sign in using their cell phone number. See <a href="https://docs.touchpointsoftware.com/MobileApp/QuickSignIn.html" target="_blank">this help article</a> for more information.', @dataText, @Security),
-    ('MobileQuickSignInSubject', 'Set this to customize the subject line of the email for mobile quick sign in codes. See <a href="https://docs.touchpointsoftware.com/MobileApp/QuickSignIn.html" target="_blank">this help article</a> for more information.', @dataText, @Security)
+    ('UseMobileQuickSignInCodes', 'Set this to <i>true</i> to enable quick mobile app sign in without a password using a 6-digit code. See <a href="https://docs.touchpointsoftware.com/MobileApp/QuickSignIn.html" target="_blank">this help article</a> for more information.', @dataBool, @MobileApp),
+    ('MobileQuickSignInCodeSMS', 'This is the SMS message template that will be sent to users when they sign in to the mobile app using quick sign in using their cell phone number. See <a href="https://docs.touchpointsoftware.com/MobileApp/QuickSignIn.html" target="_blank">this help article</a> for more information.', @dataText, @MobileApp),
+    ('MobileQuickSignInSubject', 'Set this to customize the subject line of the email for mobile quick sign in codes. See <a href="https://docs.touchpointsoftware.com/MobileApp/QuickSignIn.html" target="_blank">this help article</a> for more information.', @dataText, @MobileApp)
 END
 GO
 
