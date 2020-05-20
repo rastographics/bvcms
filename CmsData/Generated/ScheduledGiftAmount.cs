559504c7fb87d0bb1706d6621c95ace9e7c1fd9f
Bron Davies <bron@brontech.com>
2020-05-20 18:20:08 -0500

    TPD-801 Recurring giving scheduled amounts

---
+++ CmsData/Generated/ScheduledGiftAmount.cs
@@ -0,0 +1,216 @@
+﻿using CmsData.Infrastructure;
+using System;
+using System.ComponentModel;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+namespace CmsData
+{
+    [Table(Name = "dbo.ScheduledGiftAmount")]
+    public partial class ScheduledGiftAmount : INotifyPropertyChanging, INotifyPropertyChanged
+    {
+        private static PropertyChangingEventArgs emptyChangingEventArgs => new PropertyChangingEventArgs("");
+
+        #region Private Fields
+
+        private int _ScheduledGiftAmountId;
+
+        private Guid _ScheduledGiftId;
+
+        private int _FundId;
+
+        private decimal _Amount;
+
+        private EntityRef<ContributionFund> _ContributionFund;
+
+        private EntityRef<ScheduledGift> _ScheduledGift;
+
+        #endregion
+
+        #region Extensibility Method Definitions
+
+        partial void OnLoaded();
+        partial void OnValidate(ChangeAction action);
+        partial void OnCreated();
+
+        partial void OnScheduledGiftAmountIdChanging(int value);
+        partial void OnScheduledGiftAmountIdChanged();
+
+        partial void OnScheduledGiftIdChanging(Guid value);
+        partial void OnScheduledGiftIdChanged();
+
+        partial void OnFundIdChanging(int value);
+        partial void OnFundIdChanged();
+
+        partial void OnAmountChanging(decimal value);
+        partial void OnAmountChanged();
+
+        #endregion
+
+        public ScheduledGiftAmount()
+        {
+            _ContributionFund = default;
+
+            OnCreated();
+        }
+
+        #region Columns
+
+        [Column(Name = "ScheduledGiftAmountId", UpdateCheck = UpdateCheck.Never, Storage = "_ScheduledGiftAmountId", AutoSync = AutoSync.OnInsert, DbType = "int NOT NULL IDENTITY", IsPrimaryKey = true, IsDbGenerated = true)]
+        public int ScheduledGiftAmountId
+        {
+            get => _ScheduledGiftAmountId;
+            set
+            {
+                if (_ScheduledGiftAmountId != value)
+                {
+                    OnScheduledGiftAmountIdChanging(value);
+                    SendPropertyChanging();
+                    _ScheduledGiftAmountId = value;
+                    SendPropertyChanged("ScheduledGiftAmountId");
+                    OnScheduledGiftAmountIdChanged();
+                }
+            }
+        }
+
+        [Column(Name = "ScheduledGiftId", UpdateCheck = UpdateCheck.Never, Storage = "_ScheduledGiftId", DbType = "uniqueidentifier")]
+        [IsForeignKey]
+        public Guid ScheduledGiftId
+        {
+            get => _ScheduledGiftId;
+            set
+            {
+                if (_ScheduledGiftId != value)
+                {
+                    OnScheduledGiftIdChanging(value);
+                    SendPropertyChanging();
+                    _ScheduledGiftId = value;
+                    SendPropertyChanged("ScheduledGiftId");
+                    OnScheduledGiftIdChanged();
+                }
+            }
+        }
+
+        [Column(Name = "FundId", UpdateCheck = UpdateCheck.Never, Storage = "_FundId", DbType = "int NOT NULL", IsPrimaryKey = true)]
+        [IsForeignKey]
+        public int FundId
+        {
+            get => _FundId;
+            set
+            {
+                if (_FundId != value)
+                {
+                    if (_ContributionFund.HasLoadedOrAssignedValue)
+                    {
+                        throw new ForeignKeyReferenceAlreadyHasValueException();
+                    }
+
+                    OnFundIdChanging(value);
+                    SendPropertyChanging();
+                    _FundId = value;
+                    SendPropertyChanged("FundId");
+                    OnFundIdChanged();
+                }
+            }
+        }
+
+        [Column(Name = "Amount", UpdateCheck = UpdateCheck.Never, Storage = "_Amount", DbType = "money")]
+        public decimal Amount
+        {
+            get => _Amount;
+            set
+            {
+                if (_Amount != value)
+                {
+                    OnAmountChanging(value);
+                    SendPropertyChanging();
+                    _Amount = value;
+                    SendPropertyChanged("Amount");
+                    OnAmountChanged();
+                }
+            }
+        }
+
+        #endregion
+
+        #region Foreign Key Tables
+
+        #endregion
+
+        #region Foreign Keys
+
+        [Association(Name = "FK_ScheduledGiftAmounts_ContributionFund", Storage = "_ContributionFund", ThisKey = "FundId", IsForeignKey = true)]
+        public ContributionFund ContributionFund
+        {
+            get => _ContributionFund.Entity;
+            set
+            {
+                ContributionFund previousValue = _ContributionFund.Entity;
+                if ((previousValue != value) || (_ContributionFund.HasLoadedOrAssignedValue == false))
+                {
+                    SendPropertyChanging();
+                    if (previousValue != null)
+                    {
+                        _ContributionFund.Entity = null;
+                    }
+
+                    _ContributionFund.Entity = value;
+                    if (value != null)
+                    {
+                        _FundId = value.FundId;
+                    }
+                    else
+                    {
+                        _FundId = default;
+                    }
+
+                    SendPropertyChanged("ContributionFund");
+                }
+            }
+        }
+
+        [Association(Name = "FK_ScheduledGiftAmounts_ScheduledGift", Storage = "_ScheduledGift", ThisKey = "ScheduledGiftId", IsForeignKey = true)]
+        public ScheduledGift ScheduledGift
+        {
+            get => _ScheduledGift.Entity;
+            set
+            {
+                ScheduledGift previousValue = _ScheduledGift.Entity;
+                if ((previousValue != value) || (_ScheduledGift.HasLoadedOrAssignedValue == false))
+                {
+                    SendPropertyChanging();
+                    if (previousValue != null)
+                    {
+                        _ScheduledGift.Entity = null;
+                    }
+
+                    _ScheduledGift.Entity = value;
+                    if (value != null)
+                    {
+                        _ScheduledGiftId = value.ScheduledGiftId;
+                    }
+                    else
+                    {
+                        _ScheduledGiftId = default;
+                    }
+
+                    SendPropertyChanged("ScheduledGift");
+                }
+            }
+        }
+
+        #endregion
+
+        public event PropertyChangingEventHandler PropertyChanging;
+        protected virtual void SendPropertyChanging()
+        {
+            PropertyChanging?.Invoke(this, emptyChangingEventArgs);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void SendPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- CmsData/Migrations/20200517_create-table-paymentmethod.sql
+++ CmsData/Migrations/20200517_create-table-paymentmethod.sql
@@ -35,3 +35,61 @@ GO
 
 ALTER TABLE [dbo].[PaymentMethod] CHECK CONSTRAINT [FK_PaymentMethod_People]
 GO
+
+IF OBJECT_ID(N'dbo.ScheduledGift', N'U') IS NULL
+BEGIN
+CREATE TABLE [dbo].[ScheduledGift](
+	[ScheduledGiftId] uniqueidentifier NOT NULL CONSTRAINT DF_ScheduledGift_ScheduledGiftId DEFAULT (NEWID()),
+	[PeopleId] int NOT NULL,
+	[ScheduledGiftTypeId] int NOT NULL,
+	[PaymentMethodId] uniqueidentifier NOT NULL,
+	[StartDate] datetime NOT NULL,
+	[EndDate] datetime NULL
+CONSTRAINT [PK_ScheduledGift_ScheduledGiftId] PRIMARY KEY NONCLUSTERED ([ScheduledGiftId])
+WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+END
+GO
+
+IF OBJECT_ID(N'FK_ScheduledGift_PaymentMethod') IS NULL
+ALTER TABLE [dbo].[ScheduledGift]  WITH CHECK ADD  CONSTRAINT [FK_ScheduledGift_PaymentMethod] FOREIGN KEY([PaymentMethodId])
+REFERENCES [dbo].[PaymentMethod] ([PaymentMethodId])
+GO
+
+ALTER TABLE [dbo].[ScheduledGift] CHECK CONSTRAINT [FK_ScheduledGift_PaymentMethod]
+GO
+
+IF OBJECT_ID(N'FK_ScheduledGift_People') IS NULL
+ALTER TABLE [dbo].[ScheduledGift]  WITH CHECK ADD  CONSTRAINT [FK_ScheduledGift_People] FOREIGN KEY([PeopleId])
+REFERENCES [dbo].[People] ([PeopleId])
+GO
+
+ALTER TABLE [dbo].[ScheduledGift] CHECK CONSTRAINT [FK_ScheduledGift_People]
+GO
+
+IF OBJECT_ID(N'dbo.ScheduledGiftAmount', N'U') IS NULL
+BEGIN
+CREATE TABLE [dbo].[ScheduledGiftAmount](
+	[ScheduledGiftAmountId] int IDENTITY(1,1) NOT NULL,
+	[ScheduledGiftId] uniqueidentifier NOT NULL,
+	[FundId] int NOT NULL,
+	[Amount] decimal NOT NULL
+CONSTRAINT [PK_ScheduledGiftAmount_ScheduledGiftAmountId] PRIMARY KEY CLUSTERED ([ScheduledGiftAmountId])
+WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+END
+GO
+
+ALTER TABLE [dbo].[ScheduledGiftAmount]  WITH NOCHECK ADD  CONSTRAINT [FK_ScheduledGiftAmounts_ScheduledGift] FOREIGN KEY([ScheduledGiftId])
+REFERENCES [dbo].[ScheduledGift] ([ScheduledGiftId])
+GO
+
+ALTER TABLE [dbo].[ScheduledGiftAmount] CHECK CONSTRAINT [FK_ScheduledGiftAmounts_ScheduledGift]
+GO
+
+ALTER TABLE [dbo].[ScheduledGiftAmount]  WITH NOCHECK ADD  CONSTRAINT [FK_ScheduledGiftAmounts_Fund] FOREIGN KEY([FundId])
+REFERENCES [dbo].[ContributionFund] ([FundId])
+GO
+
+ALTER TABLE [dbo].[ScheduledGiftAmount] CHECK CONSTRAINT [FK_ScheduledGiftAmounts_Fund]
+GO
