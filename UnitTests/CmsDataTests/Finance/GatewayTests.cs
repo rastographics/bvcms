49041ed9bec4a07279cf864b508c8449db2bc365
Jason Rice <jason.rice@pursuant.com>
2020-06-11 14:43:24 -0500

    unit test complete. testing for authorize and create payment method for credit card and bank account successful

--- UnitTests/CmsDataTests/Finance/GatewayTests.cs
+++ UnitTests/CmsDataTests/Finance/GatewayTests.cs
@@ -1,24 +1,70 @@
 ﻿using System;
+using System.Linq;
 using System.Linq.Dynamic;
 using CmsData;
 using CmsData.Finance;
 using CmsWeb.Areas.Giving.Models;
+using CmsWeb.Areas.Giving.Controllers;
 using SharedTestFixtures;
 using Xunit;
+using Shouldly;
 
 namespace CmsData.Finance.Tests
 {
     [Collection("Gateway Tests")]
     public class GatewayTests : DatabaseTestBase
     {
+        public GatewayTests() : base()
+        {
+            MockAppSettings.Apply(
+                ("PublicKey", "mytest"),
+                ("PublicSalt", "66 82 79 78 66 82 79 78")
+            );
+        }
+
         [Fact]
         public void AuthCreditCardCreatePaymentMethod()
         {
-            //(int ? paymentTypeId = null, bool isDefault = true, string name = "", string firstName = "", string lastName = "", string bankAccount = "", string bankRouting = "",
-            //string cardNumber = "", string cvv = "", string expiresMonth = null, string expiresYear = null, string address = "", string address2 = "", string city = "",
-            //string state = "", string country = "", string zip = "", string phone = "", string transactionTypeId = "", string emailAddress = "")
-
             var person = CreatePerson();
+            // Add/remove reference to CMSWeb in solution explorer to update metadata
+            GivingPaymentViewModel viewModel = new GivingPaymentViewModel()
+            {
+                paymentTypeId = 2,
+                isDefault = true,
+                name = "My Visa",
+                firstName = "Jason",
+                lastName = "Rice",
+                cardNumber = "4111111111111111",
+                cvv = "999",
+                expiresMonth = "05",
+                expiresYear = "2099",
+                address = "33",
+                address2 = "55",
+                city = "Dallas",
+                state = "Texas",
+                country = "United States",
+                zip = "99997-0008",
+                phone = "2149123704",
+                transactionTypeId = "authOnlyTransaction",
+                incomingPeopleId = person.PeopleId,
+                testing = true
+            };
+
+            var givingPaymentModel = new GivingPaymentModel(db);
+            givingPaymentModel.CreateMethod(viewModel);
+
+            var paymentMethod = (from pm in db.PaymentMethods
+                                 where pm.PeopleId == person.PeopleId
+                                 select pm).FirstOrDefault();
+            paymentMethod.Decrypt();
+            paymentMethod.NameOnAccount.ShouldBe("Jason Rice");
+        }
+
+        [Fact]
+        public void AuthBankCreatePaymentMethod()
+        {
+            var person = CreatePerson();
+            // Add/remove reference to CMSWeb in solution explorer to update metadata
             GivingPaymentViewModel viewModel = new GivingPaymentViewModel()
             {
                 paymentTypeId = 1,
@@ -33,37 +79,20 @@ namespace CmsData.Finance.Tests
                 testing = true
             };
 
-            //var paymentMethod = db.PaymentMethods.Where(pm => pm.)
+            var givingPaymentModel = new GivingPaymentModel(db);
+            givingPaymentModel.CreateMethod(viewModel);
 
-            //MethodsCreate(1,true,"My Bank","Jason", "Rice","123456789", "111000614","","","","","","","","","","","","authOnlyTransaction");
-            //MethodsCreate(2, true, "My Visa", "Jason", "Rice","","", "4111111111111111", "999", "05", "2021","33", "my address 2", "Dallas", "Texas", "United States", "99997-0008", "2149123704", "authOnlyTransaction");
+            var paymentMethod = (from pm in db.PaymentMethods
+                                 where pm.PeopleId == person.PeopleId
+                                 select pm).FirstOrDefault();
+            paymentMethod.Decrypt();
+            paymentMethod.NameOnAccount.ShouldBe("Jason Rice");
+        }
 
-            //TODO: Finish this test
-            //int peopleId = 1;
-            //decimal amt = 0;        
-            //string cardnumber = "4111111111111111";
-            //string expires = "0220";
-            //string description = "desc";
-            //int tranid = 123;
-            //string cardcode = "999";
-            //string email = "dahnabez@gmail.com";
-            //string first = "Oskar";
-            //string last = "Baez";
-            //string addr = "addr";
-            //string addr2 = "addr2";
-            //string city = "mycity";
-            //string state = "mystate";
-            //string country = "United States";
-            //string zip = "03510";
-            //string phone = "5547946830";
-
-            //var db = CMSDataContext.Create(DatabaseFixture.Host);
-            //var gateway = db.Gateway(true);
-
-            //var response = gateway.PayWithCreditCard(peopleId, amt, cardnumber, expires, description, tranid, cardcode, email, first, last, addr, addr2, city, state, country, zip, phone);
-            //bool actual = response.Approved;
-            //bool expected = true;
-            //Assert.Equal(expected, actual);
+        public override void Dispose()
+        {
+            base.Dispose();
+            MockAppSettings.Remove("PublicKey", "PublicSalt");
         }
     }
 }
