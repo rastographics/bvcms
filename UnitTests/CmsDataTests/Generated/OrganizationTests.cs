c5866a89678a2901e216c1f57fb3ba3e30a37dd1
Jason Rice <jason.rice@pursuant.com>
2020-06-25 13:51:40 -0500

    fixed my tests according to suggestions from Bron, added some code to test Campus creation and deletion

--- UnitTests/CmsDataTests/Generated/OrganizationTests.cs
+++ UnitTests/CmsDataTests/Generated/OrganizationTests.cs
@@ -5,15 +5,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SharedTestFixtures;
+using Shouldly;
 
 namespace CmsData.Tests
 {
-    public class OrganizationTests
+    [Collection(Collections.Database)]
+    public class OrganizationTests : DatabaseTestBase
     {
         [Fact()]
         public void OrganizationTest()
         {
-            //Assert.True(false, "This test needs an implementation");
+            var orgName = "Test Organization" + DateTime.Now.ToString();
+            var campus = MockCampus.CreateCampus(db, "AP", "Main Number 2");
+            var newOrg = MockOrganizations.CreateOrganization(db, orgName, campus.Id, null, null, "http://localhost:50171/Give/Test%20222%20Title");
+
+            var org = (from o in db.Organizations where o.OrganizationId == newOrg.OrganizationId select o).FirstOrDefault();
+            org.OrganizationName.ShouldBe(orgName);
+            org.RedirectUrl.ShouldBe(newOrg.RedirectUrl);
+
+            MockOrganizations.DeleteOrganization(db, org);
+            MockCampus.DeleteCampus(db, campus);
         }
     }
 }
