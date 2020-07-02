0b9919489fbebd2d44bff5bc036ed0fdd8c09ead
Jason Rice <jason.rice@pursuant.com>
2020-07-02 17:34:30 -0500

    added db settings for TPD-1153 Donor Covers Fee, just need the descriptions from Chris and then this card will be finished.

---
+++ CmsData/Migrations/20200702_Donor_Covers_Fee.sql
@@ -0,0 +1,53 @@
+﻿IF (select count(*) from SettingMetadata where [SettingId] like 'CreditBase') = 0
+BEGIN
+INSERT INTO [dbo].SettingMetadata
+           ([SettingId], [DisplayName], [Description], [DataType], [SettingCategoryId], [DefaultValue])
+     VALUES
+           ('CreditBase', NULL, '', 5, 3, NULL)
+END
+GO
+
+IF (select count(*) from SettingMetadata where [SettingId] like 'CreditPercent') = 0
+BEGIN
+INSERT INTO [dbo].SettingMetadata
+           ([SettingId], [DisplayName], [Description], [DataType], [SettingCategoryId], [DefaultValue])
+     VALUES
+           ('CreditPercent', NULL, '', 5, 3, NULL)
+END
+GO
+
+IF (select count(*) from SettingMetadata where [SettingId] like 'DebitBase') = 0
+BEGIN
+INSERT INTO [dbo].SettingMetadata
+           ([SettingId], [DisplayName], [Description], [DataType], [SettingCategoryId], [DefaultValue])
+     VALUES
+           ('DebitBase', NULL, '', 5, 3, NULL)
+END
+GO
+
+IF (select count(*) from SettingMetadata where [SettingId] like 'DebitPercent') = 0
+BEGIN
+INSERT INTO [dbo].SettingMetadata
+           ([SettingId], [DisplayName], [Description], [DataType], [SettingCategoryId], [DefaultValue])
+     VALUES
+           ('DebitPercent', NULL, '', 5, 3, NULL)
+END
+GO
+
+IF (select count(*) from SettingMetadata where [SettingId] like 'ACHBase') = 0
+BEGIN
+INSERT INTO [dbo].SettingMetadata
+           ([SettingId], [DisplayName], [Description], [DataType], [SettingCategoryId], [DefaultValue])
+     VALUES
+           ('ACHBase', NULL, '', 5, 3, NULL)
+END
+GO
+
+IF (select count(*) from SettingMetadata where [SettingId] like 'ACHPercent') = 0
+BEGIN
+INSERT INTO [dbo].SettingMetadata
+           ([SettingId], [DisplayName], [Description], [DataType], [SettingCategoryId], [DefaultValue])
+     VALUES
+           ('ACHPercent', NULL, '', 5, 3, NULL)
+END
+GO
