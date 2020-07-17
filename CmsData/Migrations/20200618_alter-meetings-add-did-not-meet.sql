34638c2fc2918c66f319b97d571d5a3f61a57be4
brondavies <bron@brontech.com>
2020-07-17 15:55:06 -0400

    TPD-310 Add setting for AttendanceReminderAuthenticatedLinks

--- CmsData/Migrations/20200618_alter-meetings-add-did-not-meet.sql
+++ CmsData/Migrations/20200618_alter-meetings-add-did-not-meet.sql
@@ -6,3 +6,13 @@ BEGIN
     ADD DidNotMeet bit NOT NULL CONSTRAINT DF_Meetings_DidNotMeet DEFAULT 0
 END
 GO
+
+IF NOT EXISTS(select 1 from dbo.SettingMetadata where SettingId = 'AttendanceReminderAuthenticatedLinks')
+BEGIN
+    DECLARE @Administration INT = 1, @dataBool INT = 1;
+
+    INSERT INTO dbo.SettingMetadata
+    (SettingId, Description, DataType, SettingCategoryId) VALUES
+    ('AttendanceReminderAuthenticatedLinks', 'Set this to <i>true</i> to enable automatic sign in with links for group leader attendance reminder emails and SMS', @dataBool, @Administration)
+END
+GO
